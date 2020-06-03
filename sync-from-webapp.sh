a9e333baa3653395ae91a46a22701add4aee3685
Abel Toledano <atoledano@tuenti.com>
2020-06-03 12:14:22 +0200

    fix sync script

--- sync-from-webapp.sh
+++ sync-from-webapp.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 set -e
-# cp -r ../webapp/packages/ui/src .
-# cp -r ../webapp/packages/ui/doc .
-# cp -r ../webapp/packages/playroom/src/* ./playroom
-rm ./playroom/roboto.css
 
+# copy some files from webapp
+cp -r ../webapp/packages/ui/src .
+cp -r ../webapp/packages/ui/doc .
+cp -r ../webapp/packages/playroom/src/* ./playroom
+rm ./playroom/roboto.css # we already have roboto.css in /css folder
+
+#  fix some imorts 
+find src -type f -exec sed -i -e 's/@tuenti\/ui/@telefonica\/mistica/g' {} \;
+find doc -type f -exec sed -i -e 's/@tuenti\/ui/@telefonica\/mistica/g' {} \;
+find src -type f -exec sed -i -e 's/@tuenti\/libphonenumber/@telefonica\/libphonenumber/g' {} \;
+find playroom -type f -exec sed -i -e 's/\.\/roboto\.css/\.\.\/css\/roboto\.css/g' {} \;
+find playroom -type f -exec sed -i -e 's/@tuenti\/ui\/css\/reset\.css/\.\.\/css\/reset\.css/g' {} \;
+find playroom -type f -exec sed -i -e 's/@tuenti\/ui/\.\.\/src/g' {} \;
+
+# remove some eslint suppression comments for rules we don't have in this repo
 for f in $(grep -rl src -e 'no-unused-prop-types'); do
     sed -i '/no-unused-prop-types/d' $f
 done
@@ -20,3 +31,5 @@ done
 for f in $(grep -rl playroom -e 'no-extraneous-dependencies'); do
     sed -i '/no-extraneous-dependencies/d' $f
 done
+
+yarn prettier --write .
