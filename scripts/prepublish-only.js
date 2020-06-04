d84828caf02ae0cf23fe1248a28d90ef0f5136a2
Pedro Ladaria <pladaria@tuenti.com>
2020-06-04 15:38:18 +0200

    APPS-6691 WIP publish to @telefonica

--- scripts/prepublish-only.js
+++ scripts/prepublish-only.js
@@ -1,7 +1,13 @@
 // @flow
 if (!process.env.CI) {
     console.error();
-    console.error('You are not allowed to publish from your machine');
+    console.error('Cannot publish from this machine');
+    console.error();
+    console.error('Execute:');
+    console.error('    tu-ci npm_publish');
+    console.error();
+    console.error('Or for a dry-run:');
+    console.error('    tu-ci npm_publish_dry_run');
     console.error();
     process.exit(1);
 }
