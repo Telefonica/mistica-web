2e9a3af987acda6c95c5cedf5792db2ffa1ca4ef
Pedro Ladaria <pladaria@tuenti.com>
2020-06-05 18:22:21 +0200

    APPS-6692 WIP migrate to TS

--- src/jss.tsx
+++ src/jss.tsx
@@ -12,11 +12,12 @@ import ThemeContext from './theme-context';
 import type {Theme} from './theme';
 import type {Rule} from 'jss';
 
+const affectedBoxSizingProps = ['width', 'min-width', 'max-width', 'height', 'min-height', 'max-height'];
+
 const autoBoxSizing = () => {
-    const affectedCssProps = ['width', 'min-width', 'max-width', 'height', 'min-height', 'max-height'];
     return {
-        onProcessRule(rule: Rule): void {
-            if (rule.style && affectedCssProps.some((cssKey) => cssKey in rule.style)) {
+        onProcessRule(rule: any /* this type should be StyleRule but it is not exposed by JSS */): void {
+            if (rule.style && affectedBoxSizingProps.some((cssKey) => cssKey in rule.style)) {
                 rule.style['box-sizing'] = 'border-box';
             }
         },
@@ -28,50 +29,75 @@ jss.use(ruleValueFunction(), nested(), camelCase(), defaultUnit(), vendorPrefixe
 
 export const getJss = (): any => jss;
 
-type CssClass = {[K: string]: string | number | boolean | CssClass, ...};
-type Sheet = {[K: string]: CssClass, ...};
+type CssClass = {[K: string]: string | number | boolean | CssClass};
+type Sheet = {[K: string]: CssClass};
 
-declare class StyledComponent<P, +WC> extends React.Component<P> {
-    static +WrappedComponent: WC;
-}
+// // $FlowFixMe
+// export const createSheet = <S: Sheet>(s: S): $ObjMap<S, () => string> => s;
 
-/*
+type ObjValuesToStr<O> = {[k in keyof O]: string};
+
+/**
  * This function does nothing, this is just used to make css-in-js autocomplete
  * editor extensions work with JSS. @see https://github.com/ansumanshah/css-in-js
+ *
  */
-// $FlowFixMe
-export const createSheet = <S: Sheet>(s: S): $ObjMap<S, () => string> => s;
+export const createSheet = <S extends Sheet>(sheet: S): ObjValuesToStr<S> =>
+    // @ts-ignore
+    sheet;
 
-type CreateStyledComponent = <P, Comp: React.ComponentType<P>, ST: {[_: $Keys<Comp>]: any, ...}>(
-    Component: Comp
-) => Class<StyledComponent<$Diff<React.ElementConfig<Comp>, {classes: *}>, Comp>> & $Shape<ST>;
+// declare class StyledComponent<P, +WC> extends React.Component<P> {
+//     static +WrappedComponent: WC;
+// }
+// declare class StyledComponent<P, +WC> extends React.Component<P> {
+//     static +WrappedComponent: WC;
+// }
 
-export const withSheet = <S: {...}>(sheet: S): CreateStyledComponent => (Component) => {
+// type CreateStyledComponent = <P, Comp: React.ComponentType<P>, ST: {[_: $Keys<Comp>]: any, ...}>(
+//     Component: Comp
+// ) => Class<StyledComponent<$Diff<React.ElementConfig<Comp>, {classes: *}>, Comp>> & $Shape<ST>;
+
+// export const withSheet = <S: {...}>(sheet: S): CreateStyledComponent => (Component) => {
+//     const StyledComponent = injectSheet(sheet)(Component);
+
+//     if (process.env.STORYBOOK_BUILD) {
+//         // $FlowFixMe proptypes is missing in React.AbstractComponentStatics
+//         StyledComponent.propTypes = Component.propTypes;
+//         if (StyledComponent.propTypes) {
+//             delete StyledComponent.propTypes.sheet;
+//         }
+
+//         // $FlowFixMe
+//         StyledComponent.defaultProps = Component.defaultProps;
+//         StyledComponent.displayName = Component.displayName || Component.name;
+//     }
+//     StyledComponent.WrappedComponent = Component;
+
+//     return StyledComponent;
+// };
+
+export const withSheet = <S extends any>(sheet: S) => <C extends any>(Component: C) => {
     const StyledComponent = injectSheet(sheet)(Component);
-
     if (process.env.STORYBOOK_BUILD) {
-        // $FlowFixMe proptypes is missing in React.AbstractComponentStatics
         StyledComponent.propTypes = Component.propTypes;
         if (StyledComponent.propTypes) {
+            // @ts-ignore - sheet does not exist in propTypes
             delete StyledComponent.propTypes.sheet;
         }
-
-        // $FlowFixMe
         StyledComponent.defaultProps = Component.defaultProps;
         StyledComponent.displayName = Component.displayName || Component.name;
     }
-
+    // @ts-ignore
     StyledComponent.WrappedComponent = Component;
-
     return StyledComponent;
 };
 
-export const removeJssProps = (props: Object): Object => {
+export const removeJssProps = <P extends any>(props: P): Pick<P, Exclude<keyof P, 'sheet' | 'classes'>> => {
     const {sheet, classes, ...withoutJssProps} = props;
     return withoutJssProps;
 };
 
-type CSSValue<P> = void | number | boolean | string | ((P) => void | string | number | boolean);
+type CSSValue<P> = void | number | boolean | string | ((props: P) => void | string | number | boolean);
 type ClassDef<P> = {
     [cssProp: string]:
         | CSSValue<P>
@@ -79,30 +105,29 @@ type ClassDef<P> = {
               [cssProp: string]:
                   | CSSValue<P>
                   | {
-                        [cssProp: string]: CSSValue<P>,
-                        ...
-                    },
-              ...
-          },
-    ...
+                        [cssProp: string]: CSSValue<P>;
+                    };
+          };
 };
-type StylesDef<P> = {[className: string]: ClassDef<P>, ...};
 
-type UseStyles<P, S: StylesDef<any>> = (props: P) => $ObjMap<S, () => string>;
+type StylesDef<P> = {[className: string]: ClassDef<P>};
 
-export const createUseStyles = <P, S: StylesDef<P>>(styles: (theme: Theme) => S): UseStyles<P, S> => {
-    if (styles.length === 0) {
-        // $FlowFixMe
-        styles = styles();
-    }
-    const useStyles = jssCreateUseStyles(styles, {theming: {context: ThemeContext}});
+// type UseStyles<P, S: StylesDef<any>> = (props: P) => $ObjMap<S, () => string>;
+type UseStyles<P, S extends StylesDef<any>> = (props: P) => ObjValuesToStr<S>;
 
-    return (...args) => {
-        try {
-            return useStyles(...args);
-        } catch (err) {
-            err.message = `${err.message} (Did you forget to add <ThemeContextProvider>?)`;
-            throw err;
-        }
-    };
-};
+// export const createUseStyles = <P, S: StylesDef<P>>(styles: (theme: Theme) => S): UseStyles<P, S> => {
+//     if (styles.length === 0) {
+//         // $FlowFixMe
+//         styles = styles();
+//     }
+//     const useStyles = jssCreateUseStyles(styles, {theming: {context: ThemeContext}});
+
+//     return (...args) => {
+//         try {
+//             return useStyles(...args);
+//         } catch (err) {
+//             err.message = `${err.message} (Did you forget to add <ThemeContextProvider>?)`;
+//             throw err;
+//         }
+//     };
+// };
