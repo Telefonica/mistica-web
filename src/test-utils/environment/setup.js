8e4331bd3093955ee9db645b8e32a45a2b5b70cd
Marcos Luis Delgado <mluis@tuenti.com>
2020-06-08 13:21:29 +0200

    APPS-6689 Little mods on scripts

--- src/test-utils/environment/setup.js
+++ src/test-utils/environment/setup.js
@@ -5,8 +5,9 @@ import {join} from 'path';
 export default async (jestConfig = {}) => {
     if (process.argv.includes('--ci')) {
         execSync('yarn storybook-static', {cwd: join(__dirname, '../../../'), stdio: 'inherit'});
+        console.log('Starting storybook server on port 6006');
         global.__STORYBOOK_SERVER__ = exec('http-server -p 6006', {
-            cwd: join(__dirname, '../../../.storybook/dist'),
+            cwd: join(__dirname, '../../../public'),
         });
     }
     await puppeteerSetup(jestConfig);
--- src/test-utils/environment/teardown.js
+++ src/test-utils/environment/teardown.js
@@ -2,6 +2,7 @@ import puppeteerTeardown from 'jest-environment-puppeteer/teardown';
 
 export default async () => {
     if (process.argv.includes('--ci')) {
+        console.log('Stopping storybook server');
         global.__STORYBOOK_SERVER__.kill();
     }
     await puppeteerTeardown();
