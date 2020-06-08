dbd12ab983ae24e9d8cf72fa159045c9925a9973
Abel Toledano <atoledano@tuenti.com>
2020-06-08 17:02:07 +0200

    docs

--- .storybook/theme-selector-addon/themes.js
+++ .storybook/theme-selector-addon/themes.js
@@ -2,17 +2,17 @@
 import type {ThemeConfig} from '../../src';
 
 export const Movistar: ThemeConfig = {
-    i18n: {locale: 'es_ES', phoneNumberFormattingRegionCode: 'ES'},
+    i18n: {locale: 'es-ES', phoneNumberFormattingRegionCode: 'ES'},
     skin: 'Movistar',
 };
 
 export const O2: ThemeConfig = {
-    i18n: {locale: 'en_US', phoneNumberFormattingRegionCode: 'GB'},
+    i18n: {locale: 'en-US', phoneNumberFormattingRegionCode: 'GB'},
     skin: 'O2',
 };
 
 export const Vivo: ThemeConfig = {
-    i18n: {locale: 'pt_BR', phoneNumberFormattingRegionCode: 'BR'},
+    i18n: {locale: 'pt-BR', phoneNumberFormattingRegionCode: 'BR'},
     skin: 'Vivo',
 };
 
