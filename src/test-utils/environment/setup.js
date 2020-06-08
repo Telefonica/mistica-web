20b1c77af10602439f6b45da3dc65b8152bb1fe8
Marcos Luis Delgado <mluis@tuenti.com>
2020-06-08 14:22:15 +0200

    APPS-6689 Added newline on logs

--- src/test-utils/environment/setup.js
+++ src/test-utils/environment/setup.js
@@ -5,7 +5,7 @@ import {join} from 'path';
 export default async (jestConfig = {}) => {
     if (process.argv.includes('--ci')) {
         execSync('yarn storybook-static --quiet', {cwd: join(__dirname, '../../../'), stdio: 'inherit'});
-        console.log('Starting storybook server on port 6006');
+        console.log('\nStarting storybook server on port 6006');
         global.__STORYBOOK_SERVER__ = exec('http-server -p 6006', {
             cwd: join(__dirname, '../../../public'),
         });
--- src/test-utils/environment/teardown.js
+++ src/test-utils/environment/teardown.js
@@ -2,7 +2,7 @@ import puppeteerTeardown from 'jest-environment-puppeteer/teardown';
 
 export default async () => {
     if (process.argv.includes('--ci')) {
-        console.log('Stopping storybook server');
+        console.log('\nStopping storybook server');
         global.__STORYBOOK_SERVER__.kill();
     }
     await puppeteerTeardown();
