5d05acaf4a54680e9ed7731a62f3b19dcc6e7e86
Abel Toledano <atoledano@tuenti.com>
2020-06-15 13:58:06 +0200

    fix message

--- scripts/watch.js
+++ scripts/watch.js
@@ -6,7 +6,7 @@ const babelProcess = spawn('yarn', ['build', '--watch']);
 babelProcess.stdout.on('data', (data) => {
     const strOut = `${data}`;
     if (strOut.includes('Successfully compiled')) {
-        console.log('Babel compied');
+        console.log('Babel compiled');
         // flow copy sources
         spawn('yarn', ['postbuild'], {stdio: 'inherit'});
     }
