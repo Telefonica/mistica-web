2465a49ffe4bbda4acde8c685f0a8358396f437e
Abel Toledano <atoledano@tuenti.com>
2020-06-15 10:57:49 +0200

    some changes in watcch scrip

--- scripts/watch.js
+++ scripts/watch.js
@@ -2,14 +2,12 @@
 const {spawn} = require('child_process');
 
 const babelProcess = spawn('yarn', ['build', '--watch']);
-babelProcess.on('message', (m) => {
-    console.log('new message', m);
-});
 
 babelProcess.stdout.on('data', (data) => {
     const strOut = `${data}`;
     if (strOut.includes('Successfully compiled')) {
         console.log('Babel compied');
+        // flow copy sources
         spawn('yarn', ['postbuild'], {stdio: 'inherit'});
     }
 });
