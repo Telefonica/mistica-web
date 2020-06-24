8de6793d1a287172995788033d8464f0445bd798
Abel Toledano <atoledano@tuenti.com>
2020-06-24 18:17:50 +0200

    APPS-6770 more ios themes

--- playroom/themes.js
+++ playroom/themes.js
@@ -4,3 +4,5 @@ import {Movistar, O2, Vivo} from '../.storybook/theme-selector-addon/themes';
 export {Movistar, O2, Vivo};
 
 export const Movistar_iOS = {...Movistar, platformOverrides: {platform: 'ios'}};
+export const O2_iOS = {...O2, platformOverrides: {platform: 'ios'}};
+export const Vivo_iOS = {...Vivo, platformOverrides: {platform: 'ios'}};
