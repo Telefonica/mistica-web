6eca4705fa2e48fd6d5a2b18becd3842a130f5b6
Abel Toledano <atoledano@tuenti.com>
2020-06-25 15:16:14 +0200

    APPS-6770 gen flow defs after migrating to TS

--- playroom/themes.tsx
+++ playroom/themes.tsx
@@ -1,6 +1,7 @@
 // @ts-expect-error importing from plain js
-export {Movistar, O2, Vivo, O2_Classic} from '../.storybook/theme-selector-addon/themes';
+import {Movistar, O2, Vivo, O2_Classic} from '../.storybook/theme-selector-addon/themes';
 
+export {Movistar, O2, Vivo, O2_Classic};
 export const Movistar_iOS = {...Movistar, platformOverrides: {platform: 'ios'}};
 export const O2_iOS = {...O2, platformOverrides: {platform: 'ios'}};
 export const O2_Classic_iOS = {...O2_Classic, platformOverrides: {platform: 'ios'}};
