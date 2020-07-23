53f8666d23dcd7bbac097bb5a4684c110f809a2e
Roberto Vidal <jrvidal@tuenti.com>
2020-07-23 08:45:12 +0200

    Rewrite without using extra state

--- src/portal.tsx
+++ src/portal.tsx
@@ -16,35 +16,36 @@ type Props = {
 };
 
 const Portal: React.FC<Props> = ({children, className}) => {
-    const rootElemRef: React.MutableRefObject<HTMLElement | null> = React.useRef(null);
-    const [, setIsReady] = React.useState(false);
+    const rootElemRef: React.MutableRefObject<HTMLElement> = React.useRef(null as any);
+
+    if (rootElemRef.current === null) {
+        rootElemRef.current = document.createElement('div');
+    }
 
     React.useEffect(() => {
-        const divRef = document.createElement('div');
-        rootElemRef.current = divRef;
         const modalRoot = document.body;
 
-        modalRoot.appendChild(divRef);
-        setIsReady(true);
+        modalRoot.appendChild(rootElemRef.current);
 
         return () => {
-            modalRoot.removeChild(divRef);
+            modalRoot.removeChild(rootElemRef.current);
         };
     }, []);
 
     React.useEffect(() => {
-        const divRef = rootElemRef.current;
-        if (divRef && className) {
-            divRef.classList.add(className);
+        const divContainer = rootElemRef.current;
+
+        if (className) {
+            divContainer.classList.add(className);
         }
 
         return () => {
-            if (divRef && className) {
-                divRef.classList.remove(className);
+            if (className) {
+                divContainer.classList.remove(className);
             }
         };
     }, [className]);
 
-    return rootElemRef.current && ReactDOM.createPortal(children, rootElemRef.current);
+    return ReactDOM.createPortal(children, rootElemRef.current);
 };
 export default Portal;
