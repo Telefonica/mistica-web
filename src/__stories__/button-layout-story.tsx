42f522bc228e08011c9fb4749c58f89a23ce0361
Abel Toledano <atoledano@tuenti.com>
2020-08-05 11:13:41 +0200

    revert self-hosted gh actions (#75)

--- src/__stories__/button-layout-story.tsx
+++ src/__stories__/button-layout-story.tsx
@@ -34,62 +34,64 @@ export const Default: StoryComponent = () => (
             </pre>
             <p>2. Adding `overflow-x: hidden;` to the parent.</p>
         </StorySection>
-        <StorySection title="ButtonLayout">
-            <ButtonLayout>
-                <ButtonSecondary fake>ok</ButtonSecondary>
-                <ButtonPrimary fake>long long long long</ButtonPrimary>
-            </ButtonLayout>
-            <ButtonLayout>
-                <ButtonSecondary fake>ok</ButtonSecondary>
-                <ButtonPrimary fake>long long long long</ButtonPrimary>
-            </ButtonLayout>
-        </StorySection>
+        <div data-testid="screenshot">
+            <StorySection title="ButtonLayout">
+                <ButtonLayout>
+                    <ButtonSecondary fake>ok</ButtonSecondary>
+                    <ButtonPrimary fake>long long long long</ButtonPrimary>
+                </ButtonLayout>
+                <ButtonLayout>
+                    <ButtonSecondary fake>ok</ButtonSecondary>
+                    <ButtonPrimary fake>long long long long</ButtonPrimary>
+                </ButtonLayout>
+            </StorySection>
 
-        <StorySection title="ButtonLayout (centered)">
-            <ButtonLayout align="center">
-                <ButtonSecondary fake>ok</ButtonSecondary>
-                <ButtonPrimary fake>long long long long</ButtonPrimary>
-            </ButtonLayout>
-        </StorySection>
+            <StorySection title="ButtonLayout (centered)">
+                <ButtonLayout align="center">
+                    <ButtonSecondary fake>ok</ButtonSecondary>
+                    <ButtonPrimary fake>long long long long</ButtonPrimary>
+                </ButtonLayout>
+            </StorySection>
 
-        <StorySection title="One button (default)">
-            <ButtonLayout>
-                <ButtonPrimary fake>Text</ButtonPrimary>
-            </ButtonLayout>
-        </StorySection>
+            <StorySection title="One button (default)">
+                <ButtonLayout>
+                    <ButtonPrimary fake>Text</ButtonPrimary>
+                </ButtonLayout>
+            </StorySection>
 
-        <StorySection title="Two buttons (default)">
-            <ButtonLayout>
-                <ButtonPrimary fake>Text </ButtonPrimary>
-                <ButtonSecondary fake>Text</ButtonSecondary>
-            </ButtonLayout>
-        </StorySection>
+            <StorySection title="Two buttons (default)">
+                <ButtonLayout>
+                    <ButtonPrimary fake>Text </ButtonPrimary>
+                    <ButtonSecondary fake>Text</ButtonSecondary>
+                </ButtonLayout>
+            </StorySection>
 
-        <StorySection title="One button (centered)">
-            <ButtonLayout align="center">
-                <ButtonPrimary fake>Text</ButtonPrimary>
-            </ButtonLayout>
-        </StorySection>
+            <StorySection title="One button (centered)">
+                <ButtonLayout align="center">
+                    <ButtonPrimary fake>Text</ButtonPrimary>
+                </ButtonLayout>
+            </StorySection>
 
-        <StorySection title="Two buttons (centered)">
-            <ButtonLayout align="center">
-                <ButtonPrimary fake>Text</ButtonPrimary>
-                <ButtonSecondary fake>Text</ButtonSecondary>
-            </ButtonLayout>
-        </StorySection>
+            <StorySection title="Two buttons (centered)">
+                <ButtonLayout align="center">
+                    <ButtonPrimary fake>Text</ButtonPrimary>
+                    <ButtonSecondary fake>Text</ButtonSecondary>
+                </ButtonLayout>
+            </StorySection>
 
-        <StorySection title="One button (centered) with link">
-            <ButtonLayout align="center" link={<ButtonLink href={' '}>Text link</ButtonLink>}>
-                <ButtonPrimary fake>Text</ButtonPrimary>
-            </ButtonLayout>
-        </StorySection>
+            <StorySection title="One button (centered) with link">
+                <ButtonLayout align="center" link={<ButtonLink href={' '}>Text link</ButtonLink>}>
+                    <ButtonPrimary fake>Text</ButtonPrimary>
+                </ButtonLayout>
+            </StorySection>
 
-        <StorySection title="Two buttons (centered) with link">
-            <ButtonLayout align="center" link={<ButtonLink href={' '}>Text link</ButtonLink>}>
-                <ButtonPrimary fake>Text</ButtonPrimary>
-                <ButtonSecondary fake>Text</ButtonSecondary>
-            </ButtonLayout>
-        </StorySection>
+            <StorySection title="Two buttons (centered) with link">
+                <ButtonLayout align="center" link={<ButtonLink href={' '}>Text link</ButtonLink>}>
+                    <ButtonPrimary fake>Text</ButtonPrimary>
+                    <ButtonSecondary fake>Text</ButtonSecondary>
+                </ButtonLayout>
+            </StorySection>
+        </div>
     </>
 );
 
