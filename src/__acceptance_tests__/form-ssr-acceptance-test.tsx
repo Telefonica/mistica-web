3cd145a32ee55cc1100ebff871802aabf8f3792b
Marcos Luis Delgado <mluis@tuenti.com>
2020-08-26 16:33:57 +0200

    APPS-7048 Using test.each

--- src/__acceptance_tests__/form-ssr-acceptance-test.tsx
+++ src/__acceptance_tests__/form-ssr-acceptance-test.tsx
@@ -1,9 +1,7 @@
 import {openSSRPage} from '../test-utils';
 
-test('ssr button on tablet', async () => {
-    await openSSRPage({name: 'form', device: 'TABLET'});
-});
+import type {Device} from '../test-utils';
 
-test('ssr button on mobile ios', async () => {
-    await openSSRPage({name: 'form', device: 'MOBILE_IOS'});
+test.each(['TABLET', 'MOBILE_IOS'] as Array<Device>)('ssr button on tablet %s', async (device) => {
+    await openSSRPage({name: 'form', device});
 });
