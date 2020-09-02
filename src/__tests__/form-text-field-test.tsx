ed5be6b8c35c9c426b2bfe33b18c4dbd9f8b12f9
Marcos Luis Delgado <mluis@tuenti.com>
2020-09-02 15:09:57 +0200

    APPS-7048 Fixed tests not working without ThemeContextProvider

--- src/__tests__/form-text-field-test.tsx
+++ src/__tests__/form-text-field-test.tsx
@@ -2,9 +2,15 @@ import * as React from 'react';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FormTextField from '../form-text-field';
+import ThemeContextProvider from '../theme-context-provider';
+import {overrideTheme} from './utils';
 
 test('FormTextField uncontrolled', async () => {
-    render(<FormTextField label="Username" name="username" />);
+    render(
+        <ThemeContextProvider theme={overrideTheme({})}>
+            <FormTextField label="Username" name="username" />
+        </ThemeContextProvider>
+    );
 
     const field = screen.getByLabelText('Username');
     await userEvent.type(field, 'pepito');
@@ -19,7 +25,11 @@ test('FormTextField controlled', async () => {
         return <FormTextField label="Username" name="username" value={value} onChangeValue={setValue} />;
     };
 
-    render(<Component />);
+    render(
+        <ThemeContextProvider theme={overrideTheme({})}>
+            <Component />
+        </ThemeContextProvider>
+    );
 
     const field = screen.getByLabelText('Username');
     await userEvent.type(field, 'pepito');
