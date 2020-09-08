9ecda8516abd61c86958c29a50f43fad8bc4f7d0
Abel Toledano <atoledano@tuenti.com>
2020-09-08 14:28:20 +0200

    move npm_token override to prepublish-only script

--- scripts/prepublish-only.js
+++ scripts/prepublish-only.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 if (!process.env.CI) {
     const lines = [
         '',
@@ -13,3 +16,20 @@ if (!process.env.CI) {
     console.error(lines.join('\n'));
     process.exit(1);
 }
+
+if (!process.env.NPM_TOKEN) {
+    console.error('NPM_TOKEN env var not set');
+    process.exit(1);
+}
+
+const lines = [
+    // set npm auth token with publish permission from environment
+    `//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN || ''}`,
+
+    // this allows to execute npm lifecycle scripts by root
+    'unsafe-perm = true',
+];
+
+fs.writeFileSync(path.join(__dirname, '..', '.npmrc'), lines.join('\n'));
+
+require('./build');
