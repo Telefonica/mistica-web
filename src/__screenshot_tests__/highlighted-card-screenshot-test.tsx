13ce6f75446182c3a2c84045b123f363d2d8a749
Pol Escolar <pol@tuenti.com>
2020-09-15 10:40:56 +0200

    APPS-7116 Updated screenshot test with text component

--- src/__screenshot_tests__/highlighted-card-screenshot-test.tsx
+++ src/__screenshot_tests__/highlighted-card-screenshot-test.tsx
@@ -1,9 +1,14 @@
 import {openStoryPage, screen} from '../test-utils';
 
-test('HighlightedCard common component screenshot', async () => {
+import type {Device} from '../test-utils';
+
+const TESTABLE_DEVICES: Array<Device> = ['MOBILE_IOS', 'DESKTOP'];
+
+test.each(TESTABLE_DEVICES)('HighlightedCard in %s', async (device) => {
     await openStoryPage({
         section: 'Components|Cards/HighlightedCard',
         name: 'HighlightedCard',
+        device,
     });
 
     const highlightedCard = await screen.findByTestId('highlighted-card');
