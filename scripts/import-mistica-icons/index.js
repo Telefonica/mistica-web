4195f417fca290c00ac4bd51989031806ba50407
Pedro Ladaria <pladaria@tuenti.com>
2020-09-17 11:38:49 +0200

    APPS-7074 generator result

--- scripts/import-mistica-icons/index.js
+++ scripts/import-mistica-icons/index.js
@@ -4,14 +4,20 @@
 
 const fs = require('fs');
 const mkdirp = require('mkdirp');
-const {join} = require('path');
+const {join, basename} = require('path');
 const {execSync} = require('child_process');
 const svgr = require('@svgr/core').default;
 const prettier = require('prettier');
+const {camelCase, upperFirst} = require('lodash');
+const {yellow, green} = require('colors/safe');
+
+const pascalCase = (s) => upperFirst(camelCase(s));
 
 const PATH_REPO_ROOT = join(__dirname, '..', '..');
 const PATH_CACHE = join(PATH_REPO_ROOT, 'node_modules', '.cache');
 const PATH_MISTICA_ICONS_REPO = join(PATH_CACHE, 'mistica-icons');
+const PATH_OUTPUT = join(PATH_REPO_ROOT, 'src', 'generated');
+const PATH_OUTPUT_FILENAME = join(PATH_OUTPUT, 'mistica-icons.tsx');
 
 const GIT_MISTICA_ICONS_MAIN_BRANCH = 'production';
 const GIT_MISTICA_ICONS = 'git@github.com:Telefonica/mistica-icons.git';
@@ -66,29 +72,67 @@ const createIconComponentSource = async (svgSource, componentName) => {
     // get icon size, needed to set the viewBox
     const svgSize = generated.match(/(width|height)={(\d+)}/)[2];
 
-    const js = generated
-        // set svg props
-        .replace(/<svg .*?>/, `<svg width={size} height={size} viewBox="0 0 ${svgSize} ${svgSize}">`)
-        // set fill color
-        .replace(/fill="#\d+"/g, 'fill={fillColor}')
-        // add space before type definition
-        .replace(/^type /gm, '\ntype ');
-
-    return prettier.format(js, {
-        ...(await prettier.resolveConfig('.')),
-        parser: 'babel-ts',
-    });
+    return (
+        generated
+            // set svg props
+            .replace(/<svg .*?>/, `<svg width={size} height={size} viewBox="0 0 ${svgSize} ${svgSize}">`)
+            // set fill color
+            .replace(/fill="#\d+"/g, 'fill={fillColor}')
+            // add space before type definition
+            .replace(/^type /gm, '\ntype ')
+            // add component type, for some reason it gets stripped from the template
+            .replace(/\s*=\s*\({\s*color,/m, ': React.FC<Props> = ({color,')
+    );
 };
 
 const main = async () => {
     checkoutMisticaIconsRepo();
     const svgFilenames = getSvgIconFilenames();
-    console.log(svgFilenames[0]);
 
-    const svgCode = `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21.018 18.531h-1.062c0-.411.327-1.072.49-1.072.164 0 .572.577.572 1.072zm-3.345-2.885c0 .33-.244.577-.49.577-1.387.247-3.345.824-4.895 2.062a.975.975 0 01-1.142 0c-1.47-1.235-3.508-1.812-4.895-2.062-.327-.08-.49-.33-.49-.577v-3.543l5.304 2.474a1.2 1.2 0 001.062 0l5.463-2.558.083 3.627zM3.068 9.63c0-.246.08-.412.327-.496l8.078-3.708c.164-.081.244-.081.408 0l8.077 3.708c.097.05.08.082.08.082h-8.485c-.244 0-.407.165-.407.411 0 .247.163.412.407.412h8.322l-8.078 3.711c-.163.081-.244.081-.407 0l-8.078-3.708a.45.45 0 01-.244-.412zm17.87 7.09v-6.678c.013-.518.05-1.373-.652-1.647l-8.08-3.708c-.409-.166-.736-.166-1.143-.082l-8.078 3.79c-.491.165-.735.658-.735 1.235 0 .577.327.989.735 1.235l1.96.908v3.873c0 .743.491 1.32 1.143 1.485 1.306.246 3.183.742 4.487 1.896.327.247.651.412 1.062.412.407 0 .734-.165 1.062-.412 1.386-1.073 3.183-1.647 4.486-1.896a1.51 1.51 0 001.143-1.485v-3.954l1.714-.743v5.77c-.652.247-.98 1.235-.98 1.812 0 .247.081.496.245.659.244.246.571.246.898.246h.491c.244 0 .48-.059.735-.246.186-.138.255-.236.286-.659.036-.607-.208-1.4-.78-1.812z" fill="#313235" fill-rule="nonzero"/></svg>`;
+    let result = `
+        /*
+         * This file is autogeterated. Don't edit this file!
+         *
+         * To update, execute "yarn start" inside "scripts/import-mistica-icons"
+         */
 
-    const source = await createIconComponentSource(svgCode, 'Component');
-    console.log(source);
+        import * as React from 'react';
+        import {useIsInverseVariant, useTheme} from '..';
+
+        type Props = {
+            color?: string;
+            size?: number;
+        };
+
+    `;
+
+    const components = [];
+
+    for (const svgFilename of svgFilenames) {
+        const svgCode = fs.readFileSync(svgFilename, 'utf-8');
+        const componentName = pascalCase(basename(svgFilename, '.svg'));
+
+        console.log(yellow(basename(svgFilename)), '=>', green(componentName));
+
+        components.push([componentName, await createIconComponentSource(svgCode, componentName)]);
+    }
+
+    result += components
+        .sort((a, b) => (a[0].toLowerCase() > b[0].toLowerCase() ? 1 : -1))
+        .map(([, source]) => source)
+        .join('\n\n');
+
+    const formatted = prettier.format(result, {
+        ...(await prettier.resolveConfig('.')),
+        parser: 'babel-ts',
+    });
+
+    mkdirp.sync(PATH_OUTPUT);
+    fs.writeFileSync(PATH_OUTPUT_FILENAME, formatted, 'utf8');
+
+    console.log();
+    console.log('Done!', yellow(PATH_OUTPUT_FILENAME));
+    console.log();
 };
 
 main();
--- scripts/import-mistica-icons/template.js
+++ scripts/import-mistica-icons/template.js
@@ -1,4 +1,8 @@
-const template = ({template}, opts, {imports, interfaces, componentName, /* props, */ jsx, exports}) => {
+const template = (
+    {template},
+    opts,
+    {/*imports, interfaces,*/ componentName, /* props, */ jsx /* exports */}
+) => {
     const plugins = ['jsx'];
 
     if (opts.typescript) {
@@ -6,25 +10,14 @@ const template = ({template}, opts, {imports, interfaces, componentName, /* prop
     }
 
     const typeScriptTemplate = template.smart({plugins});
-    return typeScriptTemplate.ast`${imports}
-        import {useIsInverseVariant, useTheme} from '../..';
-
-        ${interfaces}
-
-        type Props = {
-            color?: string,
-            size?: number,
-        }
-
-        const ${componentName}: React.FC<Props> = ({color, size}) => {
+    return typeScriptTemplate.ast`
+        export const ${componentName}: React.FC<Props> = ({color, size}) => {
             const {colors} = useTheme();
             const isInverse = useIsInverseVariant();
             const fillColor = color ?? (isInverse ? colors.iconInverse : colors.iconPrimary);
 
             return ${jsx};
         }
-
-        ${exports}
     `;
 };
 
