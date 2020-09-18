17e4096857453df7cb80ca31bcd56599e75adf16
Pedro Ladaria <pladaria@tuenti.com>
2020-09-18 13:00:20 +0200

    APPS-7074 improve story

--- src/icons/__stories__/mistica-icons-story.tsx
+++ src/icons/__stories__/mistica-icons-story.tsx
@@ -1,23 +1,35 @@
 import * as React from 'react';
 import * as misticaIcons from '../../generated/mistica-icons';
 import {useCheckbox} from '../../__stories__/helpers';
-import {ThemeVariant, useTheme, Box, Stack, FormSearchField, Inline, DoubleField} from '../..';
+import {ThemeVariant, useTheme, Box, Stack, FormSearchField, Inline, DoubleField, Text} from '../..';
 import FormIntegerField from '../../form-integer-field';
+import {kebabCase, upperFirst} from 'lodash';
 
 export default {
     title: 'Icons|Mistica Icons',
 };
 
-export const Catalog: StoryComponent = () => {
+export const Catalog: React.FC = () => {
     const [showRegular, regularCheckbox] = useCheckbox('Regular', true);
     const [showLight, lightCheckbox] = useCheckbox('Light', true);
     const [showFilled, filledCheckbox] = useCheckbox('Filled', true);
     const [isInverse, inverseCheckbox] = useCheckbox('Inverse', false);
-    const [showNames, showNamesCheckbox] = useCheckbox('Show names', false);
+    const [showNames, showNamesCheckbox] = useCheckbox('Show names', true);
     const [showIConBackground, showIConBackgroundCheckbox] = useCheckbox('Show background', false);
     const [filter, setFilter] = React.useState('');
     const [size, setSize] = React.useState(64);
     const {colors} = useTheme();
+
+    const getRealName = (name: string) => name.replace(/^Icon/, '').replace(/(Regular|Filled|Light)$/, '');
+    const getTypeSortValue = (name: string) => {
+        if (name.endsWith('Regular')) {
+            return 0;
+        }
+        if (name.endsWith('Light')) {
+            return 1;
+        }
+        return 2;
+    };
 
     const filterIcon = (name: string): boolean => {
         if (!showFilled && name.endsWith('Filled')) {
@@ -30,17 +42,37 @@ export const Catalog: StoryComponent = () => {
             return false;
         }
 
-        const realName = name.replace(/^Icon/, '').replace(/(Regular|Filled|Light)$/, '');
-
-        if (filter && !realName.toLowerCase().includes(filter.toLocaleLowerCase())) {
+        if (filter && !getRealName(name).toLowerCase().includes(filter.toLocaleLowerCase())) {
             return false;
         }
 
         return true;
     };
 
+    const compareNames = (a: string, b: string): number => {
+        const realA = getRealName(a);
+        const realB = getRealName(b);
+        if (realA < realB) {
+            return -1;
+        }
+        if (realA > realB) {
+            return 1;
+        }
+        return getTypeSortValue(a) < getTypeSortValue(b) ? -1 : 1;
+    };
+
+    const breakName = (name: string) =>
+        kebabCase(name)
+            .split('-')
+            .map(upperFirst)
+            .flatMap((part, i) => (
+                <span key={i}>
+                    {part}
+                    <wbr />
+                </span>
+            ));
+
     const backgroundColor = isInverse ? colors.backgroundBrand : colors.background;
-    const textColor = isInverse ? colors.textPrimaryInverse : colors.textPrimary;
     const iconBackgroundColor = showIConBackground ? '#aaa' : 'none';
 
     return (
@@ -63,7 +95,7 @@ export const Catalog: StoryComponent = () => {
                     </DoubleField>
                     <Stack space={16}>
                         <>
-                            <b>Type:</b> {filledCheckbox} {lightCheckbox} {regularCheckbox}
+                            {regularCheckbox} {lightCheckbox} {filledCheckbox}
                         </>
                         <>
                             {inverseCheckbox} {showNamesCheckbox} {showIConBackgroundCheckbox}
@@ -76,9 +108,17 @@ export const Catalog: StoryComponent = () => {
                 <div style={{background: backgroundColor}}>
                     {Object.entries(misticaIcons)
                         .filter(([name]) => filterIcon(name))
+                        .sort((a, b) => compareNames(a[0], b[0]))
                         .map(([name, Icon]) => (
-                            <div style={{display: 'inline-block', textAlign: 'center'}}>
-                                <div style={{textAlign: 'center', padding: 16}}>
+                            <div style={{display: 'inline-block', verticalAlign: 'top', textAlign: 'center'}}>
+                                <div
+                                    style={{
+                                        textAlign: 'center',
+                                        padding: 16,
+                                        width: size,
+                                        minWidth: showNames ? 100 : 0,
+                                    }}
+                                >
                                     <div
                                         style={{
                                             width: size,
@@ -89,7 +129,11 @@ export const Catalog: StoryComponent = () => {
                                     >
                                         <Icon size={size} />
                                     </div>
-                                    {showNames && <div style={{color: textColor, marginTop: 8}}>{name}</div>}
+                                    {showNames && (
+                                        <Box paddingTop={8}>
+                                            <Text size={13}>{breakName(name)}</Text>
+                                        </Box>
+                                    )}
                                 </div>
                             </div>
                         ))}
