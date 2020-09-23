f36752147c7e251c19039524d07f7c8ad407f70e
Pedro Ladaria <pladaria@tuenti.com>
2020-09-23 13:07:15 +0200

    fix(Version): improve version import from package.json

--- src/package-version.tsx
+++ src/package-version.tsx
@@ -1,3 +1,3 @@
-import packageJson from '../package.json';
+import {version} from '../package.json';
 
-export const PACKAGE_VERSION: string = packageJson.version;
+export const PACKAGE_VERSION: string = version;
