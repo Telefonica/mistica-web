f48f8a260ba7b8937d4fd9587ec8e72800db0ad6
Pedro Ladaria <pladaria@tuenti.com>
2020-09-24 19:41:28 +0200

    APPS-7181 update webpack config

--- scripts/size-stats/app-test-lib-overhead/webpack.config.js
+++ scripts/size-stats/app-test-lib-overhead/webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 const config = {
     entry: './src/index.js',
     // entry: './src/index-baseline.js', // uncomment to generate the baseline
-    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
