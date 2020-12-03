0df7ee4fef689c99910f1def932bc2c97bb9d1e0
Pol Escolar <pol@tuenti.com>
2020-12-03 10:31:34 +0100

    APPS-7351 Added new english texts for playroom theme

--- playroom/themes.tsx
+++ playroom/themes.tsx
@@ -1,16 +1,43 @@
-import {mediaQueriesConfig} from '../src/theme';
+import {mediaQueriesConfig, texts as textsTheme} from '../src/theme';
 import * as themes from '../.storybook/themes';
+import type {ThemeConfig} from '../src/theme';
 
 // Override the media query desktopOrTabletMinHeight to avoid to show the components mobile version
 // when playroom height is too short.
 const mediaQueries = {...mediaQueriesConfig, desktopOrTabletMinHeight: 0};
 
-export const Movistar = {...themes.Movistar, mediaQueries};
-export const Vivo = {...themes.Vivo, mediaQueries};
-export const O2 = {...themes.O2, mediaQueries};
-export const O2_Classic = {...themes.O2_Classic, mediaQueries};
+// Override texts
+const texts = {
+    ...textsTheme,
+    expirationDatePlaceholder: 'MM/YY',
+    togglePasswordVisibilityLabel: 'Show or hide password',
+    loading: 'Loading',
+    linkOpensInNewTab: 'Opens in a new window',
+    modalClose: 'Close',
+    dialogCancelButton: 'Cancel',
+    dialogAcceptButton: 'Accept',
+    formFieldOptionalLabelSuffix: 'optional',
+    formFieldErrorIsMandatory: 'Mandatory field',
+    formCreditCardNumberLabel: 'Credit card number',
+    formCreditCardExpirationLabel: 'Expiration',
+    formCreditCardCvvLabel: 'CVV',
+    formCreditCardCvvError: 'Wrong CVV',
+    formCreditCardCvvTooltipVisaMcButton: 'Show help CVV',
+    formCreditCardCvvTooltipVisaMc: 'The CVV code is the 3 last digits from the back of your card',
+    formCreditCardCvvTooltipAmex: 'If it is an American Express, add the 4 digits from the back of your card',
+    formCreditCardExpirationError: 'Invalid date',
+    formCreditCardNumberError: 'Card number not valid',
+    formEmailError: 'Wrong email',
+    closeButtonLabel: 'Close',
+    formSearchClear: 'Clear search',
+};
 
-export const Movistar_iOS = {...Movistar, platformOverrides: {platform: 'ios'}};
-export const Vivo_iOS = {...Vivo, platformOverrides: {platform: 'ios'}};
-export const O2_iOS = {...O2, platformOverrides: {platform: 'ios'}};
-export const O2_Classic_iOS = {...O2_Classic, platformOverrides: {platform: 'ios'}};
+export const Movistar: ThemeConfig = {...themes.Movistar, mediaQueries, texts};
+export const Vivo: ThemeConfig = {...themes.Vivo, mediaQueries, texts};
+export const O2: ThemeConfig = {...themes.O2, mediaQueries, texts};
+export const O2_Classic: ThemeConfig = {...themes.O2_Classic, mediaQueries, texts};
+
+export const Movistar_iOS: ThemeConfig = {...Movistar, platformOverrides: {platform: 'ios'}};
+export const Vivo_iOS: ThemeConfig = {...Vivo, platformOverrides: {platform: 'ios'}};
+export const O2_iOS: ThemeConfig = {...O2, platformOverrides: {platform: 'ios'}};
+export const O2_Classic_iOS: ThemeConfig = {...O2_Classic, platformOverrides: {platform: 'ios'}};
