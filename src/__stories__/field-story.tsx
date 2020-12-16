65ce3da1f9e1853a1334ee00709e4d5b5f440700
Pedro Ladaria <pladaria@tuenti.com>
2020-12-16 17:43:47 +0100

    APPS-7663 organize storyies to avoid duplicated title warnings

--- src/__stories__/field-story.tsx
+++ src/__stories__/field-story.tsx
@@ -16,10 +16,15 @@ import {
     Box,
     DateTimeField,
     Text8,
+    ButtonPrimary,
+    Form,
+    SectionTitle,
+    Stack,
 } from '..';
 import {inspect} from 'util';
 import Icon from '../icons/icon-visibility';
 import {StorySection, countriesList, phoneNumbersList} from './helpers';
+import {getLocalDateString, getLocalDateTimeString} from '../utils/time';
 
 export default {
     title: 'Components/Forms/Fields',
@@ -582,3 +587,46 @@ export const TypesControlled = (): React.ReactNode => (
 );
 
 TypesControlled.storyName = 'Types (controlled)';
+
+const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+
+export const DateTimeLimits: StoryComponent = () => {
+    const min = new Date(Date.now() - 7 * ONE_DAY_IN_MS);
+    const max = new Date(Date.now() + 7 * ONE_DAY_IN_MS);
+    const [dateValue, setDateValue] = React.useState('');
+    const [dateTimeValue, setDateTimeValue] = React.useState('');
+
+    return (
+        <Form onSubmit={() => alert({message: 'Success!'})}>
+            <SectionTitle>Date Field with range</SectionTitle>
+            <Stack space={16}>
+                <DateField
+                    name="date"
+                    label="Date with limits"
+                    min={min}
+                    max={max}
+                    helperText={`min: ${getLocalDateString(min)} / max: ${getLocalDateString(max)}`}
+                    onChangeValue={setDateValue}
+                />
+                <Text8 regular>Selected value: {dateValue}</Text8>
+            </Stack>
+
+            <SectionTitle>DateTime Field with range</SectionTitle>
+            <Stack space={16}>
+                <DateTimeField
+                    name="datetime"
+                    label="DateTime with limits"
+                    min={min}
+                    max={max}
+                    helperText={`min: ${getLocalDateTimeString(min)} / max: ${getLocalDateTimeString(max)}`}
+                    onChangeValue={setDateTimeValue}
+                />
+                <Text8 regular>Selected value: {dateTimeValue}</Text8>
+            </Stack>
+
+            <Box paddingTop={32}>
+                <ButtonPrimary submit>Validate</ButtonPrimary>
+            </Box>
+        </Form>
+    );
+};
---
+++ src/__stories__/form-example-automatic-story.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import {fruitEntries, countriesList} from './helpers';
+import {
+    Stack,
+    ButtonPrimary,
+    ButtonSecondary,
+    Form,
+    CreditCardFields,
+    EmailField,
+    Select,
+    ButtonLayout,
+    PhoneNumberField,
+    IntegerField,
+    DecimalField,
+    Switch,
+    Text7,
+    Checkbox,
+    Inline,
+} from '..';
+
+export default {
+    title: 'Components/Forms/Example Automatic',
+};
+
+const fakeApiCall = (data: any): Promise<void> =>
+    new Promise((r) =>
+        setTimeout(() => {
+            r();
+            window.alert(JSON.stringify(data, null, 2));
+        }, 5000)
+    );
+
+const fruitOptions = fruitEntries.map(([text, value]) => ({text, value}));
+const countryOptions = countriesList.map((text, i) => ({text, value: '' + i}));
+
+export const AutomaticForm: StoryComponent = () => {
+    return (
+        <Form
+            initialValues={{email: 'john.doe@gmail.com', decimal: '123', 'save-cc': true}}
+            onSubmit={fakeApiCall}
+            autoJump
+        >
+            <Stack space={16}>
+                <EmailField name="email" label="email" />
+
+                <PhoneNumberField name="phone" label="phone" />
+
+                <DecimalField name="decimal" label="Decimal" />
+
+                <IntegerField optional autoComplete="off" name="integer" label="Integer" />
+
+                <Select name="country" label="country" options={countryOptions} />
+
+                <Select autoFocus name="fruit" label="fruit" options={fruitOptions} />
+
+                <CreditCardFields />
+
+                <Switch
+                    name="save-cc"
+                    render={(switchElement) => (
+                        <Inline alignItems="center" space={8}>
+                            {switchElement}
+                            <Text7 regular>Save CC</Text7>
+                        </Inline>
+                    )}
+                />
+
+                <Checkbox
+                    name="t&c"
+                    render={(checkboxElement) => (
+                        <Inline alignItems="center" space={8}>
+                            {checkboxElement}
+                            <Text7 regular>Accept Terms and Conditions</Text7>
+                        </Inline>
+                    )}
+                />
+
+                <ButtonLayout>
+                    <ButtonSecondary onPress={() => window.alert('hello')}>Hello</ButtonSecondary>
+                    <ButtonPrimary submit loadingText="Sending">
+                        Send
+                    </ButtonPrimary>
+                </ButtonLayout>
+            </Stack>
+        </Form>
+    );
+};
