85a28913646a6cfff6d6cb836ae8274ddbb1f312
Pedro Ladaria <pladaria@tuenti.com>
2020-12-18 10:55:47 +0100

    update action

--- .github/actions/audit-accessibility/main.js
+++ .github/actions/audit-accessibility/main.js
@@ -5,7 +5,6 @@ const StaticServer = require('static-server');
 const {AxePuppeteer} = require('@axe-core/puppeteer');
 const {createHtmlReport} = require('axe-html-reporter');
 const puppeteer = require('puppeteer');
-const fetch = require('node-fetch').default;
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
@@ -14,7 +13,6 @@ const {uploadFile} = require('../utils/azure-storage');
 const PATH_REPO_ROOT = path.join(__dirname, '../../..');
 const PATH_REPORTS = path.join(PATH_REPO_ROOT, 'reports/accessibility');
 const PORT_STORYBOOK = 6006;
-const PORT_CHROME = 9223;
 
 const STORIES_BLACKLIST = new Set(['welcome-welcome--mistica', 'icons-mistica-icons--catalog']);
 
@@ -31,13 +29,10 @@ const getStories = () => {
     return Object.keys(require('../../../public/stories.json').stories);
 };
 
-const getStoryUrl = (id) => {
-    let host = 'localhost';
-    // if (!process.env.CI) {
-    //     host = process.platform === 'linux' ? '172.17.0.1' : 'host.docker.internal';
-    // }
-    return `http://${host}:${PORT_STORYBOOK}/iframe.html?viewMode=story&id=${id}`;
-};
+/**
+ * @param {string} id
+ */
+const getStoryUrl = (id) => `http://localhost:${PORT_STORYBOOK}/iframe.html?viewMode=story&id=${id}`;
 
 const startStorybookServer = () =>
     new Promise((resolve) => {
@@ -50,37 +45,6 @@ const startStorybookServer = () =>
             });
         });
     });
-
-// /**
-//  * @returns function to stop chrome
-//  */
-// const startChrome = async () => {
-//     if (process.env.CI) {
-//         return () => {};
-//     }
-//     console.log('Execute Chrome');
-//     execSync('yarn up-chromium', {stdio: 'inherit'});
-//     return () => {
-//         execSync('yarn down-chromium', {stdio: 'inherit'});
-//     };
-// };
-
-// /**
-//  * @returns {Promise<string>}
-//  */
-// const getBrowserWsEndpoint = async () => {
-//     let retries = 10;
-//     while (retries--) {
-//         try {
-//             const response = await fetch(`http://localhost:${PORT_CHROME}/json/version`);
-//             return (await response.json()).webSocketDebuggerUrl;
-//         } catch (e) {
-//             console.log('Waiting for Chrome dev server');
-//             await new Promise((r) => setTimeout(r, 500));
-//         }
-//     }
-//     throw Error('Chrome dev server not running');
-// };
 
 /**
  * @param {import('puppeteer').Browser} browser
@@ -129,7 +93,7 @@ const generateReport = async (results) => {
             lines.push(
                 `<details>`,
                 `  <summary>❌ (${result.violations.length}) <b>${name}</b></summary>`,
-                `  [HTML](${htmlUrl}) - [JSON](${jsonUrl})`,
+                `  <a href="${htmlUrl}" target="_blank">HTML]</a> - <a href="${jsonUrl})" target="_blank">JSON</a>`,
                 `</details>`
             );
         }
@@ -152,7 +116,6 @@ const main = async () => {
 
     const stories = getStories().filter((story) => !STORIES_BLACKLIST.has(story));
     const stopStorybookServer = await startStorybookServer();
-    // const stopChrome = await startChrome();
 
     const browser = await puppeteer.launch({args: ['--incognito', '--no-sandbox']});
 
@@ -171,7 +134,6 @@ const main = async () => {
 
     browser.close();
     stopStorybookServer();
-    // stopChrome();
 };
 
 main();
