8faa7fb995843812954f6ffc7ef12acfefa47f6a
Pol Escolar <pol@telefonica.com>
2020-12-22 11:04:59 +0100

    fix(Tabs): Tabs with icon bug with color states (#176)

--- src/__stories__/tabs-story.tsx
+++ src/__stories__/tabs-story.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Tabs from '../tabs';
 import {StorySection, useCheckbox} from './helpers';
-import {Placeholder} from '../placeholder';
+import IconClose from '../icons/icon-close';
 
 const texts = ['Tab 1', 'Tab 2', 'Tab 3'];
 
@@ -12,16 +12,20 @@ export default {
 
 export const Default: StoryComponent = () => {
     const [withIcon, withIconCheckbox] = useCheckbox('With icons', false);
+    const [withCurrentColor, withCurrentColorCheckbox] = useCheckbox('With icon color="currentColor"', false);
     const [selectedIndex, setSelectedIndex] = React.useState(0);
     return (
         <StorySection title="Tabs example">
             {withIconCheckbox}
+            {withIcon && withCurrentColorCheckbox}
             <Tabs
                 selectedIndex={selectedIndex}
                 onChange={setSelectedIndex}
                 tabs={texts.map((text) => ({
                     text,
-                    icon: withIcon ? <Placeholder width={24} height={24} /> : null,
+                    icon: withIcon ? (
+                        <IconClose color={withCurrentColor ? 'currentColor' : undefined} />
+                    ) : null,
                 }))}
             />
         </StorySection>
