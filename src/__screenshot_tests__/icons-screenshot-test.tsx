78f9b191574fe40ed801e107326bed95d0fe7274
Pedro Ladaria <pladaria@tuenti.com>
2021-02-01 10:18:27 +0100

    feat(Icons): Update mistica-icons collection (#186)

---
+++ src/__screenshot_tests__/icons-screenshot-test.tsx
@@ -0,0 +1,31 @@
+import {openStoryPage, screen} from '../test-utils';
+
+test('Icons catalog', async () => {
+    const page = await openStoryPage({
+        section: 'Icons/Mistica Icons',
+        name: 'Catalog',
+        device: 'DESKTOP',
+    });
+
+    const lightCheckbox = await screen.findByLabelText('Light');
+    const filledCheckbox = await screen.findByLabelText('Filled');
+    const regularCheckbox = await screen.findByLabelText('Regular');
+
+    filledCheckbox.click();
+    regularCheckbox.click();
+
+    const light = await page.screenshot({fullPage: true});
+    expect(light).toMatchImageSnapshot();
+
+    lightCheckbox.click();
+    regularCheckbox.click();
+
+    const regular = await page.screenshot({fullPage: true});
+    expect(regular).toMatchImageSnapshot();
+
+    regularCheckbox.click();
+    filledCheckbox.click();
+
+    const filled = await page.screenshot({fullPage: true});
+    expect(filled).toMatchImageSnapshot();
+});
