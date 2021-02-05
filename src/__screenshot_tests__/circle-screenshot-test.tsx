e9133851634f69b730b22eec28064aa88133a9ee
Abel Toledano <atoledano@tuenti.com>
2021-02-05 09:20:53 +0100

    feat(Circle): to be used with icons inside DataCard of list rows (#189)

---
+++ src/__screenshot_tests__/circle-screenshot-test.tsx
@@ -0,0 +1,19 @@
+import {ElementHandle} from 'puppeteer';
+import {openStoryPage, screen} from '../test-utils';
+
+test('Circle', async () => {
+    await openStoryPage({
+        section: 'Components/Others/Circle',
+        name: 'Circle',
+    });
+
+    const circle = (await (await screen.findByTestId('circle')).$('div')) as ElementHandle;
+    const circleWithIcon = (await (await screen.findByTestId('circle-with-icon')).$('div')) as ElementHandle;
+    const circleWithImage = (await (await screen.findByTestId('circle-with-image')).$(
+        'div'
+    )) as ElementHandle;
+
+    expect(await circle.screenshot()).toMatchImageSnapshot();
+    expect(await circleWithIcon.screenshot()).toMatchImageSnapshot();
+    expect(await circleWithImage.screenshot()).toMatchImageSnapshot();
+});
---
+++ src/circle.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {createUseStyles} from './jss';
+
+const useStyles = createUseStyles(() => ({
+    circle: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: ({backgroundColor}) => backgroundColor,
+        backgroundImage: ({backgroundImage}) => `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: ({size}) => size,
+        height: ({size}) => size,
+        borderRadius: '50%',
+    },
+}));
+
+type Props = {
+    backgroundColor?: string;
+    backgroundImage?: string;
+    size?: number | string;
+    children?: React.ReactElement<any>;
+};
+
+const Circle: React.FC<Props> = ({children, backgroundColor, backgroundImage, size}) => {
+    const classes = useStyles({backgroundColor, backgroundImage, size});
+    return <div className={classes.circle}>{children}</div>;
+};
+
+export default Circle;
