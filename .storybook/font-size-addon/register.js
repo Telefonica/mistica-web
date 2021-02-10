f08d974b216035acbb78767cd0ce9d2a1532e595
Abel Toledano <atoledano@tuenti.com>
2021-02-10 11:55:18 +0100

    use a material icon for the font size addon

--- .storybook/font-size-addon/register.js
+++ .storybook/font-size-addon/register.js
@@ -39,9 +39,13 @@ const FontSizeAddon = ({api}) => {
                 />
             )}
         >
-            <IconButton title="Change font size">
+            <IconButton title="Change base font size">
                 <div style={{display: 'flex', alignItems: 'center'}}>
-                    <span style={{marginLeft: 8}}>Base font Size: {fontSize}</span>
+                    <svg height="24" viewBox="0 0 24 24" width="24">
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path d="M9 4v3h5v12h3V7h5V4H9zm-6 8h3v7h3v-7h3V9H3v3z" fill="currentColor" />
+                    </svg>
+                    <span style={{marginLeft: 4}}>{fontSize}</span>
                 </div>
             </IconButton>
         </WithTooltip>
