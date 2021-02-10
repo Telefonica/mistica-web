94ef72a1a21867dfc02d450d9ce177d07826a158
Abel Toledano <atoledano@tuenti.com>
2021-02-10 15:10:58 +0100

    avoid mobile override to affect playroomtools controls

--- playroom/components.tsx
+++ playroom/components.tsx
@@ -12,13 +12,14 @@ import {
     useScreenSize,
     Tabs,
     Checkbox,
+    ThemeContextProvider,
 } from '../src';
 import {Movistar, Vivo, O2, O2_Classic} from './themes';
 import {useOverrideTheme} from './frame-component';
 
 export * from '../src';
 
-const useStyles = createUseStyles((theme) => ({
+const useControlsStyles = createUseStyles((theme) => ({
     controls: {
         background: 'white',
         position: 'fixed',
@@ -57,6 +58,9 @@ const useStyles = createUseStyles((theme) => ({
         alignItems: 'center',
         gap: '8px',
     },
+}));
+
+const useStyles = createUseStyles(() => ({
     floattingButton: {
         position: 'fixed',
         zIndex: 1,
@@ -81,13 +85,6 @@ const useStyles = createUseStyles((theme) => ({
     },
 }));
 
-type PreviewToolsProps = {
-    floating?: boolean;
-    position?: 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left';
-    showPlatformSelector?: boolean;
-    forceMobile?: boolean;
-};
-
 const themesMap: {[skinName in SkinName]: {themeConfig: ThemeConfig; text: string}} = {
     Movistar: {
         text: 'Movistar',
@@ -107,6 +104,94 @@ const themesMap: {[skinName in SkinName]: {themeConfig: ThemeConfig; text: strin
     },
 };
 
+type PreviewToolsControlsProps = {
+    os: 'android' | 'ios' | 'desktop';
+    onOsChange: (newOs: 'android' | 'ios' | 'desktop') => void;
+    skinName: SkinName;
+    onSkinNameChange: (newSkinName: SkinName) => void;
+    onEditStoryPress: () => void;
+    showPlatformSelector: boolean;
+};
+
+const PreviewToolsControls: React.FC<PreviewToolsControlsProps> = ({
+    os,
+    onOsChange,
+    showPlatformSelector,
+    skinName,
+    onSkinNameChange,
+    onEditStoryPress,
+}) => {
+    const classes = useControlsStyles();
+    const {colors} = useTheme();
+    const {isMobile} = useScreenSize();
+
+    if (isMobile) {
+        return (
+            <div className={`${classes.controls} ${classes.mobileControls}`}>
+                <Select
+                    label="Select brand"
+                    name="theme"
+                    options={Object.entries(themesMap).map(([skinName, {text}]) => ({value: skinName, text}))}
+                    value={skinName}
+                    onChangeValue={onSkinNameChange as any}
+                />
+                {showPlatformSelector && (
+                    <Checkbox
+                        name="iOS"
+                        checked={os === 'ios'}
+                        onChange={(checked) => onOsChange(checked ? 'ios' : 'android')}
+                        render={(checkboxElement) => (
+                            <div className={classes.checkbox}>{checkboxElement} iOS</div>
+                        )}
+                    />
+                )}
+                <IconButton label="Edit in Playroom" size={32} onPress={onEditStoryPress}>
+                    <IconCodeFilled size={32} color={colors.iconSecondary} />
+                </IconButton>
+            </div>
+        );
+    } else {
+        return (
+            <div className={`${classes.controls} ${classes.desktopControls}`}>
+                <div className={classes.tabs}>
+                    <Tabs
+                        tabs={Object.values(themesMap).map(({text}) => ({text}))}
+                        selectedIndex={Object.keys(themesMap).indexOf(skinName)}
+                        onChange={(index) => {
+                            onSkinNameChange((Object.keys(themesMap) as Array<SkinName>)[index]);
+                        }}
+                    />
+                </div>
+                <div className={classes.flexSpacer} />
+                {showPlatformSelector && (
+                    <div className={classes.desktopControlItem}>
+                        <Checkbox
+                            name="iOS"
+                            checked={os === 'ios'}
+                            onChange={(checked) => onOsChange(checked ? 'ios' : 'android')}
+                            render={(checkboxElement) => (
+                                <div className={classes.checkbox}>{checkboxElement} iOS</div>
+                            )}
+                        />
+                    </div>
+                )}
+                <div className={classes.desktopControlItem}>
+                    <IconButton label="Edit in Playroom" size={32} onPress={onEditStoryPress}>
+                        <IconCodeFilled size={32} color={colors.iconSecondary} />
+                    </IconButton>
+                </div>
+            </div>
+        );
+    }
+};
+
+type PreviewToolsProps = {
+    floating?: boolean;
+    position?: 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left';
+    showPlatformSelector?: boolean;
+    forceMobile?: boolean;
+};
+
 export const PreviewTools: React.FC<PreviewToolsProps> = ({
     children,
     floating,
@@ -117,9 +202,7 @@ export const PreviewTools: React.FC<PreviewToolsProps> = ({
     const {
         skinName: initialSkinName,
         platformOverrides: {platform: initialOs = 'android'},
-        colors,
     } = useTheme();
-    const {isMobile} = useScreenSize();
     const [showOverlay, setShowOverlay] = React.useState(false);
     const [skinName, setSkinName] = React.useState<SkinName>(initialSkinName);
     const [os, setOs] = React.useState<'android' | 'ios' | 'desktop'>(initialOs);
@@ -149,59 +232,23 @@ export const PreviewTools: React.FC<PreviewToolsProps> = ({
         }
     };
 
-    const controls = isMobile ? (
-        <div className={`${classes.controls} ${classes.mobileControls}`}>
-            <Select
-                label="Select brand"
-                name="theme"
-                options={Object.entries(themesMap).map(([skinName, {text}]) => ({value: skinName, text}))}
-                value={skinName}
-                onChangeValue={setSkinName as any}
+    const controls = (
+        <ThemeContextProvider
+            theme={{
+                ...themesMap[skinName].themeConfig,
+                platformOverrides: {platform: os},
+                // Dont override mediaqueries for PreviewToolsControls, to avoid using Select instead of Tabs in desktop
+            }}
+        >
+            <PreviewToolsControls
+                skinName={skinName}
+                onSkinNameChange={setSkinName}
+                os={os}
+                onOsChange={setOs}
+                showPlatformSelector={showPlatformSelector}
+                onEditStoryPress={editStory}
             />
-            {showPlatformSelector && (
-                <Checkbox
-                    name="iOS"
-                    checked={os === 'ios'}
-                    onChange={(checked) => setOs(checked ? 'ios' : 'android')}
-                    render={(checkboxElement) => (
-                        <div className={classes.checkbox}>{checkboxElement} iOS</div>
-                    )}
-                />
-            )}
-            <IconButton label="Edit in Playroom" size={32} onPress={editStory}>
-                <IconCodeFilled size={32} color={colors.iconSecondary} />
-            </IconButton>
-        </div>
-    ) : (
-        <div className={`${classes.controls} ${classes.desktopControls}`}>
-            <div className={classes.tabs}>
-                <Tabs
-                    tabs={Object.values(themesMap).map(({text}) => ({text}))}
-                    selectedIndex={Object.keys(themesMap).indexOf(skinName)}
-                    onChange={(index) => {
-                        setSkinName((Object.keys(themesMap) as Array<SkinName>)[index]);
-                    }}
-                />
-            </div>
-            <div className={classes.flexSpacer} />
-            {showPlatformSelector && (
-                <div className={classes.desktopControlItem}>
-                    <Checkbox
-                        name="iOS"
-                        checked={os === 'ios'}
-                        onChange={(checked) => setOs(checked ? 'ios' : 'android')}
-                        render={(checkboxElement) => (
-                            <div className={classes.checkbox}>{checkboxElement} iOS</div>
-                        )}
-                    />
-                </div>
-            )}
-            <div className={classes.desktopControlItem}>
-                <IconButton label="Edit in Playroom" size={32} onPress={editStory}>
-                    <IconCodeFilled size={32} color={colors.iconSecondary} />
-                </IconButton>
-            </div>
-        </div>
+        </ThemeContextProvider>
     );
 
     if (floating) {
