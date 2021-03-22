4d0008111acc6d72739daf9d5e47cb3e421d2c7d
Yayo <yceballost@gmail.com>
2021-03-22 16:35:09 +0100

    Update tag-story.tsx

--- src/__stories__/tag-story.tsx
+++ src/__stories__/tag-story.tsx
@@ -7,15 +7,33 @@ export default {
 };
 
 export const Default: StoryComponent = () => {
-    const [text, textField] = useTextField('Text', 'promo');
+    const [text, textField] = useTextField('Text', 'tag label');
     const {colors} = useTheme();
 
     return (
-        <Stack space={16}>
-            <div data-testid="tag">
+        <Stack space={56}>
+            <Stack space={16}>
+                Examples
+                <Tag color={colors.promo}>Promotion</Tag>
+                <Tag color={colors.brand}>In progress</Tag>
+                <Tag color={colors.success}>Completed</Tag>
+                <Tag color={colors.warning}>Pending</Tag>
+                <Tag color={colors.error}>Overdue</Tag>
+                <Tag color={colors.neutralMedium}>Removed</Tag>
+                <Tag color={colors.inverse}>Priority</Tag>
+            </Stack>
+
+            <Stack space={16}>
+                Try yourself
                 <Tag color={colors.promo}>{text}</Tag>
-            </div>
-            {textField}
+                <Tag color={colors.brand}>{text}</Tag>
+                <Tag color={colors.success}>{text}</Tag>
+                <Tag color={colors.warning}>{text}</Tag>
+                <Tag color={colors.error}>{text}</Tag>
+                <Tag color={colors.neutralMedium}>{text}</Tag>
+                <Tag color={colors.inverse}>{text}</Tag>
+                {textField}
+            </Stack>
         </Stack>
     );
 };
