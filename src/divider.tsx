c63e1661e27d0e5c4b5094b307215b7fe2a4eccb
Sergio Morchón Poveda <sergio.morchon.poveda@telefonica.com>
2021-04-07 16:08:14 +0200

    CR Changes

--- src/divider.tsx
+++ src/divider.tsx
@@ -2,22 +2,19 @@ import * as React from 'react';
 import {createUseStyles} from './jss';
 import {useIsInverseVariant} from './theme-variant-context';
 
-type StylesProps = {
-    isInverse: boolean;
-};
+const getStyles = (color: string) => ({
+    borderBottom: `1px solid ${color}`,
+});
 
 const useStyles = createUseStyles(({colors}) => ({
-    divider: {
-        height: 1,
-        borderBottom: ({isInverse}: StylesProps) =>
-            `1px solid ${isInverse ? colors.dividerInverse : colors.divider}`,
-    },
+    divider: getStyles(colors.divider),
+    dividerInverse: getStyles(colors.dividerInverse),
 }));
 
 const Divider: React.FC = () => {
     const isInverse = useIsInverseVariant();
-    const classes = useStyles({isInverse} as StylesProps);
-    return <div className={classes.divider} />;
+    const classes = useStyles();
+    return <div className={isInverse ? classes.dividerInverse : classes.divider} />;
 };
 
 export default Divider;
