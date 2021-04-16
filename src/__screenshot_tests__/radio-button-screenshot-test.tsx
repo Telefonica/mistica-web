21511b6d49a2e5d9e94fe04a9d32c43dd52659aa
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2021-04-16 12:41:08 +0200

    WEB-159 update tests

--- src/__screenshot_tests__/radio-button-screenshot-test.tsx
+++ src/__screenshot_tests__/radio-button-screenshot-test.tsx
@@ -1,9 +1,12 @@
 import {openStoryPage} from '../test-utils';
 
-test('RadioGroup', async () => {
+const DEVICES = ['MOBILE_IOS', 'MOBILE_ANDROID'] as const;
+
+test.each(DEVICES)(`RadioGroup`, async (device) => {
     await openStoryPage({
         section: 'Components/Forms/RadioButton',
         name: 'Radio Button',
+        device,
     });
 
     const image = await page.screenshot();
