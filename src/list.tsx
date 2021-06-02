70042b1b188909b8ca08dd5a188628a7f5f0b2f0
Abel Toledano <atoledano@tuenti.com>
2021-06-02 15:31:16 +0200

    fix in acceptance tests too

--- src/list.tsx
+++ src/list.tsx
@@ -22,8 +22,11 @@ const useStyles = createUseStyles(({colors}) => ({
         width: '100%',
         cursor: 'pointer',
 
-        // Only apply hover effect to devices using fine pointer devices (a mouse, for example)
-        '@media (pointer: fine)': {
+        // Only apply hover effect to user agents using fine pointer devices (a mouse, for example)
+        // Also enabled for (pointer: none) for acceptance tests, where (pointer: fine) doesn't match.
+        // WARNING: you may be tempted to use @media (hover: hover) instead, but that doesn't work as expected in some android browsers.
+        // See: https://hover-pointer-media-query.glitch.me/ and https://github.com/mui-org/material-ui/issues/15736
+        '@media (pointer: fine), (pointer: none)': {
             '&:hover': {
                 background: ({isInverse}) => (isInverse ? 'initial' : colors.backgroundAlternative),
             },
