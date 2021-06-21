2cd067929925d125d3310951002055b7dcea8247
Abel Toledano <atoledano@tuenti.com>
2021-06-21 16:35:05 +0200

    remove unused var

--- src/__tests__/credit-card-fields-test.tsx
+++ src/__tests__/credit-card-fields-test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {fireEvent, render, screen, waitFor} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import {ButtonPrimary, Form, CreditCardFields} from '..';
 import ThemeContextProvider from '../theme-context-provider';
