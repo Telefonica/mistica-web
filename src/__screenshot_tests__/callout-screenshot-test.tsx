657ede728809d771497443760d1f0b8278032b88
Abel Toledano <atoledano@tuenti.com>
2021-06-25 11:04:42 +0200

    add new test case for only description and closable. Also make semantic functions for enable/disable checkboxes

--- src/__screenshot_tests__/callout-screenshot-test.tsx
+++ src/__screenshot_tests__/callout-screenshot-test.tsx
@@ -1,4 +1,20 @@
+import {ElementHandle} from 'puppeteer';
 import {openStoryPage, screen} from '../test-utils';
+
+const isCheckboxEnabled = (element: ElementHandle) =>
+    element.evaluate((el) => el.getAttribute('aria-checked') === 'true');
+
+const enableCheckbox = async (element: ElementHandle) => {
+    if (!(await isCheckboxEnabled(element))) {
+        await element.click();
+    }
+};
+
+const disableCheckbox = async (element: ElementHandle) => {
+    if (await isCheckboxEnabled(element)) {
+        await element.click();
+    }
+};
 
 test('Callout', async () => {
     await openStoryPage({
@@ -13,12 +29,12 @@ test('Callout', async () => {
 });
 
 test('Callout over inverse', async () => {
-    const page = await openStoryPage({
+    await openStoryPage({
         section: 'Components/Dialogs/Callout',
         name: 'Callout',
     });
 
-    await page.click(await screen.findByLabelText('Over inverse'));
+    await enableCheckbox(await screen.findByLabelText('Over inverse'));
 
     const callout = await screen.findByRole('alert');
 
@@ -27,12 +43,12 @@ test('Callout over inverse', async () => {
 });
 
 test('Callout not closable', async () => {
-    const page = await openStoryPage({
+    await openStoryPage({
         section: 'Components/Dialogs/Callout',
         name: 'Callout',
     });
 
-    await page.click(await screen.findByLabelText('Is closable'));
+    await disableCheckbox(await screen.findByLabelText('Is closable'));
 
     const callout = await screen.findByRole('alert');
 
@@ -75,8 +91,24 @@ test('Callout only description', async () => {
     });
     await page.clear(await screen.findByLabelText('title (opcional)'));
     await page.select(await screen.findByLabelText('actions'), 'none');
-    await page.click(await screen.findByLabelText('With icon'));
-    await page.click(await screen.findByLabelText('Is closable'));
+    await disableCheckbox(await screen.findByLabelText('With icon'));
+    await disableCheckbox(await screen.findByLabelText('Is closable'));
+
+    const callout = await screen.findByRole('alert');
+
+    const image = await callout.screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
+test('Callout only description and closable', async () => {
+    const page = await openStoryPage({
+        section: 'Components/Dialogs/Callout',
+        name: 'Callout',
+    });
+    await page.clear(await screen.findByLabelText('title (opcional)'));
+    await page.select(await screen.findByLabelText('actions'), 'none');
+    await disableCheckbox(await screen.findByLabelText('With icon'));
+    await enableCheckbox(await screen.findByLabelText('Is closable'));
 
     const callout = await screen.findByRole('alert');
 
