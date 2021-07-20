7e820d51f14c855479c8d78fe140861453ecbb29
Yayo <yceballost@gmail.com>
2021-07-20 17:42:58 +0200

    Update snippets.tsx

--- playroom/snippets.tsx
+++ playroom/snippets.tsx
@@ -601,78 +601,78 @@ const layoutSnippets: Array<Snippet> = [
     {
         name: 'MasterDetailLayout',
         code: `
-<Box paddingY={24}>
-  <MasterDetailLayout
-    isOpen={!!getState("selectedItem")}
-    master={
-      <Stack space={32}>
-        {[
-          {
-            categoryName: "Personal information",
-            settings: [
-              { title: "Personal details", icon: <IconUserAccountRegular /> },
-              { title: "Security", icon: <IconLockClosedRegular /> },
-              {
-                title: "Payment methods",
-                icon: <IconCreditCardVisaRegular />,
-              },
-            ],
-          },
-          {
-            categoryName: "Configuration",
-            settings: [
-              { title: "Notifications", icon: <IconProgramAlarmRegular /> },
-              { title: "FAQs", icon: <IconSupportAgentRegular /> },
-              { title: "About", icon: <IconInformationUserRegular /> },
-            ],
-          },
-        ].map((category) => (
-          <Stack key={category.categoryName} space={8}>
-            <SectionTitle>{category.categoryName}</SectionTitle>
-            <NegativeBox>
-              <RowList>
-                {category.settings.map((setting) => (
-                  <Row
-                    key={setting.title}
-                    title={setting.title}
-                    icon={setting.icon}
-                    iconSize={24}
-                    onPress={() => {
-                      setState("selectedItem", setting.title);
-                    }}
-                  />
+        <Box paddingY={24}>
+        <MasterDetailLayout
+            isOpen={!!getState("selectedItem")}
+            master={
+            <Stack space={32}>
+                {[
+                {
+                    categoryName: "Personal information",
+                    settings: [
+                    { title: "Personal details", icon: <IconUserAccountRegular /> },
+                    { title: "Security", icon: <IconLockClosedRegular /> },
+                    {
+                        title: "Payment methods",
+                        icon: <IconCreditCardVisaRegular />,
+                    },
+                    ],
+                },
+                {
+                    categoryName: "Configuration",
+                    settings: [
+                    { title: "Notifications", icon: <IconProgramAlarmRegular /> },
+                    { title: "FAQs", icon: <IconSupportAgentRegular /> },
+                    { title: "About", icon: <IconInformationUserRegular /> },
+                    ],
+                },
+                ].map((category) => (
+                <Stack key={category.categoryName} space={8}>
+                    <SectionTitle>{category.categoryName}</SectionTitle>
+                    <NegativeBox>
+                    <RowList>
+                        {category.settings.map((setting) => (
+                        <Row
+                            key={setting.title}
+                            title={setting.title}
+                            icon={setting.icon}
+                            iconSize={24}
+                            onPress={() => {
+                            setState("selectedItem", setting.title);
+                            }}
+                        />
+                        ))}
+                    </RowList>
+                    </NegativeBox>
+                </Stack>
                 ))}
-              </RowList>
-            </NegativeBox>
-          </Stack>
-        ))}
-      </Stack>
-    }
-    detail={
-      getState("selectedItem") ? (
-        <Stack space={16}>
-          <Text4 as="h2" medium>
-            {getState("selectedItem")}
-          </Text4>
-          <Text2 regular>
-            You are inside {getState("selectedItem")} section
-          </Text2>
-          <Placeholder />
-          <ButtonPrimary
-            small
-            onPress={() => {
-              setState("selectedItem", null);
-            }}
-          >
-            Close
-          </ButtonPrimary>
-        </Stack>
-      ) : (
-        <Text2 regular>Select one of the sections from the sidebar</Text2>
-      )
-    }
-  />
-</Box>`,
+            </Stack>
+            }
+            detail={
+            getState("selectedItem") ? (
+                <Stack space={16}>
+                <Text4 as="h2" medium>
+                    {getState("selectedItem")}
+                </Text4>
+                <Text2 regular>
+                    You are inside {getState("selectedItem")} section
+                </Text2>
+                <Placeholder />
+                <ButtonPrimary
+                    small
+                    onPress={() => {
+                    setState("selectedItem", null);
+                    }}
+                >
+                    Close
+                </ButtonPrimary>
+                </Stack>
+            ) : (
+                <Text2 regular>Select one of the sections from the sidebar</Text2>
+            )
+            }
+        />
+        </Box>`,
     },
 ].map((snippet) => ({...snippet, group: 'Layouts'}));
 
