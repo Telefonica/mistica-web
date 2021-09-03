530afaefe7186eb4180127cb45677b483b2010b6
Ana Belén García Parra <anabelen.garciaparra@telefonica.com>
2021-09-03 16:10:04 +0200

    feedback: fix icon-tree names, fix exports

--- src/index.tsx
+++ src/index.tsx
@@ -309,6 +309,9 @@ export {default as IconBugFilled} from './generated/mistica-icons/icon-bug-fille
 export {default as IconBugLight} from './generated/mistica-icons/icon-bug-light';
 export {default as IconBugRegular} from './generated/mistica-icons/icon-bug-regular';
 export {default as IconBusFilled} from './generated/mistica-icons/icon-bus-filled';
+export {default as IconBushFilled} from './generated/mistica-icons/icon-bush-filled';
+export {default as IconBushLight} from './generated/mistica-icons/icon-bush-light';
+export {default as IconBushRegular} from './generated/mistica-icons/icon-bush-regular';
 export {default as IconBusLight} from './generated/mistica-icons/icon-bus-light';
 export {default as IconBusRegular} from './generated/mistica-icons/icon-bus-regular';
 export {default as IconBuyDataFilled} from './generated/mistica-icons/icon-buy-data-filled';
@@ -342,6 +345,9 @@ export {default as IconCancelFilled} from './generated/mistica-icons/icon-cancel
 export {default as IconCancelLight} from './generated/mistica-icons/icon-cancel-light';
 export {default as IconCancelRegular} from './generated/mistica-icons/icon-cancel-regular';
 export {default as IconCareFilled} from './generated/mistica-icons/icon-care-filled';
+export {default as IconCareHealthFilled} from './generated/mistica-icons/icon-care-health-filled';
+export {default as IconCareHealthLight} from './generated/mistica-icons/icon-care-health-light';
+export {default as IconCareHealthRegular} from './generated/mistica-icons/icon-care-health-regular';
 export {default as IconCareLight} from './generated/mistica-icons/icon-care-light';
 export {default as IconCareRegular} from './generated/mistica-icons/icon-care-regular';
 export {default as IconCareShieldFilled} from './generated/mistica-icons/icon-care-shield-filled';
@@ -424,6 +430,8 @@ export {default as IconCocktailRegular} from './generated/mistica-icons/icon-coc
 export {default as IconCodeFilled} from './generated/mistica-icons/icon-code-filled';
 export {default as IconCodeLight} from './generated/mistica-icons/icon-code-light';
 export {default as IconCodeRegular} from './generated/mistica-icons/icon-code-regular';
+export {default as IconComputerAcademicFilled} from './generated/mistica-icons/icon-computer-academic-filled';
+export {default as IconComputerAcademicRegular} from './generated/mistica-icons/icon-computer-academic-regular';
 export {default as IconComputerFilled} from './generated/mistica-icons/icon-computer-filled';
 export {default as IconComputerLight} from './generated/mistica-icons/icon-computer-light';
 export {default as IconComputerRegular} from './generated/mistica-icons/icon-computer-regular';
@@ -570,6 +578,9 @@ export {default as IconEmergencyCrossWithCircleRegular} from './generated/mistic
 export {default as IconErrorFilled} from './generated/mistica-icons/icon-error-filled';
 export {default as IconErrorLight} from './generated/mistica-icons/icon-error-light';
 export {default as IconErrorRegular} from './generated/mistica-icons/icon-error-regular';
+export {default as IconEuroSymbolCircleFilled} from './generated/mistica-icons/icon-euro-symbol-circle-filled';
+export {default as IconEuroSymbolCircleLight} from './generated/mistica-icons/icon-euro-symbol-circle-light';
+export {default as IconEuroSymbolCircleRegular} from './generated/mistica-icons/icon-euro-symbol-circle-regular';
 export {default as IconEuroSymbolFilled} from './generated/mistica-icons/icon-euro-symbol-filled';
 export {default as IconEuroSymbolLight} from './generated/mistica-icons/icon-euro-symbol-light';
 export {default as IconEuroSymbolRegular} from './generated/mistica-icons/icon-euro-symbol-regular';
@@ -884,6 +895,8 @@ export {default as IconMusicFilled} from './generated/mistica-icons/icon-music-f
 export {default as IconMusicLight} from './generated/mistica-icons/icon-music-light';
 export {default as IconMusicRegular} from './generated/mistica-icons/icon-music-regular';
 export {default as IconNeuralNetworkFilled} from './generated/mistica-icons/icon-neural-network-filled';
+export {default as IconNeuralNetworkHumanFilled} from './generated/mistica-icons/icon-neural-network-human-filled';
+export {default as IconNeuralNetworkHumanRegular} from './generated/mistica-icons/icon-neural-network-human-regular';
 export {default as IconNeuralNetworkLight} from './generated/mistica-icons/icon-neural-network-light';
 export {default as IconNeuralNetworkRegular} from './generated/mistica-icons/icon-neural-network-regular';
 export {default as IconNotConexionFilled} from './generated/mistica-icons/icon-not-conexion-filled';
