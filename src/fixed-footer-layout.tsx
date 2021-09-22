f9503a281f1094f35d7b70cff8b4d912b6c35900
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2021-09-22 18:52:41 +0200

    WEB-339 stop using portal

--- src/fixed-footer-layout.tsx
+++ src/fixed-footer-layout.tsx
@@ -4,17 +4,10 @@ import debounce from 'lodash/debounce';
 import {createUseStyles} from './jss';
 import {isRunningAcceptanceTest} from './utils/platform';
 import {useElementDimensions, useIsomorphicLayoutEffect, useScreenSize, useTheme} from './hooks';
-import {Portal} from './portal';
 import {addPassiveEventListener, removePassiveEventListener} from './utils/dom';
+import {useTopDistance} from './fixed-to-top';
 
 const getScrollDistanceToBottom = () => document.body.scrollHeight - window.innerHeight - window.scrollY;
-
-const waitForSwitchTransitionToStart = (fn: () => void) => {
-    const timeoutId = setTimeout(fn, 0);
-    return {
-        cancel: () => clearTimeout(timeoutId),
-    };
-};
 
 // this high zIndex is needed because the fixed footer must be displayed over
 // the bottom navbar from movistar.es in mobile
@@ -23,7 +16,6 @@ const Z_INDEX = 25;
 const useStyles = createUseStyles((theme) => ({
     footer: {
         width: '100%',
-        zIndex: Z_INDEX,
     },
 
     shadow: {},
@@ -35,6 +27,7 @@ const useStyles = createUseStyles((theme) => ({
     containerSmall: {
         paddingBottom: ({height}) => height,
         backgroundColor: ({containerBgColor}) => containerBgColor || theme.colors.background,
+        minHeight: ({topDistance}) => `calc(100vh - ${topDistance}px)`,
     },
 
     [theme.mq.tabletOrSmaller]: {
@@ -75,6 +68,7 @@ const FixedFooterLayout: React.FC<Props> = ({
     const {platformOverrides} = useTheme();
     const {height: realHeight, ref} = useElementDimensions();
     const {colors} = useTheme();
+    const topDistance = useTopDistance();
 
     useIsomorphicLayoutEffect(() => {
         onChangeFooterHeight?.(realHeight);
@@ -104,45 +98,39 @@ const FixedFooterLayout: React.FC<Props> = ({
             {leading: true}
         );
 
-        const transitionAwaiter = waitForSwitchTransitionToStart(checkDisplayShadow);
         addPassiveEventListener(window, 'resize', checkDisplayShadow);
         addPassiveEventListener(window, 'scroll', checkDisplayShadow);
         return () => {
             checkDisplayShadow.cancel();
             removePassiveEventListener(window, 'scroll', checkDisplayShadow);
             removePassiveEventListener(window, 'resize', checkDisplayShadow);
-            transitionAwaiter.cancel();
         };
     }, [children, childrenRef, platformOverrides]);
 
-    const classes = useStyles({footerBgColor, containerBgColor, height: realHeight});
-
-    const renderFooter = () => (
-        <div
-            ref={ref}
-            className={classnames(classes.footer, {
-                [classes.withoutFooter]: !isFooterVisible,
-                [classes.shadow]: displayShadow,
-            })}
-            // this style is inline to avoid creating a class  that may collide with
-            // other fixed footers during the page animation transition
-            style={{background: isTabletOrSmaller ? footerBgColor || colors.background : undefined}}
-            data-testid={`fixed-footer${isFooterVisible ? '-visible' : '-hidden'}`}
-        >
-            {isFooterVisible && (
-                <aside style={{height: footerHeight, marginBottom: 'env(safe-area-inset-bottom)'}}>
-                    {footer}
-                </aside>
-            )}
-        </div>
-    );
+    const classes = useStyles({footerBgColor, containerBgColor, height: realHeight, topDistance});
 
     return (
         <>
             <div ref={childrenRef} className={classnames({[classes.containerSmall]: isTabletOrSmaller})}>
                 {children}
             </div>
-            {isTabletOrSmaller ? <Portal>{renderFooter()}</Portal> : renderFooter()}
+            <div
+                ref={ref}
+                className={classnames(classes.footer, {
+                    [classes.withoutFooter]: !isFooterVisible,
+                    [classes.shadow]: displayShadow,
+                })}
+                // this style is inline to avoid creating a class that may collide with
+                // other fixed footers during the page animation transition
+                style={{background: isTabletOrSmaller ? footerBgColor || colors.background : undefined}}
+                data-testid={`fixed-footer${isFooterVisible ? '-visible' : '-hidden'}`}
+            >
+                {isFooterVisible && (
+                    <aside style={{height: footerHeight, marginBottom: 'env(safe-area-inset-bottom)'}}>
+                        {footer}
+                    </aside>
+                )}
+            </div>
         </>
     );
 };
