ee439b3f9fde69e4794acca07bbbd86cab9ab3a1
Ana Belén García Parra <anabelen.garciaparra@telefonica.com>
2021-10-07 10:18:05 +0200

    add args to ButtonGroup story

--- src/__stories__/button-group-story.tsx
+++ src/__stories__/button-group-story.tsx
@@ -9,41 +9,53 @@ export default {
 
 const handleOnPress = () => window.alert('pressed!');
 
-const primaryButton = <ButtonPrimary onPress={handleOnPress}>Action1</ButtonPrimary>;
-const longButton = (
-    <ButtonPrimary onPress={handleOnPress}>A very long action text in this button</ButtonPrimary>
-);
+type Args = {
+    primaryButtonText: string;
+    secondaryButtonText: string;
+    linkText: string;
+};
 
-const secondaryButton = <ButtonSecondary onPress={handleOnPress}>Action2</ButtonSecondary>;
-const buttonLink = <ButtonLink onPress={handleOnPress}>link</ButtonLink>;
+export const Default: StoryComponent<Args> = ({primaryButtonText, secondaryButtonText, linkText}) => {
+    const primaryButton = <ButtonPrimary onPress={handleOnPress}>{primaryButtonText}</ButtonPrimary>;
 
-export const Default: StoryComponent = () => (
-    <Stack space={16}>
-        <SectionTitle as="h2">One Button</SectionTitle>
-        <ButtonGroup primaryButton={primaryButton}></ButtonGroup>
+    const secondaryButton = <ButtonSecondary onPress={handleOnPress}>{secondaryButtonText}</ButtonSecondary>;
+    const buttonLink = <ButtonLink onPress={handleOnPress}>{linkText}</ButtonLink>;
+    const longButton = (
+        <ButtonPrimary onPress={handleOnPress}>A very long action text in this button</ButtonPrimary>
+    );
+    return (
+        <Stack space={16}>
+            <SectionTitle as="h2">One Button</SectionTitle>
+            <ButtonGroup primaryButton={primaryButton}></ButtonGroup>
 
-        <SectionTitle as="h2">Two Buttons</SectionTitle>
-        <ButtonGroup primaryButton={primaryButton} secondaryButton={secondaryButton}></ButtonGroup>
+            <SectionTitle as="h2">Two Buttons</SectionTitle>
+            <ButtonGroup primaryButton={primaryButton} secondaryButton={secondaryButton}></ButtonGroup>
 
-        <SectionTitle as="h2">Primary Button and Link</SectionTitle>
-        <ButtonGroup primaryButton={primaryButton} link={buttonLink}></ButtonGroup>
+            <SectionTitle as="h2">Primary Button and Link</SectionTitle>
+            <ButtonGroup primaryButton={primaryButton} link={buttonLink}></ButtonGroup>
 
-        <SectionTitle as="h2">Secondary Button and Link</SectionTitle>
-        <ButtonGroup secondaryButton={secondaryButton} link={buttonLink}></ButtonGroup>
+            <SectionTitle as="h2">Secondary Button and Link</SectionTitle>
+            <ButtonGroup secondaryButton={secondaryButton} link={buttonLink}></ButtonGroup>
 
-        <SectionTitle as="h2">only Link</SectionTitle>
-        <ButtonGroup link={buttonLink}></ButtonGroup>
+            <SectionTitle as="h2">only Link</SectionTitle>
+            <ButtonGroup link={buttonLink}></ButtonGroup>
 
-        <SectionTitle as="h2">Two Buttons and Link</SectionTitle>
-        <ButtonGroup
-            primaryButton={primaryButton}
-            secondaryButton={secondaryButton}
-            link={buttonLink}
-        ></ButtonGroup>
+            <SectionTitle as="h2">Two Buttons and Link</SectionTitle>
+            <ButtonGroup
+                primaryButton={primaryButton}
+                secondaryButton={secondaryButton}
+                link={buttonLink}
+            ></ButtonGroup>
 
-        <SectionTitle as="h2">Different-length Buttons</SectionTitle>
-        <ButtonGroup primaryButton={longButton} secondaryButton={secondaryButton}></ButtonGroup>
-    </Stack>
-);
+            <SectionTitle as="h2">Different-length Buttons</SectionTitle>
+            <ButtonGroup primaryButton={longButton} secondaryButton={secondaryButton}></ButtonGroup>
+        </Stack>
+    );
+};
 
 Default.storyName = 'ButtonGroup';
+Default.args = {
+    primaryButtonText: 'Action1',
+    secondaryButtonText: 'Action2',
+    linkText: 'link',
+};
