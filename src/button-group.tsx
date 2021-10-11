e125111587dd85c442cf1095e6774a03c93715b6
Ana Belén García Parra <anabelen.garciaparra@telefonica.com>
2021-10-11 18:49:48 +0200

    display in stack when button widths overflow

--- src/button-group.tsx
+++ src/button-group.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {createUseStyles} from './jss';
 import Inline from './inline';
 import {ButtonLink, ButtonLinkProps, ButtonPrimary, ButtonProps, ButtonSecondary} from './button';
+import {useIsomorphicLayoutEffect} from './hooks';
+import {Stack} from '.';
 
 const buttonLayoutSpacing = 16;
 const buttonLinkPadding = 6;
@@ -9,6 +11,11 @@ const buttonLinkPadding = 6;
 const useStyles = createUseStyles(() => ({
     container: {
         flex: 1,
+    },
+    fullWidthButtons: {
+        '& > div > *': {
+            width: '100%',
+        },
     },
     fullWidthLink: {
         marginLeft: -buttonLinkPadding,
@@ -26,18 +33,37 @@ export interface ButtonGroupProps {
 const ButtonGroup: React.FC<ButtonGroupProps> = ({primaryButton, secondaryButton, link}) => {
     const anyButton = !!primaryButton || !!secondaryButton;
     const bothButtons = !!primaryButton && !!secondaryButton;
+    const [isOverflowing, setIsOverflowing] = React.useState(false);
 
     const classes = useStyles({bothButtons});
 
+    const containerElRef = React.useRef<HTMLDivElement | null>(null);
+
+    useIsomorphicLayoutEffect(() => {
+        if (containerElRef.current) {
+            setIsOverflowing(containerElRef.current.scrollWidth > containerElRef.current.clientWidth);
+        }
+    }, []);
+
+    const actions = anyButton ? (
+        isOverflowing ? (
+            <Stack className={classes.fullWidthButtons} space={16}>
+                {primaryButton}
+                {secondaryButton}
+                {!bothButtons && link}
+            </Stack>
+        ) : (
+            <Inline space={buttonLayoutSpacing} alignItems="center">
+                {primaryButton}
+                {secondaryButton}
+                {!bothButtons && link}
+            </Inline>
+        )
+    ) : undefined;
+
     return (
-        <div className={classes.container}>
-            {anyButton && (
-                <Inline space={buttonLayoutSpacing} alignItems="center">
-                    {primaryButton}
-                    {secondaryButton}
-                    {!bothButtons && link}
-                </Inline>
-            )}
+        <div ref={containerElRef} className={classes.container}>
+            {actions}
             {link && (bothButtons || !anyButton) && <div className={classes.fullWidthLink}>{link}</div>}
         </div>
     );
