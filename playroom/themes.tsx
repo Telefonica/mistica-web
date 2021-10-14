235017bb80fd87956ce84257c2042862d53b6f85
Yayo <yceballost@gmail.com>
2021-10-14 10:43:48 +0200

    feat(Blau): new skin (#311)

--- playroom/themes.tsx
+++ playroom/themes.tsx
@@ -17,9 +17,11 @@ export const Vivo: ThemeConfig = {...themes.Vivo, ...common};
 export const O2: ThemeConfig = {...themes.O2, ...common};
 export const O2_Classic: ThemeConfig = {...themes.O2_Classic, ...common};
 export const Telefonica: ThemeConfig = {...themes.Telefonica, ...common};
+export const Blau: ThemeConfig = {...themes.Blau, ...common};
 
 export const Movistar_iOS: ThemeConfig = {...Movistar, platformOverrides: {platform: 'ios'}};
 export const Vivo_iOS: ThemeConfig = {...Vivo, platformOverrides: {platform: 'ios'}};
 export const O2_iOS: ThemeConfig = {...O2, platformOverrides: {platform: 'ios'}};
 export const O2_Classic_iOS: ThemeConfig = {...O2_Classic, platformOverrides: {platform: 'ios'}};
 export const Telefonica_iOS: ThemeConfig = {...themes.Telefonica, platformOverrides: {platform: 'ios'}};
+export const Blau_iOS: ThemeConfig = {...themes.Blau, platformOverrides: {platform: 'ios'}};
