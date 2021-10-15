65b00ece7656dfee1f33929d15bcdc027c1b2a97
Yayo <yceballost@gmail.com>
2021-10-15 17:39:01 +0200

    new visual for icon story

--- src/icons/__stories__/mistica-icons-story.tsx
+++ src/icons/__stories__/mistica-icons-story.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import {useCheckbox} from '../../__stories__/helpers';
-import {ThemeVariant, useTheme, Box, Stack, SearchField, Inline, DoubleField, Text} from '../..';
+import {
+    ThemeVariant,
+    useTheme,
+    Box,
+    Stack,
+    SearchField,
+    Inline,
+    Text,
+    TextField,
+    Divider,
+    Checkbox,
+    Text1,
+} from '../..';
 import IntegerField from '../../integer-field';
 import {kebabCase, upperFirst} from 'lodash';
+import {useScreenSize} from '../../hooks';
 
 // require all icons
 const misticaIcons = ((requireContext) =>
@@ -15,17 +28,20 @@ const misticaIcons = ((requireContext) =>
 
 export default {
     title: 'Icons/Mistica Icons',
+    parameters: {
+        fullScreen: true,
+    },
 };
 
 export const Catalog: React.FC = () => {
     const [showRegular, regularCheckbox] = useCheckbox('Regular', true);
-    const [showLight, lightCheckbox] = useCheckbox('Light', true);
-    const [showFilled, filledCheckbox] = useCheckbox('Filled', true);
+    const [showLight, lightCheckbox] = useCheckbox('Light', false);
+    const [showFilled, filledCheckbox] = useCheckbox('Filled', false);
     const [isInverse, inverseCheckbox] = useCheckbox('Inverse', false);
-    const [showNames, showNamesCheckbox] = useCheckbox('Show names', true);
-    const [showIConBackground, showIConBackgroundCheckbox] = useCheckbox('Show background', false);
+    const [showNames, showNamesCheckbox] = useCheckbox('Name', true);
+    const [showIConBackground, showIConBackgroundCheckbox] = useCheckbox('Icon area', false);
     const [filter, setFilter] = React.useState('');
-    const [size, setSize] = React.useState(64);
+    const [size, setSize] = React.useState(24);
     const {colors} = useTheme();
 
     const getRealName = (name: string) => name.replace(/^Icon/, '').replace(/(Regular|Filled|Light)$/, '');
@@ -81,71 +97,145 @@ export const Catalog: React.FC = () => {
             ));
 
     const backgroundColor = isInverse ? colors.backgroundBrand : colors.background;
-    const iconBackgroundColor = showIConBackground ? '#aaa' : 'none';
+    const iconBackgroundColor = showIConBackground ? '#FFD0FE' : 'none';
+    const {isTabletOrSmaller, isTablet} = useScreenSize();
 
     return (
-        <div>
-            <Box paddingY={16}>
-                <Inline space={16}>
-                    <DoubleField>
-                        <SearchField name="filter" value={filter} label="Filter" onChangeValue={setFilter} />
-                        <IntegerField
-                            name="size"
-                            value={String(size || 0)}
-                            label="Size (px)"
-                            onChangeValue={(v) => setSize(Number(v) || 0)}
-                        />
-                    </DoubleField>
-                    <Stack space={8}>
-                        <Inline space={16}>
-                            {regularCheckbox}
-                            {lightCheckbox}
-                            {filledCheckbox}
-                        </Inline>
-                        <Inline space={16}>
-                            {inverseCheckbox}
-                            {showNamesCheckbox}
-                            {showIConBackgroundCheckbox}
-                        </Inline>
-                    </Stack>
-                </Inline>
-            </Box>
-
-            <ThemeVariant isInverse={isInverse}>
-                <div style={{background: backgroundColor}}>
-                    {misticaIcons
-                        .filter(({name}) => filterIcon(name))
-                        .sort((a, b) => compareNames(a.name, b.name))
-                        .map((Icon) => (
-                            <div style={{display: 'inline-block', verticalAlign: 'top', textAlign: 'center'}}>
-                                <div
-                                    style={{
-                                        textAlign: 'center',
-                                        padding: 16,
-                                        width: size,
-                                        minWidth: showNames ? 100 : 0,
-                                    }}
-                                >
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: isTabletOrSmaller ? 'column' : 'row',
+            }}
+        >
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: isTabletOrSmaller ? 'column' : 'row',
+                }}
+            >
+                <div
+                    style={{
+                        height: isTabletOrSmaller ? 'auto' : '100vh',
+                        width: isTabletOrSmaller ? 'auto' : '280px',
+                        position: isTabletOrSmaller ? 'static' : 'fixed',
+                        background: colors.background,
+                        borderRight: isTabletOrSmaller ? 'none' : '1px solid' + colors.divider,
+                        borderBottom: isTabletOrSmaller ? '1px solid' + colors.divider : 'none',
+                    }}
+                >
+                    <Box padding={16}>
+                        <Stack space={16}>
+                            <SearchField
+                                name="filter"
+                                value={filter}
+                                label="Filter"
+                                onChangeValue={setFilter}
+                                fullWidth
+                            />
+                            {isTabletOrSmaller ? (
+                                <Inline space={16} fullWidth>
+                                    <IntegerField
+                                        name="size"
+                                        value={String(size || 0)}
+                                        label="Size (px)"
+                                        onChangeValue={(v) => setSize(Number(v) || 0)}
+                                    />
+                                    <Stack space="between">
+                                        <Inline space="between">
+                                            <div style={{width: '160px'}}>{lightCheckbox}</div>
+                                            <div style={{width: '160px'}}>{regularCheckbox}</div>
+                                            <div style={{width: '160px'}}>{filledCheckbox}</div>
+                                        </Inline>
+                                        <Inline space="between">
+                                            <div style={{width: '160px'}}>{inverseCheckbox}</div>
+                                            <div style={{width: '160px'}}>{showNamesCheckbox}</div>
+                                            <div style={{width: '160px'}}>{showIConBackgroundCheckbox}</div>
+                                        </Inline>
+                                    </Stack>
+                                </Inline>
+                            ) : (
+                                <Stack space={16}>
+                                    <IntegerField
+                                        name="size"
+                                        value={String(size || 0)}
+                                        label="Size (px)"
+                                        onChangeValue={(v) => setSize(Number(v) || 0)}
+                                        fullWidth
+                                    />
+                                    <Divider />
+                                    {lightCheckbox}
+                                    {regularCheckbox}
+                                    {filledCheckbox}
+                                    <Divider />
+                                    {inverseCheckbox}
+                                    {showNamesCheckbox}
+                                    {showIConBackgroundCheckbox}
+                                </Stack>
+                            )}
+                        </Stack>
+                    </Box>
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        width: isTabletOrSmaller ? 'auto' : 'calc(100% - 280px)',
+                        marginLeft: isTabletOrSmaller ? 'auto' : '280px',
+                    }}
+                >
+                    <ThemeVariant isInverse={isInverse}>
+                        <div style={{background: backgroundColor}}>
+                            {misticaIcons
+                                .filter(({name}) => filterIcon(name))
+                                .sort((a, b) => compareNames(a.name, b.name))
+                                .map((Icon) => (
                                     <div
                                         style={{
-                                            width: size,
-                                            margin: 'auto',
-                                            background: iconBackgroundColor,
-                                            fontSize: 0,
+                                            display: 'inline-block',
+                                            verticalAlign: 'top',
+                                            textAlign: 'center',
                                         }}
                                     >
-                                        <Icon size={size} />
+                                        <div
+                                            style={{
+                                                textAlign: 'center',
+                                                padding: 0,
+                                                width: 173,
+                                            }}
+                                        >
+                                            <div
+                                                style={{
+                                                    margin: 'auto',
+                                                    padding: '56px 16px 24px 16px',
+                                                    borderRight: '1px solid' + colors.divider,
+                                                    borderBottom: '1px solid' + colors.divider,
+                                                    borderRadius: '0px',
+                                                    width: 140,
+                                                    height: 96,
+
+                                                    fontSize: 0,
+                                                }}
+                                            >
+                                                <Stack space="between">
+                                                    <div
+                                                        style={{
+                                                            width: size,
+                                                            margin: 'auto',
+                                                            background: iconBackgroundColor,
+                                                            fontSize: 0,
+                                                        }}
+                                                    >
+                                                        <Icon size={size} />
+                                                    </div>
+                                                    {showNames && <Text1>{breakName(Icon.name)}</Text1>}
+                                                </Stack>
+                                            </div>
+                                        </div>
                                     </div>
-                                    {showNames && (
-                                        <Box paddingTop={8}>
-                                            <Text size={13}>{breakName(Icon.name)}</Text>
-                                        </Box>
-                                    )}
-                                </div>
-                            </div>
-                        ))}
+                                ))}
+                        </div>
+                    </ThemeVariant>
                 </div>
-            </ThemeVariant>
+            </div>
         </div>
     );
 };
