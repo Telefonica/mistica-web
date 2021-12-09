f2a451cf40443211179c812fa6fb393d23050f4c
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2021-12-09 13:50:45 +0100

    WEB-407 update mistica icons

--- src/generated/mistica-icons/icon-file-illustrator-filled.tsx
+++ src/generated/mistica-icons/icon-file-illustrator-filled.tsx
@@ -10,7 +10,7 @@ import {useIsInverseVariant} from '../../theme-variant-context';
 
 import type {IconProps} from '../../utils/types';
 
-const IconFileFilled: React.FC<IconProps> = ({color, size = 24, children, ...rest}) => {
+const IconFileIllustratorFilled: React.FC<IconProps> = ({color, size = 24, children, ...rest}) => {
     const {colors} = useTheme();
     const isInverse = useIsInverseVariant();
     const fillColor = color ?? (isInverse ? colors.inverse : colors.neutralHigh);
@@ -24,4 +24,4 @@ const IconFileFilled: React.FC<IconProps> = ({color, size = 24, children, ...res
     );
 };
 
-export default IconFileFilled;
+export default IconFileIllustratorFilled;
