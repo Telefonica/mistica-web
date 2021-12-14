e471aef2d4e7f88e652895c40fe1a37c5c3bbd4a
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2021-12-14 18:10:28 +0100

    update patch

--- resolve/lib/sync.js
+++ resolve/lib/sync.js
@@ -138,7 +138,7 @@ module.exports = function resolveSync(x, options) {
             debug('loadAsFileSync, try extensions:', x + extensions[i]);
             var file = x + extensions[i];
             if (isFile(file)) {
-                console.log('loadAsFileSync, found:', file);
+                debug('loadAsFileSync, found:', file);
                 return file;
             }
         }
