044d1b086e6564a9bb21dd42965406a89247919f
Sergio Fernandez Jimenez <sergio.fernandezjimenez@telefonica.com>
2022-01-05 13:45:07 +0100

    PoC autocomplete

--- src/__stories__/prueba-story.tsx
+++ src/__stories__/prueba-story.tsx
@@ -1,19 +1,24 @@
 import * as React from 'react';
+import classNames from 'classnames';
 import {StorySection} from './helpers';
 import {useTheme, Text3, TextField, Menu, Touchable, Box, Text1, createUseStyles, Form} from '..';
+import {DOWN, ENTER, SPACE, UP} from '../utils/key-codes';
+import {cancelEvent} from '../utils/dom';
+import {combineRefs} from '../utils/common';
 
 export default {
     title: 'Components/Prueba/Prueba',
 };
 
-const useStyles = createUseStyles(({colors, mq}) => ({
+const useStyles = createUseStyles(({mq}) => ({
     hover: {
         [mq.supportsHover]: {
             '&:hover': {
-                background: colors.backgroundAlternative,
+                backgroundColor: 'rgba(0, 0, 0, 0.08)',
             },
         },
     },
+    selected: {backgroundColor: 'rgba(0, 0, 0, 0.08)'},
 }));
 
 const OPTIONS = [
@@ -29,16 +34,155 @@ const OPTIONS = [
         text: 'Patata 3 (checking this will close the menu)',
         value: 'patata',
     },
+    {
+        text: 'Option 4',
+        value: 'option4',
+    },
+    {
+        text: 'Option 5',
+        value: 'option5',
+    },
+    {
+        text: 'Option 6',
+        value: 'option6',
+    },
+    {
+        text: 'Option 7',
+        value: 'option7',
+    },
 ];
 
-export const PruebaStory: StoryComponent = () => {
-    const {colors} = useTheme();
+type OptionsProps = {
+    options: ReadonlyArray<{text: string; value: string}>;
+    selectedValue: string;
+    setFilterValue: any;
+    setSelectedValue: any;
+    className: string;
+    closeMenu: () => void;
+    elementRef: React.Ref<HTMLDivElement>;
+};
+
+const Options = ({
+    className,
+    options,
+    selectedValue,
+    setSelectedValue,
+    setFilterValue,
+    closeMenu,
+    elementRef,
+}: OptionsProps) => {
     const classes = useStyles();
+    const {colors} = useTheme();
+    const scrollRef = React.useRef<HTMLDivElement>();
+    const optionRefs = React.useRef(new Map<string, HTMLDivElement>());
+    const [tentativeValueState, setTentativeValueState] = React.useState('');
+
+    const scrollIntoTargetListElement = (targetValueState: string) => {
+        const menuClientRect = scrollRef.current?.getBoundingClientRect();
+        if (menuClientRect && targetValueState && optionRefs.current.has(targetValueState)) {
+            const itemRef = optionRefs.current.get(targetValueState);
+            const clientRect = itemRef?.getBoundingClientRect();
+            if (
+                clientRect &&
+                clientRect.top + clientRect.height / 2 >= menuClientRect.top + menuClientRect.height
+            ) {
+                itemRef?.scrollIntoView();
+                return;
+            }
+            if (clientRect && clientRect.top + clientRect.height / 2 <= menuClientRect.top) {
+                itemRef?.scrollIntoView(false);
+            }
+        }
+    };
+
+    React.useEffect(() => {
+        const updateTentativeValueState = (e: KeyboardEvent) => {
+            const keyToOperand: Record<number, 1 | -1 | undefined> = {[UP]: -1, [DOWN]: 1};
+            const operand = keyToOperand[e.keyCode];
+            if (operand) {
+                cancelEvent(e);
+                const newTentativeValueState =
+                    options[options.findIndex(({value}) => value === tentativeValueState) + operand]?.value ??
+                    tentativeValueState;
+                setTentativeValueState(newTentativeValueState);
+                scrollIntoTargetListElement(newTentativeValueState);
+            }
+        };
+        const handleKeyDown = (e: KeyboardEvent) => {
+            if (e.keyCode === ENTER || e.keyCode === SPACE) {
+                cancelEvent(e);
+                if (
+                    options.findIndex(({value}) => value === tentativeValueState) !== -1 &&
+                    tentativeValueState !== selectedValue
+                ) {
+                    setSelectedValue(tentativeValueState);
+                    setFilterValue(tentativeValueState);
+                }
+                closeMenu();
+            }
+
+            updateTentativeValueState(e);
+        };
+        document.addEventListener('keydown', handleKeyDown, false);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown, false);
+        };
+    });
+
+    return (
+        <div ref={combineRefs(elementRef, scrollRef)} className={className}>
+            {options.map((option) => (
+                <Box
+                    paddingX={16}
+                    paddingY={8}
+                    key={option.value}
+                    className={classNames(classes.hover, {
+                        [classes.selected]: option.value === tentativeValueState,
+                    })}
+                    ref={(liRef) => {
+                        if (liRef) {
+                            optionRefs.current.set(option.value, liRef);
+                        } else {
+                            optionRefs.current.delete(option.value);
+                        }
+                    }}
+                >
+                    <Touchable
+                        onPress={() => {
+                            setSelectedValue(option.value);
+                            setFilterValue(option.value);
+                            closeMenu();
+                        }}
+                    >
+                        <Text1 as="p" regular>
+                            {option.value}
+                        </Text1>
+                        <Text1 regular color={colors.textSecondary}>
+                            {option.text}
+                        </Text1>
+                    </Touchable>
+                </Box>
+            ))}
+            {options.length === 0 ? (
+                <Box paddingX={16} paddingY={8}>
+                    <Text3 regular color={colors.textSecondary}>
+                        No option
+                    </Text3>
+                </Box>
+            ) : null}
+        </div>
+    );
+};
+
+export const PruebaStory: StoryComponent = () => {
     const [options, setOptions] = React.useState(OPTIONS);
     const [filterValue, setFilterValue] = React.useState('');
     const [selectedValue, setSelectedValue] = React.useState('');
 
-    const filterOptions = (valueInput: string) => {
+    const filterOptions = (valueInput: string, openMenu: () => void, isMenuOpen: boolean) => {
+        if (!isMenuOpen) {
+            openMenu();
+        }
         setFilterValue(valueInput);
     };
 
@@ -56,57 +200,36 @@ export const PruebaStory: StoryComponent = () => {
     }, [filterValue]);
 
     return (
-        <>
-            <StorySection title="Prueba">
-                <Form onSubmit={() => {}}>
-                    <Menu
-                        renderTarget={({ref, onPress}) => (
-                            <Touchable elementRef={ref} onPress={onPress}>
-                                <TextField
-                                    fullWidth
-                                    value={filterValue}
-                                    name="prueba"
-                                    label="Prueba"
-                                    onChangeValue={filterOptions}
-                                    onBlur={checkIfRightOption}
-                                    autoComplete="off"
-                                />
-                            </Touchable>
-                        )}
-                        renderMenu={({ref, className, close}) => (
-                            <div ref={ref} className={className}>
-                                {options.map((option) => (
-                                    <Box paddingX={16} paddingY={8} key={option.value}>
-                                        <Touchable
-                                            className={classes.hover}
-                                            onPress={() => {
-                                                setSelectedValue(option.value);
-                                                setFilterValue(option.value);
-                                                close();
-                                            }}
-                                        >
-                                            <Text1 as="p" regular>
-                                                {option.value}
-                                            </Text1>
-                                            <Text1 regular color={colors.textSecondary}>
-                                                {option.text}
-                                            </Text1>
-                                        </Touchable>
-                                    </Box>
-                                ))}
-                                {options.length === 0 ? (
-                                    <Box paddingX={16} paddingY={8}>
-                                        <Text3 regular color={colors.textSecondary}>
-                                            No option
-                                        </Text3>
-                                    </Box>
-                                ) : null}
-                            </div>
-                        )}
-                    />
-                </Form>
-            </StorySection>
-        </>
+        <StorySection title="Prueba">
+            <Form onSubmit={() => {}}>
+                <Menu
+                    renderTarget={({ref, onPress, isMenuOpen}) => (
+                        <TextField
+                            ref={ref}
+                            onFocus={onPress}
+                            fullWidth
+                            value={filterValue}
+                            name="prueba"
+                            label="Prueba"
+                            onChangeValue={(value) => filterOptions(value, onPress, isMenuOpen)}
+                            onBlur={checkIfRightOption}
+                            autoComplete="off"
+                        />
+                    )}
+                    renderMenu={({ref, className, close}) => (
+                        <Options
+                            elementRef={ref}
+                            options={options}
+                            className={className}
+                            selectedValue={selectedValue}
+                            setFilterValue={setFilterValue}
+                            setSelectedValue={setSelectedValue}
+                            closeMenu={close}
+                        />
+                    )}
+                />
+            </Form>
+        </StorySection>
     );
 };
 
--- src/text-field.tsx
+++ src/text-field.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {useFieldProps} from './form-context';
 import TextFieldBase from './text-field-base';
+import {combineRefs} from './utils/common';
 
 import type {CommonFormFieldProps} from './text-field-base';
 
@@ -12,69 +13,77 @@ export interface TextFieldProps extends CommonFormFieldProps {
     getSuggestions?: (value: string) => Array<string>;
 }
 
-const TextField: React.FC<TextFieldProps> = ({
-    disabled,
-    error,
-    helperText,
-    name,
-    optional,
-    validate,
-    onChangeValue,
-    onChange,
-    value,
-    defaultValue,
-    onBlur: onBlurProp,
-    onFocus: onFocusProp,
-    ...rest
-}) => {
-    const inputRef = React.useRef<HTMLInputElement | null>(null);
-    const processValue = (v: string) => v;
+const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
+    (
+        {
+            disabled,
+            error,
+            helperText,
+            name,
+            optional,
+            validate,
+            onChangeValue,
+            onChange,
+            value,
+            defaultValue,
+            onBlur: onBlurProp,
+            onFocus: onFocusProp,
+            ...rest
+        },
+        ref
+    ) => {
+        const inputRef = React.useRef<HTMLInputElement | null>(null);
+        const processValue = (v: string) => v;
 
-    const onBlur = (event: React.FocusEvent<Element>) => {
-        if (rest.multiline && inputRef.current) {
-            // scroll to start
-            inputRef.current.scrollTop = 0;
-        }
-        onBlurProp?.(event);
-    };
+        const onBlur = (event: React.FocusEvent<Element>) => {
+            if (rest.multiline && inputRef.current) {
+                // scroll to start
+                inputRef.current.scrollTop = 0;
+            }
+            onBlurProp?.(event);
+        };
 
-    const onFocus = (event: React.FocusEvent<Element>) => {
-        if (rest.multiline) {
-            setTimeout(() => {
-                if (inputRef.current) {
-                    // place caret to the end
-                    const value = inputRef.current.value;
-                    inputRef.current.value = '';
-                    inputRef.current.value = value;
-                    // scroll to end
-                    inputRef.current.scrollTop = inputRef.current.scrollHeight;
-                }
-            }, 0);
-        }
-        onFocusProp?.(event);
-    };
+        const onFocus = (event: React.FocusEvent<Element>) => {
+            if (rest.multiline) {
+                setTimeout(() => {
+                    if (inputRef.current) {
+                        // place caret to the end
+                        const value = inputRef.current.value;
+                        inputRef.current.value = '';
+                        inputRef.current.value = value;
+                        // scroll to end
+                        inputRef.current.scrollTop = inputRef.current.scrollHeight;
+                    }
+                }, 0);
+            }
+            onFocusProp?.(event);
+        };
 
-    const fieldProps = useFieldProps({
-        name,
-        value,
-        defaultValue,
-        processValue,
-        helperText,
-        optional,
-        error,
-        disabled,
-        onBlur,
-        validate,
-        onChange,
-        onChangeValue,
-    });
+        const fieldProps = useFieldProps({
+            name,
+            value,
+            defaultValue,
+            processValue,
+            helperText,
+            optional,
+            error,
+            disabled,
+            onBlur,
+            validate,
+            onChange,
+            onChangeValue,
+        });
 
-    const setInputRef = (field: HTMLInputElement | null) => {
-        inputRef.current = field;
-        fieldProps.inputRef(field);
-    };
-
-    return <TextFieldBase {...rest} {...fieldProps} inputRef={setInputRef} onFocus={onFocus} type="text" />;
-};
+        return (
+            <TextFieldBase
+                {...rest}
+                {...fieldProps}
+                inputRef={combineRefs(inputRef, fieldProps.inputRef, ref)}
+                onFocus={onFocus}
+                type="text"
+            />
+        );
+    }
+);
 
 export default TextField;
