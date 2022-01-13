bdbd0bdbca91c6e2170a064167dd86ac1564650c
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2022-01-13 12:55:26 +0100

    WEB-396 Video component

--- src/__stories__/video-story.tsx
+++ src/__stories__/video-story.tsx
@@ -1,15 +1,58 @@
 import * as React from 'react';
-import {Video} from '..';
+import {Stack, ButtonPrimary, Inline, SectionTitle, Video} from '..';
 
 export default {
     title: 'Components/Others/Video',
     component: Video,
 };
 
-export const Default: StoryComponent = () => (
-    <div data-testid="video">
-        <Video url="https://fr-cert1-es.mytelco.io/2O4-xBJqiMlAfLkseq8RkXs_mv2ACV7Hnt20HqXxNl-mK7KLI3M2dAw" />
-    </div>
-);
+const VIDEO_SRC = 'https://fr-cert1-es.mytelco.io/2O4-xBJqiMlAfLkseq8RkXs_mv2ACV7Hnt20HqXxNl-mK7KLI3M2dAw';
+const POSTER_SRC = 'https://i.imgur.com/Fu7RiuY.jpg';
+
+export const Default: StoryComponent = () => {
+    const videoRef = React.useRef<HTMLVideoElement | null>(null);
+    return (
+        <Stack space={64} data-testid="video">
+            <Stack space={16}>
+                <SectionTitle>Auto Play</SectionTitle>
+                <Video width={480} src={VIDEO_SRC} />
+            </Stack>
+
+            <Stack space={16}>
+                <SectionTitle>Manual Play</SectionTitle>
+                <Video
+                    poster={POSTER_SRC}
+                    width={480}
+                    height={480 / 2.4}
+                    ref={videoRef}
+                    autoPlay={false}
+                    src={{src: VIDEO_SRC, type: 'video/mp4'}}
+                />
+                <Inline space={16}>
+                    <ButtonPrimary
+                        small
+                        onPress={() => {
+                            if (videoRef.current) {
+                                videoRef.current.currentTime = 0; // play from the beginning
+                                videoRef.current.play();
+                            }
+                        }}
+                    >
+                        Play
+                    </ButtonPrimary>
+                    <ButtonPrimary
+                        small
+                        onPress={() => {
+                            videoRef.current?.pause();
+                            videoRef.current?.load(); // to force showing the poster again
+                        }}
+                    >
+                        Stop
+                    </ButtonPrimary>
+                </Inline>
+            </Stack>
+        </Stack>
+    );
+};
 
 Default.storyName = 'Video';
--- src/video.tsx
+++ src/video.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {createUseStyles} from './jss';
+import {combineRefs} from './utils/common';
 
 const useStyles = createUseStyles(() => ({
     video: {
@@ -10,59 +11,67 @@ const useStyles = createUseStyles(() => ({
     },
 }));
 
-type Source = {
-    type?: string; // video/webm, video/mp4, etc.
+type VideoSource = {
     src: string;
+    type?: string; // video/webm, video/mp4...
 };
 
 type Props = {
     width?: number | string;
     height?: number | string;
     /** accepts multiple sources */
-    src: string | Array<string> | Source | Array<Source>;
+    src: string | Array<string> | VideoSource | Array<VideoSource>;
     /** defaults to true */
     loop?: boolean;
     /** defaults to true */
     muted?: boolean;
     /** defaults to true */
     autoPlay?: boolean;
-    fallbackImageUrl?: string;
+    poster?: string;
     children?: void;
 };
 
-const Video: React.FC<Props> = ({
-    width,
-    height,
-    fallbackImageUrl,
-    autoPlay = true,
-    muted = true,
-    loop = true,
-}) => {
-    const classes = useStyles();
-    const videoRef = React.useRef<HTMLVideoElement>(null);
+const Video = React.forwardRef<HTMLVideoElement, Props>(
+    ({width, height, src, poster, autoPlay = true, muted = true, loop = true}, ref) => {
+        const classes = useStyles();
+        const videoRef = React.useRef<HTMLVideoElement | null>(null);
 
-    React.useEffect(() => {
-        if (autoPlay) {
-            videoRef.current?.play();
-        }
-    });
+        React.useEffect(() => {
+            if (autoPlay && !videoRef.current?.paused) {
+                videoRef.current?.play();
+            }
+        }, [autoPlay]);
 
-    return (
-        <video
-            ref={videoRef}
-            playsInline
-            disablePictureInPicture
-            disableRemotePlayback
-            preload="auto"
-            autoPlay={autoPlay}
-            muted={muted}
-            loop={loop}
-            width={width}
-            height={height}
-            poster={fallbackImageUrl}
-            className={classes.video}
-        ></video>
-    );
-};
+        // normalize sources
+        const sources: Array<VideoSource> = (Array.isArray(src) ? src : [src]).map((source) => {
+            if (typeof source === 'string') {
+                return {src: source};
+            } else {
+                return source;
+            }
+        });
+
+        return (
+            <video
+                ref={combineRefs(ref, videoRef)}
+                playsInline
+                disablePictureInPicture
+                disableRemotePlayback
+                preload="auto"
+                autoPlay={autoPlay}
+                muted={muted}
+                loop={loop}
+                width={width}
+                height={height}
+                poster={poster}
+                className={classes.video}
+            >
+                {sources.map(({src, type}, index) => (
+                    <source key={index} src={src} type={type} />
+                ))}
+            </video>
+        );
+    }
+);
 
 export default Video;
