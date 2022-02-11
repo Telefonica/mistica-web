bc8da4bda217134a9c02ab7538268ecc5b2f6b51
Abel Toledano <atoledano@tuenti.com>
2022-02-11 13:05:18 +0100

    fix(Dialog): remove unstable_disableHistoryUpdateInDialogs (#415)

--- src/theme-context-provider.tsx
+++ src/theme-context-provider.tsx
@@ -123,7 +123,6 @@ const ThemeContextProvider: React.FC<Props> = ({theme, children}) => {
             isDarkMode: isDarkModeEnabled,
             isIos: getPlatform(platformOverrides) === 'ios',
             useHrefDecorator: theme.useHrefDecorator ?? useDefaultHrefDecorator,
-            unstable_disableHistoryUpdateInDialogs: !!theme.unstable_disableHistoryUpdateInDialogs,
         };
     }, [theme, isOsDarkModeEnabled]);
 
