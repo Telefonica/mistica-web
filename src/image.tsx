2eb7ab48d5efd5ea2a40b93c6410e47dfb5561d3
Yayo <yceballost@gmail.com>
2022-02-14 16:20:23 +0100

    adding 4:3 to image

--- src/image.tsx
+++ src/image.tsx
@@ -24,12 +24,13 @@ const useStyles = createUseStyles(() => ({
     },
 }));
 
-export type AspectRatio = '1:1' | '16:9' | '7:10';
+export type AspectRatio = '1:1' | '16:9' | '7:10' | '4:3';
 
 export const RATIO = {
     '1:1': 1,
     '16:9': 16 / 9,
     '7:10': 7 / 10,
+    '4:3': 4 / 3,
 };
 
 export type ImageProps = {
