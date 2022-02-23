0b01b6ed67bb0bcb10927ffb44812095abacc233
Pedro Ladaria <pladaria@tuenti.com>
2022-02-23 11:39:05 +0100

    feat(webview-bridge): upgrade webview-bridge dependency (#420)
    Co-authored-by: Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>

--- src/utils/types.tsx
+++ src/utils/types.tsx
@@ -1,12 +1,6 @@
 export type {RendersElement, RendersNullableElement} from './renders-element';
 
-export type TrackingEvent = {
-    readonly [key: string]: unknown;
-    readonly category: string;
-    readonly action: string;
-    readonly label?: string;
-    readonly value?: number;
-};
+export type {TrackingEvent} from '@tef-novum/webview-bridge';
 
 export type DataAttributes = {[name: string]: string | number | boolean | undefined};
 
