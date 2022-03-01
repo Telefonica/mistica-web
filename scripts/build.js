bd03063b2305f0e0428dc46f5a46f3de006cc878
Abel Toledano <atoledano@tuenti.com>
2022-03-01 11:43:23 +0100

    chore(size-stats): replace commited json by gh workflow (#427)

--- scripts/build.js
+++ scripts/build.js
@@ -1,27 +1,9 @@
 const childProcess = require('child_process');
-const rimraf = require('rimraf');
+const compile = require('./compile');
 const execSync = childProcess.execSync;
-const genSizeStats = require('./size-stats');
 
 const run = (command) => {
     execSync(command, {stdio: 'inherit'});
-};
-
-const compile = () => {
-    run(`yarn swc src --out-dir dist-es --extensions .tsx`);
-
-    [
-        '__tests__',
-        '__acceptance_tests__',
-        '__screenshot_tests__',
-        '__type_tests__',
-        '__stories__',
-        'test-utils',
-    ].forEach((dirName) => {
-        rimraf.sync(`dist-es/${dirName}`);
-    });
-
-    run(`yarn swc dist-es --out-dir dist -C module.type=commonjs`);
 };
 
 (async () => {
@@ -36,7 +18,4 @@ const compile = () => {
 
     console.log('Flow check...');
     run('yarn flow check');
-
-    console.log('Generating size stats...');
-    genSizeStats();
 })();
