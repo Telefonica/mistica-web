6e5816630edc587572d22dea9789957b8dbd7a89
Rubén Morchón Poveda <ruben.morchon.poveda@outlook.com>
2022-03-17 17:51:09 +0100

    fix(Row): checkbox onChange always returning true (#428)

--- src/list.tsx
+++ src/list.tsx
@@ -374,7 +374,7 @@ const useControlState = ({
             setIsChecked(!isChecked);
         }
         if (onChange) {
-            onChange(!value);
+            onChange(isControlledByParent ? !value : !isChecked);
         }
     };
 
