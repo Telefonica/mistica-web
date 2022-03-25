a2c40537ddeef8b1b5889d7a0cb5a742deb4f252
Abel Toledano <atoledano@tuenti.com>
2022-03-25 10:47:51 +0100

    fix ts defs and jsdom config

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     ...require('./jest.base.config'),
     displayName: 'unit',
+    testEnvironment: 'jsdom',
     testMatch: ['**/__tests__/*-test.tsx'],
     testURL: 'http://test.tuenti.com',
     setupFilesAfterEnv: [require.resolve('./setup-test-env.tsx'), '@testing-library/jest-dom/extend-expect'],
