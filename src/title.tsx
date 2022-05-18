e1e836f539045cee7074cf4d84ec112ee15fac8c
Abel Toledano <atoledano@tuenti.com>
2022-05-18 15:22:02 +0200

    data attributes

--- src/title.tsx
+++ src/title.tsx
@@ -8,16 +8,15 @@ import {DataAttributes} from './utils/types';
 type TitleLayoutProps = {
     title: React.ReactElement;
     right?: React.ReactNode;
-    dataAttributes?: DataAttributes;
 };
 
-const TitleLayout = ({title, right, dataAttributes}: TitleLayoutProps): React.ReactElement => {
+const TitleLayout = ({title, right}: TitleLayoutProps): React.ReactElement => {
     if (!right) {
         return title;
     }
 
     return (
-        <Inline space="between" alignItems="baseline" dataAttributes={dataAttributes}>
+        <Inline space="between" alignItems="baseline">
             {title}
             <Box paddingLeft={16}>
                 <Text2 regular>{right}</Text2>
@@ -31,6 +30,7 @@ type TitleProps = {
     id?: string;
     right?: React.ReactNode;
     as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+    /** "data-" prefix is automatically added. For example, use "testid" instead of "data-testid" */
     dataAttributes?: DataAttributes;
 };
 
@@ -40,12 +40,18 @@ export const Title1 = ({children, as = 'h3', id, right, dataAttributes}: TitlePr
     return (
         <TitleLayout
             title={
-                <Text1 color={theme.colors.textSecondary} transform="uppercase" medium as={as} id={id}>
+                <Text1
+                    color={theme.colors.textSecondary}
+                    transform="uppercase"
+                    medium
+                    as={as}
+                    id={id}
+                    dataAttributes={dataAttributes}
+                >
                     {children}
                 </Text1>
             }
             right={right}
-            dataAttributes={dataAttributes}
         />
     );
 };
@@ -54,12 +60,11 @@ export const Title2 = ({children, as = 'h3', id, right, dataAttributes}: TitlePr
     return (
         <TitleLayout
             title={
-                <Text5 as={as} id={id}>
+                <Text5 as={as} id={id} dataAttributes={dataAttributes}>
                     {children}
                 </Text5>
             }
             right={right}
-            dataAttributes={dataAttributes}
         />
     );
 };
