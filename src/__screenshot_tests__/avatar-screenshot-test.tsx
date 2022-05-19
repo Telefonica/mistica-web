ad72bba288fbb64a7172d3e8d73a0125459824e2
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2022-05-19 17:48:19 +0200

    update test

--- src/__screenshot_tests__/avatar-screenshot-test.tsx
+++ src/__screenshot_tests__/avatar-screenshot-test.tsx
@@ -2,62 +2,45 @@ import {openStoryPage, screen} from '../test-utils';
 
 const sizes = [24, 40, 64, 96];
 
-test.each(sizes)('Avatar with numeric badge. Size: %d', async (size) => {
-    await openStoryPage({
-        id: 'components-avatar--default',
-        device: 'MOBILE_IOS',
-        args: {size, badge: '5'},
-    });
+test('Avatar with image and badge', async () => {
+    // simple badge
+    for (const size of sizes) {
+        await openStoryPage({
+            id: 'components-avatar--default',
+            device: 'MOBILE_IOS',
+            args: {size, badge: 'true'},
+        });
 
-    const element = await screen.findByTestId('avatar');
-    expect(await element.screenshot()).toMatchImageSnapshot();
+        const element = await screen.findByTestId('avatar');
+        expect(await element.screenshot()).toMatchImageSnapshot();
+    }
+
+    // numeric badge
+    for (const size of sizes) {
+        await openStoryPage({
+            id: 'components-avatar--default',
+            device: 'MOBILE_IOS',
+            args: {size, badge: '5'},
+        });
+
+        const element = await screen.findByTestId('avatar');
+        expect(await element.screenshot()).toMatchImageSnapshot();
+    }
 });
 
-test.each(sizes)('Avatar with simple badge. Size: %d', async (size) => {
-    await openStoryPage({
-        id: 'components-avatar--default',
-        device: 'MOBILE_IOS',
-        args: {size, badge: 'true'},
-    });
+test('Avatar with initials', async () => {
+    for (const size of sizes) {
+        await openStoryPage({
+            id: 'components-avatar--default',
+            device: 'MOBILE_IOS',
+            args: {initials: 'PL', size, src: ''},
+        });
 
-    const element = await screen.findByTestId('avatar');
-    expect(await element.screenshot()).toMatchImageSnapshot();
-});
+        const element = await screen.findByTestId('avatar');
+        expect(await element.screenshot()).toMatchImageSnapshot();
+    }
 
-test.each(sizes)('Avatar with initials. Size: %d', async (size) => {
-    await openStoryPage({
-        id: 'components-avatar--default',
-        device: 'MOBILE_IOS',
-        args: {initials: 'PL', size, src: ''},
-    });
-
-    const element = await screen.findByTestId('avatar');
-    expect(await element.screenshot()).toMatchImageSnapshot();
-});
-
-test.each(sizes)('Avatar with default icon. Size: %d', async (size) => {
-    await openStoryPage({
-        id: 'components-avatar--default',
-        device: 'MOBILE_IOS',
-        args: {initials: '', size, src: ''},
-    });
-
-    const element = await screen.findByTestId('avatar');
-    expect(await element.screenshot()).toMatchImageSnapshot();
-});
-
-test.each(sizes)('Avatar with custom icon. Size: %d', async (size) => {
-    await openStoryPage({
-        id: 'components-avatar--default',
-        device: 'MOBILE_IOS',
-        args: {initials: '', size, src: '', icon: 'IconFireRegular'},
-    });
-
-    const element = await screen.findByTestId('avatar');
-    expect(await element.screenshot()).toMatchImageSnapshot();
-});
-
-test('Avatar inverse with initials', async () => {
+    // inverse
     await openStoryPage({
         id: 'components-avatar--default',
         device: 'MOBILE_IOS',
@@ -68,13 +51,40 @@ test('Avatar inverse with initials', async () => {
     expect(await element.screenshot()).toMatchImageSnapshot();
 });
 
-test('Avatar inverse with icon', async () => {
-    await openStoryPage({
-        id: 'components-avatar--default',
-        device: 'MOBILE_IOS',
-        args: {initials: '', size: 64, src: '', icon: 'IconFireRegular', inverse: 'true'},
-    });
+test('Avatar with icon', async () => {
+    // default icon
+    for (const size of sizes) {
+        await openStoryPage({
+            id: 'components-avatar--default',
+            device: 'MOBILE_IOS',
+            args: {initials: '', size, src: ''},
+        });
 
-    const element = await screen.findByTestId('avatar');
-    expect(await element.screenshot()).toMatchImageSnapshot();
+        const element = await screen.findByTestId('avatar');
+        expect(await element.screenshot()).toMatchImageSnapshot();
+    }
+
+    // custom icon
+    {
+        await openStoryPage({
+            id: 'components-avatar--default',
+            device: 'MOBILE_IOS',
+            args: {initials: '', size: 64, src: '', icon: 'IconFireRegular'},
+        });
+
+        const element = await screen.findByTestId('avatar');
+        expect(await element.screenshot()).toMatchImageSnapshot();
+    }
+
+    // inverse
+    {
+        await openStoryPage({
+            id: 'components-avatar--default',
+            device: 'MOBILE_IOS',
+            args: {initials: '', size: 64, src: '', icon: 'IconFireRegular', inverse: 'true'},
+        });
+
+        const element = await screen.findByTestId('avatar');
+        expect(await element.screenshot()).toMatchImageSnapshot();
+    }
 });
