f1d01c091dff16f38ffe0c03ab8f676d2f1c83a0
Yayo <yceballost@gmail.com>
2022-08-05 13:52:58 +0200

    update icons from mistica-icons

--- src/generated/mistica-icons/icon-photo-camera-filled.tsx
+++ src/generated/mistica-icons/icon-photo-camera-filled.tsx
@@ -11,21 +11,36 @@ import {useIsInverseVariant} from '../../theme-variant-context';
 import type {IconProps} from '../../utils/types';
 
 const IconPhotoCameraFilled: React.FC<IconProps> = ({color, size = 24, children, ...rest}) => {
-    const {colors} = useTheme();
+    const {skinName, colors} = useTheme();
     const isInverse = useIsInverseVariant();
     const fillColor = color ?? (isInverse ? colors.inverse : colors.neutralHigh);
-    return (
-        <svg width={size} height={size} viewBox="0 0 24 24" role="presentation" {...rest}>
-            <path
-                d="M8.248 12.712a3.753 3.753 0 013.748-3.748 3.753 3.753 0 013.748 3.748 3.75 3.75 0 01-3.748 3.748 3.75 3.75 0 01-3.748-3.748z"
-                fill={fillColor}
-            />
-            <path
-                d="M17.648 6.288h2.204c1.18 0 2.144.96 2.144 2.144v10c0 1.184-.96 2.144-2.144 2.144H4.14a2.144 2.144 0 01-2.144-2.144v-10c0-1.184.96-2.144 2.144-2.144h2.2L8.992 3.64a.69.69 0 01.504-.212h5c.188 0 .372.076.508.212l2.644 2.648zM7.176 12.716c0 2.66 2.164 4.82 4.82 4.82s4.82-2.16 4.82-4.82-2.164-4.82-4.82-4.82a4.825 4.825 0 00-4.82 4.82z"
-                fill={fillColor}
-            />
-        </svg>
-    );
+    if (skinName.match(/^o2/i)) {
+        return (
+            <svg width={size} height={size} viewBox="0 0 24 24" role="presentation" {...rest}>
+                <path
+                    d="M8.248 12.712a3.753 3.753 0 013.748-3.748 3.753 3.753 0 013.748 3.748 3.75 3.75 0 01-3.748 3.748 3.75 3.75 0 01-3.748-3.748z"
+                    fill={fillColor}
+                />
+                <path
+                    d="M17.648 6.288h2.204c1.18 0 2.144.96 2.144 2.144v10c0 1.184-.96 2.144-2.144 2.144H4.14a2.144 2.144 0 01-2.144-2.144v-10c0-1.184.96-2.144 2.144-2.144h2.2L8.992 3.64a.69.69 0 01.504-.212h5c.188 0 .372.076.508.212l2.644 2.648zM7.176 12.716c0 2.66 2.164 4.82 4.82 4.82s4.82-2.16 4.82-4.82-2.164-4.82-4.82-4.82a4.825 4.825 0 00-4.82 4.82z"
+                    fill={fillColor}
+                />
+            </svg>
+        );
+    } else {
+        return (
+            <svg width={size} height={size} viewBox="0 0 24 24" role="presentation" {...rest}>
+                <path
+                    d="M11.597 11.36h.806a1.327 1.327 0 011.329 1.33v.805a1.33 1.33 0 01-1.33 1.33h-.805a1.327 1.327 0 01-1.33-1.33v-.806a1.33 1.33 0 011.33-1.329z"
+                    fill={fillColor}
+                />
+                <path
+                    d="M9.603 5.36a1.636 1.636 0 011.468-.913h1.69c.597 0 1.146.324 1.434.847l.722 1.313h5.345c.96 0 1.738.779 1.738 1.739v9.461c0 .96-.778 1.739-1.738 1.739H3.331A1.331 1.331 0 012 18.214V8.346c0-.96.778-1.739 1.738-1.739h5.25l.615-1.247zm1.994 4.773a2.556 2.556 0 00-2.556 2.556v.806a2.556 2.556 0 002.556 2.556h.806a2.556 2.556 0 002.556-2.556v-.806a2.556 2.556 0 00-2.556-2.556h-.806z"
+                    fill={fillColor}
+                />
+            </svg>
+        );
+    }
 };
 
 export default IconPhotoCameraFilled;
