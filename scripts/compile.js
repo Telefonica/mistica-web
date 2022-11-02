17e8571bbb10617e83b045485067a0fb3f0fe76d
Abel Toledano <atoledano@tuenti.com>
2022-11-02 16:03:12 +0100

    embed VE sytles into js

--- scripts/compile.js
+++ scripts/compile.js
@@ -1,4 +1,5 @@
 const childProcess = require('child_process');
+const {readFileSync, writeFileSync} = require('fs');
 const execSync = childProcess.execSync;
 
 const run = (command) => {
@@ -7,8 +8,12 @@ const run = (command) => {
 
 const compile = () => {
     run(`yarn vite build`);
-    run(`yarn swc dist-es --out-dir dist -C module.type=commonjs`);
+    let themeContextProviderJs = readFileSync('dist-es/theme-context-provider.js').toString();
+    const styles = readFileSync('dist-es/style.css').toString();
+    themeContextProviderJs = themeContextProviderJs.replace('"<MISTICA_CSS>"', `\`${styles}\``);
+    writeFileSync('dist-es/theme-context-provider.js', themeContextProviderJs);
     run(`cp dist-es/style.css css/mistica.css`);
+    run(`yarn swc dist-es --out-dir dist -C module.type=commonjs`);
 };
 
 if (require.main === module) {
