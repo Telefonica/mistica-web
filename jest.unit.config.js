3652de220c4461eccfa1f538fbaade8290605e1c
Sergio Fernandez Jimenez <sergio.fernandezjimenez@telefonica.com>
2022-11-04 13:49:34 +0100

    WEB-687 update jest to latest

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -3,6 +3,6 @@ module.exports = {
     displayName: 'unit',
     testEnvironment: 'jsdom',
     testMatch: ['**/__tests__/*-test.tsx'],
-    testURL: 'http://test.tuenti.com',
+    testEnvironmentOptions: {url: 'http://test.tuenti.com'},
     setupFilesAfterEnv: [require.resolve('./setup-test-env.tsx'), '@testing-library/jest-dom/extend-expect'],
 };
