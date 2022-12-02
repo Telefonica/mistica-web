2caece140f3cdd6a0231efcdc76ab8d2c56f802d
Abel Toledano <atoledano@tuenti.com>
2022-12-02 09:49:33 +0100

    feat(colors): added codemod to migrate from theme.colors to skinVars.colors (#608)

--- src/utils/color.tsx
+++ src/utils/color.tsx
@@ -17,7 +17,7 @@ export const fromHexToRgb = (hexColor: string): [number, number, number] => {
 };
 
 /**
- * @param color hex color or css variable with rgb components
+ * @param color hex color or css variable with rgb components (skinVars.rawColors)
  * @param alpha the alpha value to apply
  * @returns The same color with the alpha channel applied
  */
