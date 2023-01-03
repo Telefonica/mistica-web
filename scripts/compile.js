763550b284654c1787732f307ec5effdc200f771
Sergio <sergio.fernandezjimenez@telefonica.com>
2023-01-03 17:52:19 +0100

    feat(browserlist): update supported browsers (#630)

--- scripts/compile.js
+++ scripts/compile.js
@@ -8,7 +8,13 @@ const run = (command) => {
 const compile = () => {
     run(`yarn vite build`);
     run(`cp dist-es/style.css css/mistica.css`);
-    run(`yarn swc dist-es --out-dir dist-es`); // transpile to es5 (package.json browserslist)
+
+    // transpile to es5 (see .swcrc targets and package.json browserslist)
+    // Copy/paste from package.json "browserslist" as SWC has a bug and cannot read that config
+    // https://github.com/swc-project/swc/issues/3365
+    // "ios_saf" is "ios" only for SWC
+    run(`yarn swc dist-es --out-dir dist-es`);
+
     run(`yarn swc dist-es --out-dir dist -C module.type=commonjs`);
 };
 
