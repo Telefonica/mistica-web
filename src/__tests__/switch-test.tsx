7b60240339d258f4a0b86960ff4bf1033b0be42f
Abel Toledano <atoledano@tuenti.com>
2023-01-05 17:01:48 +0100

    feat(flow): remove flow types generation

--- src/__tests__/switch-test.tsx
+++ src/__tests__/switch-test.tsx
@@ -1,4 +1,3 @@
-// @flow
 import * as React from 'react';
 import {render, waitFor, fireEvent, screen} from '@testing-library/react';
 import {ButtonPrimary, Form, ThemeContextProvider, Switch} from '..';
