f15060634e50718920e434a97aa81077d98e5add
Sergio <sergio.fernandezjimenez@telefonica.com>
2023-02-17 10:22:15 +0100

    fix(Image): SSR issue (#664)

--- src/image.tsx
+++ src/image.tsx
@@ -3,13 +3,13 @@ import classnames from 'classnames';
 import {SkeletonRectangle} from './skeletons';
 import {AspectRatioElement} from './utils/aspect-ratio-support';
 import {getPrefixedDataAttributes} from './utils/dom';
-import IconImageRegular from './generated/mistica-icons/icon-image-regular';
 import {useIsInverseVariant} from './theme-variant-context';
 import {useTheme} from './hooks';
 import {VIVO_SKIN} from './skins/constants';
 import {sprinkles} from './sprinkles.css';
 import * as styles from './image.css';
 import {vars} from './skins/skin-contract.css';
+import {combineRefs} from './utils/common';
 
 import type {DataAttributes} from './utils/types';
 
@@ -65,11 +65,17 @@ const ImageError = ({noBorderRadius}: ImageErrorProps) => {
             {skinName === VIVO_SKIN ? (
                 <VivoLogo style={{width: '10%', minWidth: 24, maxWidth: 48}} />
             ) : (
-                <IconImageRegular
-                    color={isInverse ? vars.colors.inverse : vars.colors.neutralMedium}
-                    size=""
+                // We cannot use IconImageRegular because we want to avoid the usage of widht and height props to allow this icon to be responsive
+                <svg
+                    viewBox="0 0 24 24"
+                    role="presentation"
                     style={{width: '10%', minWidth: 24, maxWidth: 48}}
-                />
+                >
+                    <path
+                        fill={isInverse ? vars.colors.inverse : vars.colors.neutralMedium}
+                        d="M20.25 4.622c.832 0 1.591.664 1.591 1.393v11.98c0 .728-.76 1.392-1.591 1.392H3.746c-.863 0-1.591-.639-1.591-1.392V6.015c0-.754.728-1.393 1.59-1.393H20.25zm0 13.558c.193 0 .367-.146.39-.207l-.003-11.956a.555.555 0 00-.384-.182H3.746c-.21 0-.364.132-.387.19v11.973c.014.044.171.182.387.182H20.25zM6.152 9.068c0 .554.451 1.005 1.003 1.005a1.006 1.006 0 000-2.01c-.552 0-1.003.45-1.003 1.005zm1.003 2.215a2.216 2.216 0 010-4.43c1.218 0 2.207.993 2.207 2.215a2.213 2.213 0 01-2.207 2.215zm12.42 1.337l-3.18-2.44a.605.605 0 00-.742.005l-3.636 2.9-.879-.83a.6.6 0 00-.798-.025l-5.168 4.317a.607.607 0 00.386 1.07.59.59 0 00.384-.14l4.759-3.975 3.308 3.123c.241.23.625.221.852-.025a.605.605 0 00-.026-.854l-1.932-1.824 3.131-2.496 2.807 2.154a.603.603 0 00.846-.112.609.609 0 00-.112-.848z"
+                    />
+                </svg>
             )}
         </div>
     );
@@ -96,8 +102,8 @@ export type ImageProps = {
     children?: void;
     dataAttributes?: DataAttributes;
     noBorderRadius?: boolean;
-    onError?: (event: React.SyntheticEvent) => void;
-    onLoad?: (event: React.SyntheticEvent) => void;
+    onError?: () => void;
+    onLoad?: () => void;
     loadingFallback?: boolean;
     errorFallback?: boolean;
 };
@@ -118,6 +124,7 @@ const Image = React.forwardRef<HTMLImageElement, ImageProps>(
         },
         ref
     ) => {
+        const imageRef = React.useRef<HTMLImageElement>();
         const noBorderRadiusContext = useDisableBorderRadius();
         const noBorderSetting = noBorderRadius ?? noBorderRadiusContext;
         const [isError, setIsError] = React.useState(false);
@@ -129,6 +136,16 @@ const Image = React.forwardRef<HTMLImageElement, ImageProps>(
         const withLoadingFallback = loadingFallback && !!(ratio !== 0 || (props.width && props.height));
         const withErrorFallback = errorFallback && !!(ratio !== 0 || (props.width && props.height));
 
+        const onLoadHandler = React.useCallback(() => {
+            setIsError(false);
+            setIsLoading(false);
+            setTimeout(() => {
+                setHideLoadingFallback(true);
+            }, styles.FADE_IN_DURATION_MS);
+
+            onLoad?.();
+        }, [onLoad]);
+
         const img = (
             // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/309
             // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
@@ -137,7 +154,7 @@ const Image = React.forwardRef<HTMLImageElement, ImageProps>(
                 style={{
                     ...(isLoading && withLoadingFallback ? {opacity: 0} : {opacity: 1}),
                 }}
-                ref={ref}
+                ref={combineRefs(imageRef, ref)}
                 src={src}
                 className={classnames(
                     styles.image,
@@ -147,22 +164,23 @@ const Image = React.forwardRef<HTMLImageElement, ImageProps>(
                     })
                 )}
                 alt={alt}
-                onError={(event) => {
+                onError={() => {
                     setIsError(true);
                     setIsLoading(false);
                     setHideLoadingFallback(true);
-                    onError?.(event);
+                    onError?.();
                 }}
-                onLoad={(event) => {
-                    setIsError(false);
-                    setIsLoading(false);
-                    setTimeout(() => {
-                        setHideLoadingFallback(true);
-                    }, styles.FADE_IN_DURATION_MS);
-                    onLoad?.(event);
-                }}
+                onLoad={onLoadHandler}
             />
         );
+
+        React.useEffect(() => {
+            // Needed because there is some race condition with SSR and onLoad events
+            // https://github.com/facebook/react/issues/15446
+            if (imageRef.current?.complete) {
+                onLoadHandler();
+            }
+        }, [onLoadHandler]);
 
         return (
             <AspectRatioElement
