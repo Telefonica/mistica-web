9499eaf8aeacfd5e3d55b4458cbc347ee38dee9d
Sergio Fernandez Jimenez <sergio.fernandezjimenez@telefonica.com>
2023-03-22 10:15:49 +0100

    WEB-792 update tablet design

--- src/hero.tsx
+++ src/hero.tsx
@@ -125,11 +125,11 @@ const Hero = React.forwardRef<HTMLDivElement, HeroProps>(
         {height, background = 'default', media, desktopMediaPosition = 'left', dataAttributes, ...rest},
         ref
     ) => {
-        const {isTabletOrSmaller} = useScreenSize();
+        const {isTabletOrBigger} = useScreenSize();
         const isInsideSlideShow = useIsInsideSlideshowContext();
         const isInverse = background === 'brand' || background === 'brand-secondary';
 
-        if (isTabletOrSmaller) {
+        if (!isTabletOrBigger) {
             return (
                 <MediaBorderRadiusProvider value={false}>
                     <div
