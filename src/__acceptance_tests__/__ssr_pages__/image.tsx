9e39f3c7ed59a77a1843c32f69a34601d32b01f1
Sergio Fernandez Jimenez <sergio.fernandezjimenez@telefonica.com>
2023-04-03 10:54:46 +0200

    WEB-775 test upload images

--- src/__acceptance_tests__/__ssr_pages__/image.tsx
+++ src/__acceptance_tests__/__ssr_pages__/image.tsx
@@ -7,7 +7,7 @@ const ImageTest = (): JSX.Element => (
             <Title1>Image aspectRatio: 16:9</Title1>
             <Image
                 src="https://images.unsplash.com/photo-1622819584099-e04ccb14e8a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
-                aspectRatio="4:3"
+                aspectRatio="16:9"
                 width={200}
             />
         </Stack>
