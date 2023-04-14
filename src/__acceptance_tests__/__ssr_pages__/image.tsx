6d895931e9bf39f56b4b8334a7b3b3571a2a63c7
Abel Toledano <atoledano@tuenti.com>
2023-04-14 13:20:36 +0200

    use local images in ssr tests too

--- src/__acceptance_tests__/__ssr_pages__/image.tsx
+++ src/__acceptance_tests__/__ssr_pages__/image.tsx
@@ -5,11 +5,7 @@ const ImageTest = (): JSX.Element => (
     <ResponsiveLayout>
         <Stack space={48}>
             <Title1>Image aspectRatio: 16:9</Title1>
-            <Image
-                src="https://images.unsplash.com/photo-1622819584099-e04ccb14e8a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80"
-                aspectRatio="16:9"
-                width={200}
-            />
+            <Image src="using-vr.jpg" aspectRatio="16:9" width={200} />
         </Stack>
     </ResponsiveLayout>
 );
