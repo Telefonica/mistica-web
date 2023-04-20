0670f875393139149391b0882ae24869b61dab21
marcoskolodny <marcoskolodny@gmail.com>
2023-04-20 21:19:48 +0200

    fix prettier

--- src/card.css.ts
+++ src/card.css.ts
@@ -141,7 +141,7 @@ export const displayCardBackground = style([
         objectFit: 'cover',
         width: '100%',
         height: '100%',
-    })
+    }),
 ]);
 
 export const displayCardGradient = style({
