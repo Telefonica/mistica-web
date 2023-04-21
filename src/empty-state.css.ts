d9538123195a20cf071306d019b8895dff1f3aeb
Abel Toledano <atoledano@tuenti.com>
2023-04-21 13:53:42 +0200

    revert empty state border radius in desktop

--- src/empty-state.css.ts
+++ src/empty-state.css.ts
@@ -21,7 +21,7 @@ export const container = style([
                 maxWidth: 368,
             },
             [mq.desktopOrBigger]: {
-                borderRadius: skin.vars.borderRadii.container,
+                borderRadius: skin.vars.borderRadii.legacyDisplay,
                 flexDirection: 'row',
                 overflow: 'hidden',
                 backgroundColor,
