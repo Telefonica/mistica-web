8b43d9ce795bd2c59cc4a1b2298ccd8941a16932
marcoskolodny <marcoskolodny@gmail.com>
2023-04-25 13:50:07 +0200

    fix eslint error

--- src/__stories__/button-story.tsx
+++ src/__stories__/button-story.tsx
@@ -258,7 +258,7 @@ export const LoadingState: StoryComponent = () => {
         }, 2000);
     };
 
-    const handlePressWithPromise = () =>
+    const handlePressWithPromise: () => Promise<void> = () =>
         new Promise((resolve) => setTimeout(resolve, 3000));
 
     return (
