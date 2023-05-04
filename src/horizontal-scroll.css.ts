bb29b2d373a61160a3311e070b23657b0b1da2f7
Abel Toledano <atabel87@gmail.com>
2023-05-04 11:03:58 +0200

    use sprinkles

--- src/horizontal-scroll.css.ts
+++ src/horizontal-scroll.css.ts
@@ -1,5 +1,6 @@
 import {fallbackVar, globalStyle, style} from '@vanilla-extract/css';
 import {vars as responsiveLayoutVars} from './responsive-layout.css';
+import {sprinkles} from './sprinkles.css';
 
 const responsiveLayoutSideMargin = fallbackVar(responsiveLayoutVars.sideMargin, '0px');
 
@@ -10,17 +11,20 @@ const sideMarginSpacer = {
     width: responsiveLayoutSideMargin,
 };
 
-export const scroll = style({
-    display: 'flex',
-    overflowX: 'auto',
-    scrollbarWidth: 'none', // Hide scrollbar in FF
-    '::-webkit-scrollbar': {
-        display: 'none', // Hide scrollbar in Chrome/Safari
+export const scroll = style([
+    sprinkles({
+        display: 'flex',
+    }),
+    {
+        scrollbarWidth: 'none', // Hide scrollbar in FF
+        '::-webkit-scrollbar': {
+            display: 'none', // Hide scrollbar in Chrome/Safari
+        },
+        margin: `0 calc(${responsiveLayoutSideMargin} * -1)`,
+        ':after': sideMarginSpacer,
+        ':before': sideMarginSpacer,
     },
-    margin: `0 calc(${responsiveLayoutSideMargin} * -1)`,
-    ':after': sideMarginSpacer,
-    ':before': sideMarginSpacer,
-});
+]);
 
 globalStyle(`${scroll} > *`, {
     flexShrink: 0,
