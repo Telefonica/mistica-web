95f579e13ab483b969abe80b775b8fb57d60ec3e
daviteixeira-tlf <davi.teixeira.ext@telefonica.com>
2023-05-10 15:06:07 -0300

    add: ardActionsGroup

--- src/advanced-card.css.ts
+++ src/advanced-card.css.ts
@@ -1,6 +1,8 @@
 import {style} from '@vanilla-extract/css';
 import {sprinkles} from './sprinkles.css';
 import * as mq from './media-queries.css';
+import {vars} from './skins/skin-contract.css';
+import {applyAlpha} from './utils/color';
 
 export const actions = style([
     sprinkles({display: 'flex', flex: 1, justifyContent: 'space-between'}),
@@ -42,6 +44,53 @@ export const dataCard = style([
                 paddingBottom: 32,
                 paddingLeft: 24,
                 paddingRight: 24,
+            },
+        },
+    },
+]);
+
+const cardActionBase = sprinkles({
+    width: 32,
+    height: 32,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '50%',
+});
+
+export const cardActionIconButton = sprinkles({
+    display: 'flex',
+    width: 48,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+});
+
+export const cardAction = style([
+    cardActionBase,
+    {
+        background: vars.colors.backgroundContainer,
+        transition: 'background-color 0.2s ease-in-out',
+        '@media': {
+            [mq.supportsHover]: {
+                ':hover': {
+                    background: vars.colors.backgroundAlternative,
+                },
+            },
+        },
+    },
+]);
+
+export const cardActionInverse = style([
+    cardActionBase,
+    {
+        background: applyAlpha(vars.rawColors.backgroundContainer, 0.7),
+        transition: 'background-color 0.2s ease-in-out',
+        '@media': {
+            [mq.supportsHover]: {
+                ':hover': {
+                    background: applyAlpha(vars.rawColors.backgroundContainer, 0.9),
+                },
             },
         },
     },
--- src/advanced-card.tsx
+++ src/advanced-card.tsx
@@ -4,7 +4,6 @@ import {sprinkles} from './sprinkles.css';
 import Stack from './stack';
 import * as styles from './advanced-card.css';
 import Image from './image';
-import IconMeatballFilled from './generated/mistica-icons/icon-meatball-filled';
 import Divider from './divider';
 import NegativeBox from './negative-box';
 import {Placeholder} from './placeholder';
@@ -12,11 +11,80 @@ import Text, {Text2} from './text';
 import Tag from './tag';
 import {useTheme} from './hooks';
 import {vars} from './skins/skin-contract.css';
-import {style} from '@vanilla-extract/css';
+import IconCloseRegular from './generated/mistica-icons/icon-close-regular';
+import IconButton from './icon-button';
+import Inline from './inline';
 
 import type {RendersNullableElement} from './utils/renders-element';
-import type {DataAttributes} from './utils/types';
+import type {DataAttributes, IconProps} from './utils/types';
 import type {ButtonPrimary, ButtonLink} from './button';
+
+type CardAction = {
+    label: string;
+    onPress: () => void;
+    Icon?: React.FC<IconProps>;
+    iconSize?: number;
+    iconColor?: string;
+    iconBackground?: string;
+    iconBackgroundInverse?: string;
+};
+
+type CardActionsGroupProps = {
+    actions: Array<CardAction>;
+    isInverse?: boolean;
+};
+
+const CardActionsGroup = ({actions, isInverse}: CardActionsGroupProps): JSX.Element => {
+    return (
+        <Inline space={0}>
+            {actions.map(
+                (
+                    {
+                        onPress,
+                        label,
+                        Icon,
+                        iconSize = 20,
+                        iconColor = vars.colors.neutralHigh,
+                        iconBackground = styles.cardAction,
+                        iconBackgroundInverse = styles.cardActionInverse,
+                    },
+                    index
+                ) =>
+                    Icon ? (
+                        <IconButton
+                            size={48}
+                            key={index}
+                            onPress={onPress}
+                            aria-label={label}
+                            className={styles.cardActionIconButton}
+                            style={{display: 'flex'}}
+                        >
+                            <div className={isInverse ? iconBackgroundInverse : iconBackground}>
+                                <Icon color={iconColor} size={iconSize} />
+                            </div>
+                        </IconButton>
+                    ) : (
+                        <div key={index} className={styles.cardActionIconButton} />
+                    )
+            )}
+        </Inline>
+    );
+};
+
+const useTopActions = (actions?: Array<CardAction>, onClose?: () => void) => {
+    const {texts} = useTheme();
+    const finalActions = actions ? [...actions] : [];
+
+    if (onClose) {
+        finalActions.push({
+            label: texts.closeButtonLabel,
+            onPress: onClose,
+            Icon: IconCloseRegular,
+        });
+    }
+
+    return finalActions;
+};
 
 type CardContentProps = {
     headline?: string | RendersNullableElement<typeof Tag>;
@@ -117,13 +185,13 @@ interface AdvancedDataCardProps {
 
     slot?: React.ReactNode;
 
-    showOptions?: boolean;
-    dataAttributes?: DataAttributes;
-
     button?: RendersNullableElement<typeof ButtonPrimary>;
     image?: string;
     footerText?: string;
     buttonLink?: RendersNullableElement<typeof ButtonLink>;
+
+    dataAttributes?: DataAttributes;
+    actions?: Array<CardAction>;
     'aria-label'?: string;
     onClose?: () => void;
 }
@@ -142,19 +210,21 @@ const AdvancedDataCard = React.forwardRef<HTMLDivElement, AdvancedDataCardProps>
             description,
             descriptionLinesMax,
 
-            showOptions,
-
             button,
             image,
             footerText,
             buttonLink,
 
             dataAttributes,
+            actions,
             'aria-label': ariaLabel,
             onClose,
         },
         ref
     ) => {
+        const finalActions = useTopActions(actions, onClose);
+
+        const hasAcations = finalActions?.length > 0;
         const hasIcon = !!icon;
         const hasButton = !!button;
         const hasImage = !!image;
@@ -163,7 +233,6 @@ const AdvancedDataCard = React.forwardRef<HTMLDivElement, AdvancedDataCardProps>
         const hasFooter = hasButton || hasImage || hasFooterText || hasButtonLink;
 
         const margin = {marginLeft: '16px'};
-
         const topActionsStylesWithIcon = {position: 'absolute', top: 8, right: 8, zIndex: 2} as const;
 
         return (
@@ -181,23 +250,29 @@ const AdvancedDataCard = React.forwardRef<HTMLDivElement, AdvancedDataCardProps>
                                 display: 'flex',
                             })}
                         >
-                            <Stack space={16} className={sprinkles({flex: 1})}>
-                                {hasIcon ? icon : null}
-                                <CardContent
-                                    headline={headline}
-                                    pretitle={pretitle}
-                                    pretitleLinesMax={pretitleLinesMax}
-                                    title={title}
-                                    titleLinesMax={titleLinesMax}
-                                    subtitle={subtitle}
-                                    subtitleLinesMax={subtitleLinesMax}
-                                    description={description}
-                                    descriptionLinesMax={descriptionLinesMax}
-                                />
-                            </Stack>
-                            {showOptions && (
+                            <div
+                                className={sprinkles({
+                                    paddingTop: 8,
+                                })}
+                            >
+                                <Stack space={16} className={sprinkles({flex: 1})}>
+                                    {hasIcon ? icon : null}
+                                    <CardContent
+                                        headline={headline}
+                                        pretitle={pretitle}
+                                        pretitleLinesMax={pretitleLinesMax}
+                                        title={title}
+                                        titleLinesMax={titleLinesMax}
+                                        subtitle={subtitle}
+                                        subtitleLinesMax={subtitleLinesMax}
+                                        description={description}
+                                        descriptionLinesMax={descriptionLinesMax}
+                                    />
+                                </Stack>
+                            </div>
+                            {hasAcations && (
                                 <div style={topActionsStylesWithIcon}>
-                                    <IconMeatballFilled />
+                                    <CardActionsGroup actions={finalActions} />
                                 </div>
                             )}
                         </div>
