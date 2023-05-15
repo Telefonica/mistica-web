ee04d56cb3887a04b8bbb2f3e54f09e6225f2e72
Bruno Cezar <bruno.guimaraes.ext@telefonica.com>
2023-05-15 14:28:15 -0300

    fix and adjustment code

--- src/advanced-fatura.tsx
+++ src/advanced-fatura.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Text2, {Text3, Text5, Text8} from './text';
-import {vars} from './skins/skin-contract.css';
+import Text2, { Text3, Text5, Text8 } from './text';
+import { vars } from './skins/skin-contract.css';
 import * as styles from './advanced-fatura.css';
 import classNames from 'classnames';
 import Box from './box';
@@ -22,28 +22,25 @@ export const AdvancedFatura: React.FC<faturaCardProps> = ({
     mainValue,
 }) => {
     return (
-        <>
-            <div className={styles.column}></div>
-            <Box>
-                <Inline space={'between'} alignItems="flex-end">
-                    <div className={styles.column}>
-                        <Text3 regular color={vars.colors.textPrimary}>
-                            {title}
-                        </Text3>
-                        <Text2 lineHeight={20} color={vars.colors.textSecondary}>
-                            {subtitle}
-                        </Text2>
-                        <Text2 lineHeight={20} color={vars.colors.textSecondary}>
-                            {description}
-                        </Text2>
-                    </div>
-                    <div className={classNames(styles.column, styles.secundaryContainer)}>
-                        <Text2 color={vars.colors.textSecondary}>{secundaryValue}</Text2>
-                        <Text5 color={vars.colors.brand}>{mainValue}</Text5>
-                    </div>
-                </Inline>
-            </Box>
-        </>
+        <Box>
+            <Inline space='between' alignItems="flex-end">
+                <div className={styles.column}>
+                    <Text3 regular color={vars.colors.textPrimary}>
+                        {title}
+                    </Text3>
+                    <Text2 lineHeight={20} color={vars.colors.textSecondary}>
+                        {subtitle}
+                    </Text2>
+                    <Text2 lineHeight={20} color={vars.colors.textSecondary}>
+                        {description}
+                    </Text2>
+                </div>
+                <div className={classNames(styles.column, styles.secundaryContainer)}>
+                    <Text2 color={vars.colors.textSecondary}>{secundaryValue}</Text2>
+                    <Text5 color={vars.colors.brand}>{mainValue}</Text5>
+                </div>
+            </Inline>
+        </Box>
     );
 };
 
--- src/advanced-saldo.tsx
+++ src/advanced-saldo.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Text2, {Text3, Text5, Text8} from './text';
-import {vars} from './skins/skin-contract.css';
+import Text2, { Text3, Text5, Text8 } from './text';
+import { vars } from './skins/skin-contract.css';
 import Box from './box';
 
 interface saltoCardProps {
@@ -10,29 +10,27 @@ interface saltoCardProps {
     description?: string;
 }
 
-export const AdvancedSaldo: React.FC<saltoCardProps> = ({title, value, subtitle, description}) => {
+export const AdvancedSaldo: React.FC<saltoCardProps> = ({ title, value, subtitle, description }) => {
     return (
-        <>
-            <Box>
-                <div
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}
-                >
-                    <Text3 regular color={vars.colors.textPrimary}>
-                        {title}
-                    </Text3>
-                    <Text8 color={vars.colors.brand}>{value}</Text8>
-                    <Text2 lineHeight={20} color={vars.colors.textSecondary}>
-                        {subtitle}
-                    </Text2>
-                    <Text2 lineHeight={20} color={vars.colors.textSecondary}>
-                        {description}
-                    </Text2>
-                </div>
-            </Box>
-        </>
+        <Box>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}
+            >
+                <Text3 regular color={vars.colors.textPrimary}>
+                    {title}
+                </Text3>
+                <Text8 color={vars.colors.brand}>{value}</Text8>
+                <Text2 lineHeight={20} color={vars.colors.textSecondary}>
+                    {subtitle}
+                </Text2>
+                <Text2 lineHeight={20} color={vars.colors.textSecondary}>
+                    {description}
+                </Text2>
+            </div>
+        </Box>
     );
 };
 
