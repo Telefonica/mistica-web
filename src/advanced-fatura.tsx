9785c30bd4bdbdd8b127a6aa13595484b639a2cb
Bruno Cezar <bruno.guimaraes.ext@telefonica.com>
2023-05-15 16:53:36 -0300

    text correction and optimization

--- src/advanced-fatura.tsx
+++ src/advanced-fatura.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import Text2, { Text3, Text5, Text8 } from './text';
+import { Text2, Text3, Text5 } from './text';
 import { vars } from './skins/skin-contract.css';
 import * as styles from './advanced-fatura.css';
 import classNames from 'classnames';
-import Box from './box';
 import Inline from './inline';
 
 interface faturaCardProps {
     title?: string;
     subtitle?: string;
     description?: string;
-    mainValue?: string;
+    mainValue: string;
     secundaryValue?: string;
 }
 
@@ -22,25 +21,23 @@ export const AdvancedFatura: React.FC<faturaCardProps> = ({
     mainValue,
 }) => {
     return (
-        <Box>
-            <Inline space='between' alignItems="flex-end">
-                <div className={styles.column}>
-                    <Text3 regular color={vars.colors.textPrimary}>
-                        {title}
-                    </Text3>
-                    <Text2 lineHeight={20} color={vars.colors.textSecondary}>
-                        {subtitle}
-                    </Text2>
-                    <Text2 lineHeight={20} color={vars.colors.textSecondary}>
-                        {description}
-                    </Text2>
-                </div>
-                <div className={classNames(styles.column, styles.secundaryContainer)}>
-                    <Text2 color={vars.colors.textSecondary}>{secundaryValue}</Text2>
-                    <Text5 color={vars.colors.brand}>{mainValue}</Text5>
-                </div>
-            </Inline>
-        </Box>
+        <Inline space='between' alignItems='flex-end'>
+            <div className={styles.column}>
+                <Text3 regular color={vars.colors.textPrimary}>
+                    {title}
+                </Text3>
+                <Text2 regular color={vars.colors.textSecondary}>
+                    {subtitle}
+                </Text2>
+                <Text2 regular color={vars.colors.textSecondary}>
+                    {description}
+                </Text2>
+            </div>
+            <div className={classNames(styles.column, styles.container)}>
+                <Text2 regular color={vars.colors.textSecondary}>{secundaryValue}</Text2>
+                <Text5 color={vars.colors.brand}>{mainValue}</Text5>
+            </div>
+        </Inline>
     );
 };
 
