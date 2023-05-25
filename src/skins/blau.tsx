ead261c1fed55b8612ac8bb433f0eee3b4b3f750
Flow <lifecycle-github@telefonica.com>
2023-05-25 11:30:49 +0200

    feat(skin): update design tokens (#737)
    Automated changes by
    [create-pull-request](https://github.com/peter-evans/create-pull-request)
    GitHub action
    
    ---------
    
    Co-authored-by: atabel <atabel@users.noreply.github.com>
    Co-authored-by: Abel Toledano <atoledano@tuenti.com>

--- src/skins/blau.tsx
+++ src/skins/blau.tsx
@@ -1,45 +1,40 @@
-import {applyAlpha} from '../utils/color';
 import {BLAU_SKIN} from './constants';
+import {applyAlpha} from '../utils/color';
 
-import type {GetKnownSkin} from './types';
-
-// https://github.com/Telefonica/mistica-design/blob/production/tokens/blau-constants.json
-// https://github.com/Telefonica/mistica-design/blob/production/tokens/blau-skin-schema.json
+import type {GetKnownSkin, KnownSkin} from './types';
 
 export const palette = {
     blauBluePrimary: '#00B6F1',
     blauBluePrimary10: '#F7FDFF',
     blauBluePrimary20: '#E5F6FD',
     blauBluePrimary30: '#B3E9FB',
-
+    blauBluePrimary60: '#00A2D6',
+    blauBluePrimary70: '#008FBD',
     blauBlueSecondary: '#0072BC',
     blauBlueSecondary10: '#E5F1F9',
     blauBlueSecondary20: '#B2D4EC',
     blauBlueSecondary30: '#80B7DF',
+    blauBlueSecondary50: '#0066AD',
     blauBlueSecondary60: '#005A99',
-
     blauPurple: '#7814B3',
     blauPurple10: '#F1E7F7',
     blauPurple30: '#BB89D9',
-
     blauYellow: '#FFA922',
     blauYellow10: '#FFF6E9',
     blauYellow40: '#FFC364',
     blauYellow60: '#F09500',
     blauYellow70: '#996614',
-
     blauGreen: '#30D300',
     blauGreen10: '#EAFBE5',
     blauGreen30: '#97E980',
     blauGreen70: '#1D7F00',
-
     blauRed: '#F64417',
     blauRed10: '#FEECE8',
     blauRed20: '#FCC7B9',
     blauRed30: '#FA9E87',
     blauRed40: '#F97C5D',
     blauRed70: '#C93712',
-
+    grey0: '#F6FCFE',
     grey1: '#F5F9FA',
     grey2: '#E7E7E7',
     grey3: '#B8B8B8',
@@ -47,250 +42,255 @@ export const palette = {
     grey5: '#808285',
     grey6: '#000000',
     white: '#FFFFFF',
-
-    // specific for dark mode:
     darkModeBlack: '#191919',
     darkModeGrey: '#242424',
     darkModeGrey6: '#313235',
-} as const;
+    darkModeGrey7: '#303030',
+    darkModeGrey8: '#3D3D3D',
+};
 
 export const getBlauSkin: GetKnownSkin = () => {
-    return {
+    const skin: KnownSkin = {
         name: BLAU_SKIN,
         colors: {
-            // BACKGROUNDS
+            backgroundBrand: palette.blauBluePrimary,
+            backgroundBrandSecondary: palette.blauBluePrimary,
             appBarBackground: palette.white,
             background: palette.white,
             backgroundContainer: palette.white,
+            backgroundContainerHover: palette.grey0,
+            backgroundContainerPressed: palette.grey1,
             backgroundContainerBrand: palette.blauBluePrimary,
+            backgroundContainerBrandHover: palette.blauBluePrimary60,
+            backgroundContainerBrandPressed: palette.blauBluePrimary70,
             backgroundContainerBrandOverInverse: palette.blauBlueSecondary,
+            backgroundContainerBrandOverInverseHover: palette.blauBlueSecondary50,
+            backgroundContainerBrandOverInversePressed: palette.blauBlueSecondary60,
             backgroundContainerAlternative: palette.blauBluePrimary20,
-            backgroundBrand: palette.blauBluePrimary,
-            backgroundBrandSecondary: palette.blauBluePrimary,
+            backgroundContainerAlternativeHover: applyAlpha(palette.grey5, 0.1),
+            backgroundContainerAlternativePressed: applyAlpha(palette.grey5, 0.2),
             backgroundOverlay: applyAlpha(palette.blauBlueSecondary, 0.75),
             backgroundSkeleton: palette.grey2,
             backgroundSkeletonInverse: palette.blauBlueSecondary,
             navigationBarBackground: palette.blauBluePrimary,
             backgroundAlternative: palette.blauBluePrimary20,
             backgroundFeedbackBottom: palette.blauBluePrimary,
-
             skeletonWave: palette.grey2,
-
-            // BORDERS
             borderLow: palette.grey1,
             border: palette.grey2,
             borderHigh: palette.grey5,
             borderSelected: palette.blauBlueSecondary60,
-
-            // BUTTONS
             buttonDangerBackground: palette.blauRed,
             buttonDangerBackgroundSelected: palette.blauRed70,
             buttonDangerBackgroundHover: palette.blauRed70,
-
             buttonLinkBackgroundSelected: palette.blauPurple10,
-            buttonLinkBackgroundInverseSelected: applyAlpha(palette.white, 0.3),
-
+            buttonLinkBackgroundInverseSelected: applyAlpha(palette.white, 0.15),
             buttonPrimaryBackground: palette.blauBlueSecondary,
             buttonPrimaryBackgroundInverse: palette.white,
             buttonPrimaryBackgroundSelected: palette.blauBlueSecondary60,
             buttonPrimaryBackgroundHover: palette.blauBlueSecondary60,
             buttonPrimaryBackgroundInverseSelected: palette.blauBluePrimary30,
-
             buttonSecondaryBorder: palette.blauBlueSecondary,
             buttonSecondaryBorderSelected: palette.blauBlueSecondary60,
             buttonSecondaryBorderInverse: palette.white,
-            buttonSecondaryBorderInverseSelected: palette.white,
             buttonSecondaryBackgroundHover: palette.blauBlueSecondary10,
             buttonSecondaryBackgroundSelected: palette.blauBlueSecondary10,
+            buttonSecondaryBorderInverseSelected: palette.white,
             buttonSecondaryBackgroundInverseHover: applyAlpha(palette.white, 0.2),
             buttonSecondaryBackgroundInverseSelected: applyAlpha(palette.white, 0.2),
-
             textButtonPrimary: palette.white,
             textButtonPrimaryInverse: palette.blauBlueSecondary,
             textButtonPrimaryInverseSelected: palette.blauBlueSecondary60,
-
             textButtonSecondary: palette.blauBlueSecondary,
             textButtonSecondarySelected: palette.blauBlueSecondary60,
             textButtonSecondaryInverse: palette.white,
             textButtonSecondaryInverseSelected: palette.white,
-
             textLink: palette.blauPurple,
             textLinkInverse: palette.white,
             textLinkDanger: palette.blauRed,
             textLinkSnackbar: palette.blauPurple30,
-
-            // CONTROLS
             control: palette.grey2,
             controlActivated: palette.blauBlueSecondary,
             controlError: palette.blauRed,
             loadingBar: palette.blauBlueSecondary,
             loadingBarBackground: palette.blauBlueSecondary10,
-
             toggleAndroidInactive: palette.grey2,
             toggleAndroidBackgroundActive: palette.blauBlueSecondary10,
             iosControlKnob: palette.white,
-
-            // DIVIDERS
             divider: palette.grey2,
             dividerInverse: applyAlpha(palette.white, 0.2),
             navigationBarDivider: palette.blauBluePrimary,
-
-            // FEEDBACKS
             badge: palette.blauRed,
             feedbackErrorBackground: palette.blauRed,
             feedbackInfoBackground: palette.grey6,
-
-            // GLOBAL
             brand: palette.blauBluePrimary,
             brandHigh: palette.blauBlueSecondary,
             inverse: palette.white,
             neutralHigh: palette.grey6,
             neutralMedium: palette.grey5,
-            neutralLow: palette.grey1,
+            neutralMediumInverse: palette.grey5,
+            neutralLow: palette.grey2,
             neutralLowAlternative: palette.grey2,
-
             textPrimary: palette.grey6,
             textPrimaryInverse: palette.white,
             textSecondary: palette.grey5,
             textSecondaryInverse: palette.blauBluePrimary20,
-
-            // STATES
             error: palette.blauRed,
             success: palette.blauGreen,
             warning: palette.blauYellow,
             promo: palette.blauPurple,
             highlight: palette.blauBluePrimary,
-
-            // BARS TEXTS
+            successLow: palette.blauGreen10,
+            warningLow: palette.blauYellow10,
+            errorLow: palette.blauRed10,
+            promoLow: palette.blauPurple10,
+            brandLow: palette.blauBlueSecondary10,
+            successHigh: palette.blauGreen70,
+            warningHigh: palette.blauYellow70,
+            errorHigh: palette.blauRed70,
+            promoHigh: palette.blauPurple,
+            successHighInverse: palette.blauGreen70,
+            warningHighInverse: palette.blauYellow70,
+            errorHighInverse: palette.blauRed70,
+            promoHighInverse: palette.blauPurple,
             textNavigationBarPrimary: palette.white,
             textNavigationBarSecondary: palette.blauBlueSecondary20,
             textNavigationSearchBarHint: palette.blauBlueSecondary20,
             textNavigationSearchBarText: palette.white,
             textAppBar: palette.grey5,
             textAppBarSelected: palette.blauBlueSecondary60,
-
-            // TAGS
-            successLow: palette.blauGreen10,
-            warningLow: palette.blauYellow10,
-            errorLow: palette.blauRed10,
-            promoLow: palette.blauPurple10,
-            brandLow: palette.blauBlueSecondary10,
-
-            successHigh: palette.blauGreen70,
-            warningHigh: palette.blauYellow70,
-            errorHigh: palette.blauRed70,
-            promoHigh: palette.blauPurple,
-
-            successHighInverse: palette.blauGreen70,
-            warningHighInverse: palette.blauYellow70,
-            errorHighInverse: palette.blauRed70,
-            promoHighInverse: palette.blauPurple,
-            neutralMediumInverse: palette.grey5,
+            customTabsBackground: palette.blauBluePrimary,
         },
         darkModeColors: {
-            // BACKGROUNDS
+            background: palette.darkModeBlack,
+            backgroundAlternative: palette.darkModeBlack,
             backgroundBrand: palette.darkModeBlack,
             backgroundBrandSecondary: palette.darkModeBlack,
-            background: palette.darkModeBlack,
-            appBarBackground: palette.darkModeGrey,
             backgroundContainer: palette.darkModeGrey,
+            backgroundContainerHover: palette.darkModeGrey7,
+            backgroundContainerPressed: palette.darkModeGrey8,
             backgroundContainerBrand: palette.darkModeGrey,
+            backgroundContainerBrandHover: palette.darkModeGrey7,
+            backgroundContainerBrandPressed: palette.darkModeGrey8,
+            backgroundContainerBrandOverInverseHover: palette.darkModeGrey7,
+            backgroundContainerBrandOverInversePressed: palette.darkModeGrey8,
             backgroundContainerBrandOverInverse: palette.darkModeGrey,
             backgroundContainerAlternative: palette.darkModeGrey,
-            backgroundAlternative: palette.darkModeBlack,
+            backgroundContainerAlternativeHover: palette.darkModeGrey7,
+            backgroundContainerAlternativePressed: palette.darkModeGrey8,
             backgroundOverlay: applyAlpha(palette.darkModeGrey, 0.8),
-            backgroundSkeleton: palette.darkModeGrey6,
-            backgroundSkeletonInverse: palette.darkModeGrey6,
+            backgroundSkeleton: palette.darkModeGrey,
+            backgroundSkeletonInverse: palette.darkModeGrey,
             backgroundFeedbackBottom: palette.darkModeBlack,
+            appBarBackground: palette.darkModeGrey,
             navigationBarBackground: palette.darkModeBlack,
-
             skeletonWave: palette.grey5,
-
-            // BORDERS
             borderLow: palette.darkModeBlack,
             border: palette.darkModeGrey,
-
-            // BUTTONS
-
+            borderHigh: palette.grey5,
+            borderSelected: palette.blauBlueSecondary60,
+            buttonDangerBackground: palette.blauRed,
+            buttonDangerBackgroundSelected: palette.blauRed70,
+            buttonDangerBackgroundHover: palette.blauRed70,
             buttonLinkBackgroundSelected: applyAlpha(palette.blauPurple, 0.3),
             buttonLinkBackgroundInverseSelected: applyAlpha(palette.blauPurple, 0.3),
-
-            buttonPrimaryBackgroundInverse: palette.blauBlueSecondary,
+            buttonPrimaryBackground: palette.blauBlueSecondary,
+            buttonPrimaryBackgroundInverse: palette.blauBluePrimary,
             buttonPrimaryBackgroundSelected: palette.blauBlueSecondary60,
-
+            buttonPrimaryBackgroundHover: palette.blauBlueSecondary60,
+            buttonPrimaryBackgroundInverseSelected: palette.blauBlueSecondary60,
             buttonSecondaryBorder: palette.white,
             buttonSecondaryBorderSelected: palette.white,
-            buttonSecondaryBorderInverse: palette.white,
-            buttonSecondaryBorderInverseSelected: palette.white,
             buttonSecondaryBackgroundHover: applyAlpha(palette.white, 0.15),
             buttonSecondaryBackgroundSelected: applyAlpha(palette.white, 0.15),
+            buttonSecondaryBorderInverse: palette.white,
+            buttonSecondaryBorderInverseSelected: palette.white,
             buttonSecondaryBackgroundInverseHover: applyAlpha(palette.white, 0.15),
             buttonSecondaryBackgroundInverseSelected: applyAlpha(palette.white, 0.15),
-
             textButtonPrimary: palette.grey2,
             textButtonPrimaryInverse: palette.grey2,
             textButtonPrimaryInverseSelected: palette.grey2,
-
             textButtonSecondary: palette.grey2,
             textButtonSecondarySelected: palette.grey2,
             textButtonSecondaryInverse: palette.grey2,
             textButtonSecondaryInverseSelected: palette.grey2,
-
             textLink: palette.blauPurple30,
             textLinkInverse: palette.blauPurple30,
-
-            // CONTROLS
+            textLinkDanger: palette.blauRed,
+            textLinkSnackbar: palette.blauPurple30,
             control: palette.darkModeGrey6,
+            controlActivated: palette.blauBlueSecondary,
+            controlError: palette.blauRed,
             loadingBar: palette.blauBluePrimary,
             loadingBarBackground: palette.darkModeGrey,
-
-            toggleAndroidInactive: palette.grey5, // web only
-            toggleAndroidBackgroundActive: palette.blauBlueSecondary30, // web only
-            iosControlKnob: palette.grey2, // web only
-
-            // DIVIDERS
+            toggleAndroidInactive: palette.grey5,
+            toggleAndroidBackgroundActive: palette.blauBlueSecondary30,
+            iosControlKnob: palette.grey2,
             divider: applyAlpha(palette.white, 0.05),
             dividerInverse: applyAlpha(palette.white, 0.05),
             navigationBarDivider: palette.darkModeBlack,
-
-            // GLOBAL
+            badge: palette.blauRed,
+            feedbackErrorBackground: palette.blauRed,
+            feedbackInfoBackground: palette.darkModeGrey,
             brand: palette.blauBluePrimary,
-            brandHigh: palette.grey5,
+            brandHigh: applyAlpha(palette.white, 0.05),
             inverse: palette.grey2,
             neutralHigh: palette.grey2,
             neutralMedium: palette.grey5,
+            neutralMediumInverse: palette.grey5,
             neutralLow: palette.darkModeGrey6,
             neutralLowAlternative: palette.darkModeGrey6,
-
             textPrimary: palette.grey2,
             textPrimaryInverse: palette.grey2,
             textSecondary: palette.grey4,
             textSecondaryInverse: palette.grey4,
-
-            textNavigationBarPrimary: palette.grey2,
-            textNavigationBarSecondary: palette.grey4,
-            textNavigationSearchBarHint: palette.grey4, // iOS
-            textNavigationSearchBarText: palette.grey2, // iOS
-            textAppBar: palette.grey5,
-            textAppBarSelected: palette.grey2,
-
-            // TAGS
+            error: palette.blauRed,
+            success: palette.blauGreen,
+            warning: palette.blauYellow,
+            promo: palette.blauPurple,
+            highlight: palette.blauBluePrimary,
             successLow: palette.darkModeGrey6,
             warningLow: palette.darkModeGrey6,
             errorLow: palette.darkModeGrey6,
             promoLow: palette.darkModeGrey6,
             brandLow: palette.darkModeGrey6,
-
             successHigh: palette.blauGreen30,
             warningHigh: palette.blauYellow40,
             errorHigh: palette.blauRed40,
             promoHigh: palette.blauPurple30,
-
             successHighInverse: palette.blauGreen70,
             warningHighInverse: palette.blauYellow70,
             errorHighInverse: palette.blauRed70,
             promoHighInverse: palette.blauPurple,
-            neutralMediumInverse: palette.grey5,
+            textNavigationBarPrimary: palette.grey2,
+            textNavigationBarSecondary: palette.grey4,
+            textNavigationSearchBarHint: palette.grey4,
+            textNavigationSearchBarText: palette.grey2,
+            textAppBar: palette.grey5,
+            textAppBarSelected: palette.grey2,
+            customTabsBackground: palette.darkModeBlack,
+        },
+        borderRadii: {
+            avatar: '50%',
+            bar: '999px',
+            button: '4px',
+            checkbox: '2px',
+            container: '8px',
+            indicator: '999px',
+            input: '8px',
+            legacyDisplay: '16px',
+            popup: '8px',
+            sheet: '8px',
+        },
+        textPresets: {
+            cardTitle: {weight: 'light'},
+            text5: {weight: 'light'},
+            text6: {weight: 'light'},
+            text7: {weight: 'light'},
+            text8: {weight: 'light'},
+            text9: {weight: 'light'},
+            text10: {weight: 'light'},
         },
     };
+    return skin;
 };
