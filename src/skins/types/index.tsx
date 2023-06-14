c474eaf1bd288e17eea8cf38d5c0ed555da191fe
Abel Toledano <atoledano@tuenti.com>
2023-06-14 13:33:25 +0200

    fix types

--- src/skins/types/index.tsx
+++ src/skins/types/index.tsx
@@ -11,7 +11,18 @@ export type GetKnownSkin = (variant?: SkinVariant) => KnownSkin;
 
 export type FontWeight = 'light' | 'regular' | 'medium' | 'bold';
 
-type TextPresetName = 'text5' | 'text6' | 'text7' | 'text8' | 'text9' | 'text10' | 'cardTitle';
+type TextPresetName =
+    | 'text5'
+    | 'text6'
+    | 'text7'
+    | 'text8'
+    | 'text9'
+    | 'text10'
+    | 'cardTitle'
+    | 'button'
+    | 'link'
+    | 'title1'
+    | 'indicator';
 export type TextPresetsConfig = {
     [preset in TextPresetName]: {
         weight: FontWeight;
