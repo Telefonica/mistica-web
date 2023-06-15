d5e4150ded929ec3323d04e5e559c57eabe7e0b1
daviteixeira-tlf <davi.teixeira.ext@telefonica.com>
2023-06-15 17:15:12 -0300

    add: Slider Unit Test

--- src/__tests__/slider-test.tsx
+++ src/__tests__/slider-test.tsx
@@ -29,3 +29,66 @@ test('change slider value', () => {
   expect(slider).toHaveValue('90')
 });
 
+test('change slider value and show on input', () => {
+  render(
+      <ThemeContextProvider theme={makeTheme()}>
+        <Slider field  />
+      </ThemeContextProvider>
+  );
+
+  const slider = screen.getByRole('slider');
+  fireEvent.change(slider, { target: { value: 90 } });
+
+  const field = screen.getByRole('textbox')
+
+  expect(field).toHaveValue('90')
+});
+
+test('change input value and show on slider', () => {
+  render(
+      <ThemeContextProvider theme={makeTheme()}>
+        <Slider field  />
+      </ThemeContextProvider>
+  );
+
+  
+  const field = screen.getByRole('textbox')
+  
+  fireEvent.change(field, { target: { value: 90 } });
+  
+  const slider = screen.getByRole('slider');
+
+  expect(slider).toHaveValue('90')
+});
+
+test('change input to invalid value and show error message', () => {
+  render(
+      <ThemeContextProvider theme={makeTheme()}>
+        <Slider field min={20}  />
+      </ThemeContextProvider>
+  );
+
+  
+  const field = screen.getByRole('textbox')
+  
+  fireEvent.change(field, { target: { value: 101 } });
+  
+  const slider = screen.getByRole('slider');
+
+  let text = screen.getByText(/Max: 100/i)
+
+  expect(slider).not.toHaveValue('101')
+  expect(slider).toHaveValue('100')
+  expect(text).toBeInTheDocument()
+
+  fireEvent.change(field, { target: { value: 0 } });
+
+  text = screen.getByText(/Min: 20/i)
+
+  expect(slider).not.toHaveValue('0')
+  expect(slider).toHaveValue('20')
+  expect(text).toBeInTheDocument()
+
+});
+
+
