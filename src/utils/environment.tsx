3c81cf86482253887a200961064e202c2b83da0a
marcoskolodny <marcoskolodny@gmail.com>
2023-06-15 11:29:53 +0200

    resolve comment

--- src/utils/environment.tsx
+++ src/utils/environment.tsx
@@ -1,3 +1,3 @@
 export const isServerSide = (): boolean => typeof window === 'undefined';
 
-export const isClientSide = (): boolean => typeof window !== 'undefined';
+export const isClientSide = (): boolean => !isServerSide();
