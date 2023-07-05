96762a916dac0111dfc84e3c8a3f804492d179b2
marcoskolodny <marcoskolodny@gmail.com>
2023-07-05 12:15:03 +0200

    refactor stories for primitives

--- src/__screenshot_tests__/video-screenshot-test.tsx
+++ src/__screenshot_tests__/video-screenshot-test.tsx
@@ -1,10 +1,63 @@
 import {openStoryPage, screen, ssimScreenshotConfig} from '../test-utils';
 
-test('Video', async () => {
+const ASPECT_RATIO = ['1:1', '16:9', '4:3', '0'];
+
+test('Video - width and height', async () => {
     await openStoryPage({id: 'components-primitives-video--default'});
 
     const story = await screen.findByTestId('video');
 
-    // https://jira.tid.es/browse/WEB-680
+    expect(await story.screenshot()).toMatchImageSnapshot(ssimScreenshotConfig);
+});
+
+test.each(ASPECT_RATIO)('Video - aspect ratio %s', async (aspectRatio) => {
+    await openStoryPage({
+        id: 'components-primitives-video--default',
+        args: {
+            type: 'width and aspect ratio',
+            aspectRatio: aspectRatio.replace(':', ' '),
+        },
+    });
+
+    const story = await screen.findByTestId('video');
+
+    expect(await story.screenshot()).toMatchImageSnapshot(ssimScreenshotConfig);
+});
+
+test.each(ASPECT_RATIO)('Video - full width with aspect ratio %s', async (aspectRatio) => {
+    await openStoryPage({
+        id: 'components-primitives-video--default',
+        args: {type: 'full width', aspectRatio: aspectRatio.replace(':', ' ')},
+    });
+
+    const story = await screen.findByTestId('video');
+
+    expect(await story.screenshot()).toMatchImageSnapshot(ssimScreenshotConfig);
+});
+
+test.each(ASPECT_RATIO.slice(0, 3))('Video - no poster with aspect ratio %s', async (aspectRatio) => {
+    await openStoryPage({
+        id: 'components-primitives-video--default',
+        args: {type: 'width and aspect ratio', aspectRatio: aspectRatio.replace(':', ' '), withPoster: false},
+    });
+
+    const story = await screen.findByTestId('video');
+
+    expect(await story.screenshot()).toMatchImageSnapshot(ssimScreenshotConfig);
+});
+
+test.each(ASPECT_RATIO.slice(0, 3))('Video - error fallback with aspect ratio %s', async (aspectRatio) => {
+    await openStoryPage({
+        id: 'components-primitives-video--default',
+        args: {
+            type: 'width and aspect ratio',
+            aspectRatio: aspectRatio.replace(':', ' '),
+            withPoster: false,
+            withErrorFallback: true,
+        },
+    });
+
+    const story = await screen.findByTestId('video');
+
     expect(await story.screenshot()).toMatchImageSnapshot(ssimScreenshotConfig);
 });
--- src/__stories__/touchable-story.tsx
+++ src/__stories__/touchable-story.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
 import {Touchable, Stack, Text2, skinVars} from '..';
-import {StorySection} from './helpers';
 
 export default {
     title: 'Components/Primitives/Touchable',
     component: Touchable,
+    argTypes: {
+        type: {
+            options: ['href', 'onPress'],
+            control: {type: 'select'},
+        },
+        newTab: {if: {arg: 'type', eq: 'href'}},
+    },
 };
 
-export const Default: StoryComponent = () => {
-    const [count, setCount] = React.useState(0);
+type Args = {
+    type: 'href' | 'onPress';
+    newTab: boolean;
+};
 
+export const Default: StoryComponent<Args> = ({type, newTab}) => {
     const styles = {
         border: `1px solid ${skinVars.colors.border}`,
         display: 'flex',
@@ -18,32 +27,25 @@ export const Default: StoryComponent = () => {
         color: skinVars.colors.textPrimary,
     };
 
+    const props =
+        type === 'href' ? {href: window.location.origin, newTab} : {onPress: () => window.alert('Pressed')};
+
     return (
-        <>
-            <StorySection title="Touchable">
-                <Stack space={16}>
-                    <Text2 as="p" regular>
-                        Touchable is the base component we use for any component that can be clicked/tapped by
-                        the user, for example buttons, list rows, etc. This component handles touch events,
-                        can track analytics events on press, and guarantees the required a11y for any
-                        touchable area.
-                    </Text2>
-                    <Touchable style={styles} href={window.location.origin}>
-                        Touchable with href
-                    </Touchable>
-                    <Touchable style={styles} href={window.location.origin} newTab>
-                        Touchable with href (opens in new tab)
-                    </Touchable>
-                    <Touchable style={styles} onPress={() => setCount(count + 1)}>
-                        Touchable with onPress
-                    </Touchable>
-                    <Text2 as="div" regular>
-                        Pressed {count} times
-                    </Text2>
-                </Stack>
-            </StorySection>
-        </>
+        <Stack space={16} dataAttributes={{testid: 'touchable'}}>
+            <Text2 as="p" regular>
+                Touchable is the base component we use for any component that can be clicked/tapped by the
+                user, for example buttons, list rows, etc. This component handles touch events, can track
+                analytics events on press, and guarantees the required a11y for any touchable area.
+            </Text2>
+            <Touchable style={styles} {...props}>
+                Touchable
+            </Touchable>
+        </Stack>
     );
 };
 
 Default.storyName = 'Touchable';
+Default.args = {
+    type: 'href',
+    newTab: false,
+};
