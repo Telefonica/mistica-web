a18535e9fff4dbef79b45638a17c6383ced2b454
Flow <lifecycle-github@telefonica.com>
2023-07-05 14:01:39 +0200

    feat(skin): update design tokens (#808)
    Automated changes by
    [create-pull-request](https://github.com/peter-evans/create-pull-request)
    GitHub action
    
    https://jira.tid.es/browse/WEB-1444
    
    ---------
    
    Co-authored-by: atabel <atabel@users.noreply.github.com>
    Co-authored-by: Abel Toledano <atoledano@tuenti.com>

--- src/skins/blau.tsx
+++ src/skins/blau.tsx
@@ -275,6 +275,11 @@ export const getBlauSkin: GetKnownSkin = () => {
         textPresets: {
             cardTitle: {weight: 'regular'},
             button: {weight: 'medium'},
+            tabsLabel: {
+                weight: 'medium',
+                size: {mobile: 16, desktop: 18},
+                lineHeight: {mobile: 24, desktop: 24},
+            },
             link: {weight: 'medium'},
             title1: {weight: 'medium'},
             indicator: {weight: 'medium'},
@@ -284,7 +289,6 @@ export const getBlauSkin: GetKnownSkin = () => {
             text8: {weight: 'light'},
             text9: {weight: 'light'},
             text10: {weight: 'light'},
-            tabsLabel: {size: {mobile: 16, desktop: 18}, lineHeight: {mobile: 24, desktop: 24}},
         },
     };
     return skin;
