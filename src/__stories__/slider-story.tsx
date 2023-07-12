7e7fa8c5a20a87b9456378bada1ac6ba66f1196e
daviteixeira-tlf <davi.teixeira.ext@telefonica.com>
2023-07-12 11:50:28 -0300

    fix: slider storybook

--- src/__stories__/slider-story.tsx
+++ src/__stories__/slider-story.tsx
@@ -1,36 +1,47 @@
 import * as React from 'react';
-import { Slider } from '..';
-
+import {Slider} from '..';
 
 export default {
     title: 'Components/Slider',
-    component: Slider
-   
+    component: Slider,
 };
 
 type SliderArgs = {
-    disabled: boolean
-    steps: number
-    max: number
-    min: number
-    field: boolean
-    stepsInArray: boolean
+    disabled: boolean;
+    steps: number;
+    max: number;
+    min: number;
+    field: boolean;
+    stepsInArray: boolean;
+    invalidText: string;
 };
 
-export const Default: StoryComponent<SliderArgs> = ({disabled,
+export const Default: StoryComponent<SliderArgs> = ({
+    disabled,
     steps,
     max,
     min,
     field,
-    stepsInArray}) => {
+    stepsInArray,
+    invalidText,
+}) => {
+    const step = stepsInArray ? [0, 8, 12, 16, 24, 40] : steps;
 
-    const step = stepsInArray ? [0,8,12,16,24,40] : steps
-
-    return <div data-testid="slider">
-        <Slider disabled={disabled} steps={step} max={max} min={min} field={field}  />
-    </div>
-}
-
+    return (
+        <div data-testid="slider">
+            <div style={{marginTop: 54}}>
+                <Slider
+                    invalidText={invalidText}
+                    disabled={disabled}
+                    steps={step}
+                    max={max}
+                    min={min}
+                    field={field}
+                />
+            </div>
+        </div>
+    );
+};
 
 Default.storyName = 'Slider';
 
@@ -38,7 +49,8 @@ Default.args = {
     disabled: false,
     steps: 1,
     max: 100,
-    min:0,
-    field:false,
+    min: 0,
+    field: false,
     stepsInArray: false,
+    invalidText: 'Invalid',
 };
