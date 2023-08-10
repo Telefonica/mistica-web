a9ea7e585832360166e6d17010c6094b0319e0ea
Yayo <yceballost@gmail.com>
2023-08-10 16:36:56 +0200

    on air font

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,7 @@
 import './css/roboto.css';
 import './css/vivo-font.css';
 import './css/telefonica-font.css';
+import './css/onair-font.css';
 import './css/main.css';
 import * as React from 'react';
 import {
@@ -72,6 +73,7 @@ const MisticaThemeProvider = ({Story, context}): React.ReactElement => {
         <ThemeContextProvider theme={getTheme(skin as string, platform, colorScheme)}>
             {skin === VIVO_NEW_SKIN && <style>{`body {font-family: "Vivo Type"}`}</style>}
             {skin === TELEFONICA_SKIN && <style>{`body {font-family: "Telefonica Sans"}`}</style>}
+            {skin === O2_SKIN && <style>{`body {font-family: "On Air"}`}</style>}
             <Story {...context} />
         </ThemeContextProvider>
     );
