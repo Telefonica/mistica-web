a5ada69eba887a768d216ecc012c593383e7cdcf
Yayo <yceballost@gmail.com>
2023-09-05 13:44:46 +0200

    update icons

--- src/generated/mistica-icons/icon-funnel-regular.tsx
+++ src/generated/mistica-icons/icon-funnel-regular.tsx
@@ -5,6 +5,7 @@
  */
 
 import * as React from 'react';
+import {useTheme} from '../../hooks';
 import {useIsInverseVariant} from '../../theme-variant-context';
 import {vars} from '../../skins/skin-contract.css';
 
@@ -13,15 +14,26 @@ import type {IconProps} from '../../utils/types';
 const IconFunnelRegular = ({color, size = 24, ...rest}: IconProps): JSX.Element => {
     const isInverse = useIsInverseVariant();
     const fillColor = color ?? (isInverse ? vars.colors.inverse : vars.colors.neutralHigh);
-
-    return (
-        <svg width={size} height={size} viewBox="0 0 24 24" role="presentation" {...rest}>
-            <path
-                fill={fillColor}
-                d="M9.87 19.776c0 1.228.957 2.224 2.126 2.224 1.17 0 2.126-.996 2.126-2.224v-4.443l6.295-12.242a.773.773 0 00-.018-.73.702.702 0 00-.607-.361H4.21c-.25 0-.48.138-.607.36a.76.76 0 00-.018.73L9.87 15.334v4.443zm1.333-4.979L5.393 3.48H18.6l-5.81 11.319a.73.73 0 00-.087.35v4.633c0 .408-.317.74-.707.74-.39 0-.707-.332-.707-.74v-4.633a.785.785 0 00-.086-.35z"
-            />
-        </svg>
-    );
+    const {skinName} = useTheme();
+    if (skinName.match(/^o2/i)) {
+        return (
+            <svg width={size} height={size} viewBox="0 0 24 24" role="presentation" {...rest}>
+                <path
+                    fill={fillColor}
+                    d="M9.87 19.776c0 1.228.957 2.224 2.126 2.224 1.17 0 2.126-.996 2.126-2.224v-4.443l6.295-12.242a.773.773 0 00-.018-.73.702.702 0 00-.607-.361H4.21c-.25 0-.48.138-.607.36a.76.76 0 00-.018.73L9.87 15.334v4.443zm1.333-4.979L5.393 3.48H18.6l-5.81 11.319a.73.73 0 00-.087.35v4.633c0 .408-.317.74-.707.74-.39 0-.707-.332-.707-.74v-4.633a.785.785 0 00-.086-.35z"
+                />
+            </svg>
+        );
+    } else {
+        return (
+            <svg width={size} height={size} viewBox="0 0 24 24" role="presentation" {...rest}>
+                <path
+                    fill={fillColor}
+                    d="M14.255 19.776c0 1.228-1.012 2.224-2.25 2.224s-2.26-.996-2.26-2.224v-4.443L3.09 3.091a.723.723 0 01.019-.73A.75.75 0 013.752 2h16.497a.75.75 0 01.642.36c.14.223.144.503.02.73l-6.656 12.243v4.443zm4.74-16.297H5.005l6.152 11.319a.75.75 0 01.09.35v4.633c0 .408.346.74.759.74a.745.745 0 00.748-.74v-4.633a.75.75 0 01.091-.35l6.152-11.319z"
+                />
+            </svg>
+        );
+    }
 };
 
 export default IconFunnelRegular;
