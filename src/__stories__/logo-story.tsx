990da2988341de12bdb043688f46a6d8932a935e
marcoskolodny <marcoskolodny@gmail.com>
2023-09-05 17:25:24 +0200

    update logo component, stories and tests

--- src/__stories__/logo-story.tsx
+++ src/__stories__/logo-story.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import {BlauLogo, Logo, MovistarLogo, O2Logo, TelefonicaLogo, VivoLogo} from '../logo';
-import {StorySection} from './helpers';
-import {Text2} from '../text';
-import {ThemeVariant} from '../theme-variant-context';
-import {skinVars} from '../index';
+import {Box, ResponsiveLayout} from '../index';
 
 export default {
     title: 'Components/Logo',
@@ -11,70 +8,86 @@ export default {
     parameters: {
         fullScreen: true,
     },
-    inverse: false,
 };
 
-type Args = {type: 'isotype' | 'imagotype' | 'vertical'; size: number; inverse: boolean};
+type Args = {
+    skin: 'default' | 'Movistar' | 'O2' | 'Vivo' | 'Vivo-new' | 'Telefonica' | 'Blau';
+    type: 'isotype' | 'imagotype' | 'vertical';
+    size: number;
+    inverse: boolean;
+    action: string;
+};
 
-export const Default: StoryComponent<Args> = ({type, size, inverse}) => {
-    const [count, setCount] = React.useState(0);
+const getLogoActionProps = (action: string) => {
+    return action === 'onPress'
+        ? {
+              onPress: () => {
+                  window.alert('pressed!');
+              },
+              'aria-label': 'pressable logo',
+          }
+        : action === 'href'
+        ? {
+              href: 'https://www.google.com',
+              newTab: true,
+              'aria-label': 'logo-link',
+          }
+        : action === 'to'
+        ? {
+              to: '#',
+              replace: true,
+              'aria-label': 'logo-link',
+          }
+        : {};
+};
+
+export const Default: StoryComponent<Args> = ({type, size, inverse, action, skin}) => {
+    const logoProps = {
+        ...getLogoActionProps(action),
+        type,
+        size,
+    };
+
+    const CurrentLogo = {
+        default: Logo,
+        Movistar: MovistarLogo,
+        Vivo: VivoLogo,
+        'Vivo-new': VivoLogo,
+        O2: O2Logo,
+        Telefonica: TelefonicaLogo,
+        Blau: BlauLogo,
+    }[skin];
+
     return (
-        <div
-            data-testid="logo"
-            style={{
-                padding: 16,
-                background: inverse ? skinVars.colors.backgroundBrand : skinVars.colors.background,
-            }}
-        >
-            <ThemeVariant isInverse={inverse}>
-                <StorySection title="Without touchable props">
-                    <Logo type={type} size={size} />
-                </StorySection>
-                <StorySection title="With to prop">
-                    <Logo type={type} size={size} to="#" aria-label="logo link" replace />
-                </StorySection>
-                <StorySection title="With href prop">
-                    <Logo type={type} size={size} href="#" aria-label="logo link" newTab />
-                </StorySection>
-                <StorySection title="With onPress prop">
-                    <Logo
-                        type={type}
-                        size={size}
-                        aria-label="pressable logo"
-                        onPress={() => {
-                            setCount((prev) => ++prev);
-                        }}
-                    />
-                    <Text2 as="div" regular>
-                        Pressed {count} times
-                    </Text2>
-                </StorySection>
-                <StorySection title="Explicit Logos">
-                    <Text2 as="p" regular>
-                        Same properties apply. It does not depend on the skin.
-                    </Text2>
-                    <MovistarLogo type={type} size={size} />
-                    <VivoLogo type={type} size={size} />
-                    <O2Logo type={type} size={size} />
-                    <TelefonicaLogo type={type} size={size} />
-                    <BlauLogo type={type} size={size} />
-                </StorySection>
-            </ThemeVariant>
-        </div>
+        <ResponsiveLayout isInverse={inverse} fullWidth>
+            <Box padding={16}>
+                <CurrentLogo {...logoProps} dataAttributes={{testid: 'logo'}} />
+            </Box>
+        </ResponsiveLayout>
     );
 };
 
 Default.storyName = 'Logo';
 
 Default.args = {
+    skin: 'default',
     type: 'isotype',
+    action: 'none',
     size: 48,
     inverse: false,
 };
 
 Default.argTypes = {
+    skin: {
+        options: ['default', 'Movistar', 'O2', 'Vivo', 'Vivo-new', 'Telefonica', 'Blau'],
+        control: {type: 'select'},
+    },
     type: {
         options: ['isotype', 'imagotype', 'vertical'],
+        control: {type: 'select'},
+    },
+    action: {
+        options: ['none', 'onPress', 'href', 'to'],
         control: {type: 'select'},
     },
     size: {
