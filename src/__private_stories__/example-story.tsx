250137f2fa618d8a6c8404eb748b9d8bd7f786f0
marcoskolodny <marcoskolodny@gmail.com>
2023-09-18 15:56:38 +0200

    test

--- src/__private_stories__/example-story.tsx
+++ src/__private_stories__/example-story.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
-import {ResponsiveLayout, Stack, Title2, Text2, Box} from '..';
+import {Stack, Video} from '..';
+import beachImg from '../__stories__/images/beach.jpg';
+import videoBeach from '../__stories__/videos/beach.mp4';
+import videoChema from '../__stories__/videos/video-chema.mp4';
 
 export default {
     title: 'Private/Example',
@@ -10,22 +13,23 @@ export default {
 
 export const Default: StoryComponent = () => {
     return (
-        <Box paddingY={24}>
-            <ResponsiveLayout>
-                <Stack space={16}>
-                    <Title2 as="h1">This is an example of private story</Title2>
-                    <Stack space={8}>
-                        <Text2 regular as="p">
-                            Private stories are only available in dev and CI builds, not in production.
-                        </Text2>
-                        <Text2 regular as="p">
-                            These stories are meant to be used for acceptance/screenshot tests that we don't
-                            want to appear in the production mística storybook.
-                        </Text2>
-                    </Stack>
-                </Stack>
-            </ResponsiveLayout>
-        </Box>
+        <Stack space={24}>
+            <Video src={{src: videoBeach, type: 'video/webm'}} width={640} height={360} />
+            <Video src={{src: videoChema, type: 'video/webm'}} width={640} height={360} />
+            <video playsInline autoPlay muted loop poster={beachImg} width={640} height={360}>
+                <source src={videoChema} type="video/webm" />
+                Your browser does not support the video tag.
+            </video>
+
+            <Video src={{src: videoChema, type: 'video/webm'}} width={640} height={360} poster={beachImg} />
+
+            <Video src={{src: videoBeach, type: 'video/webm'}} width={640} height={360} />
+
+            <video playsInline autoPlay muted loop poster={beachImg} width={640} height={360}>
+                <source src={videoChema} type="video/webm" />
+                Your browser does not support the video tag.
+            </video>
+        </Stack>
     );
 };
 
