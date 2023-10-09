6ae069b575aef1e6ec40db77417a68417aa50a75
Pedro Ladaria <pladaria@tuenti.com>
2023-10-09 11:39:28 +0200

    chore(SonarQube): Setup SonarQube (#903)
    * WEB-1570 configure sonarqube
    
    * WEB-1570 remove and ignore .scannerwork
    
    * WEB-1570 setup gh workflow
    
    * WEB-1570 setup gh workflow
    
    * WEB-1570 setup gh workflow
    
    * WEB-1570 setup gh workflow
    
    * WEB-1570 setup gh workflow
    
    * WEB-1570 revert workflow
    
    * WEB-1570 add comment
    
    ---------
    
    Co-authored-by: Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>

---
+++ scripts/sonarqube-scan.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# To run this script you need to have the SONAR_TOKEN environment variable set
+
+# Change working directory to the root of the project
+cd "$(dirname "$0")"/..
+
+# Extract version from package.json
+SONAR_PROJECT_VERSION=`node -e "console.log(require('./package.json').version)"`
+
+echo "Running SonarQube analysis for mistica-web..."
+docker run \
+    --rm \
+    -e SONAR_TOKEN="${SONAR_TOKEN}" \
+    -e SONAR_PROJECT_VERSION="${SONAR_PROJECT_VERSION}" \
+    -v "${PWD}:/usr/src" \
+    sonarsource/sonar-scanner-cli
