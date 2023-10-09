25a93043063f3a7e6ebe2270b1b0f6ddf42acf70
Wiljan Arias Milián <you@example.com>
2023-10-09 19:12:23 +0200

    now nested responsive layouts give priority to deeper instance

--- src/responsive-layout.css.ts
+++ src/responsive-layout.css.ts
@@ -11,7 +11,37 @@ export const LARGE_DESKTOP_MAX_WIDTH = 1224;
 const sideMargin = createVar();
 export const vars = {sideMargin};
 
-export const container = sprinkles({width: '100%'});
+export const container = style([
+    sprinkles({width: '100%'}),
+    {
+        vars: {
+            [sideMargin]: 'auto',
+        },
+        '@media': {
+            [mq.desktopOrBigger]: {
+                vars: {
+                    [sideMargin]: `${SMALL_DESKTOP_SIDE_MARGIN}px`,
+                },
+            },
+            [mq.tablet]: {
+                vars: {
+                    [sideMargin]: `${TABLET_SIDE_MARGIN}px`,
+                },
+            },
+            [mq.mobile]: {
+                vars: {
+                    [sideMargin]: `${MOBILE_SIDE_MARGIN}px`,
+                },
+            },
+        },
+        selectors: {
+            '& &': {
+                margin: `0 calc(-1 * ${sideMargin})`,
+                width: 'auto',
+            },
+        },
+    },
+]);
 
 export const backgroundVariant = {
     inverse: sprinkles({background: skinVars.colors.backgroundBrand}),
@@ -19,44 +49,10 @@ export const backgroundVariant = {
 };
 
 export const responsiveLayout = style({
-    margin: 'auto',
     paddingLeft: 'env(safe-area-inset-left)',
     paddingRight: 'env(safe-area-inset-right)',
 
-    vars: {
-        [sideMargin]: '0px',
-    },
-
-    '@media': {
-        [mq.largeDesktop]: {
-            width: LARGE_DESKTOP_MAX_WIDTH,
-            maxWidth: `calc(100% - ${SMALL_DESKTOP_SIDE_MARGIN * 2}px)`, // to make ResponsiveLayout work inside desktop modals
-        },
-        [mq.desktop]: {
-            margin: `0 ${SMALL_DESKTOP_SIDE_MARGIN}px`,
-        },
-        [mq.tablet]: {
-            margin: `0 ${TABLET_SIDE_MARGIN}px`,
-
-            vars: {
-                [sideMargin]: `${TABLET_SIDE_MARGIN}px`,
-            },
-        },
-        [mq.mobile]: {
-            margin: `0 ${MOBILE_SIDE_MARGIN}px`,
-
-            vars: {
-                [sideMargin]: `${MOBILE_SIDE_MARGIN}px`,
-            },
-        },
-    },
-
-    selectors: {
-        '& &': {
-            margin: 0,
-            width: 'auto',
-        },
-    },
+    margin: `0 ${sideMargin}`,
 });
 
 export const fullWidth = style([
