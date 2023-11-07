91d76568500f4d73e83bc99c879c774545b4ffbc
marcoskolodny <marcoskolodny@gmail.com>
2023-11-07 15:29:41 +0100

    update screenshots

--- src/__screenshot_tests__/counter-screenshot-test.tsx
+++ src/__screenshot_tests__/counter-screenshot-test.tsx
@@ -1,39 +1,47 @@
 import {openStoryPage, screen} from '../test-utils';
 
 const SKINS = ['Movistar', 'O2', 'Vivo', 'Vivo-new', 'Telefonica', 'Blau'] as const;
-const INVERSE_VALUES = [false, true];
-const DARK_MODE_VALUES = [false, true];
 
-const getCases = () => {
-    const cases = [];
-    for (const skin of SKINS) {
-        for (const isInverse of INVERSE_VALUES) {
-            for (const isDarkMode of DARK_MODE_VALUES) {
-                cases.push([skin, isInverse, isDarkMode]);
-            }
-        }
-    }
+test.each(SKINS)('Counter default. skin={%s}', async (skin) => {
+    await openStoryPage({
+        id: 'components-counter--default',
+        device: 'DESKTOP',
+        args: {defaultValue: 1},
+        skin: skin as (typeof SKINS)[number],
+    });
 
-    return cases;
-};
+    const counter = await screen.findByTestId('counter');
 
-test.each(getCases())(
-    'Counter default. skin={%s} isInverse={%s} isDarkMode={%s}',
-    async (skin, isInverse, isDarkMode) => {
-        await openStoryPage({
-            id: 'components-counter--default',
-            device: 'DESKTOP',
-            args: {inverse: isInverse, defaultValue: 1},
-            isDarkMode: isDarkMode as boolean,
-            skin: skin as (typeof SKINS)[number],
-        });
+    const image = await counter.screenshot();
+    expect(image).toMatchImageSnapshot();
+});
 
-        const counter = await screen.findByTestId('counter');
+test('Counter. isInverse', async () => {
+    await openStoryPage({
+        id: 'components-counter--default',
+        device: 'DESKTOP',
+        args: {isInverse: true, defaultValue: 1},
+    });
 
-        const image = await counter.screenshot();
-        expect(image).toMatchImageSnapshot();
-    }
-);
+    const counter = await screen.findByTestId('counter');
+
+    const image = await counter.screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
+test('Counter. isDarkMode', async () => {
+    await openStoryPage({
+        id: 'components-counter--default',
+        device: 'DESKTOP',
+        args: {defaultValue: 1},
+        isDarkMode: true,
+    });
+
+    const counter = await screen.findByTestId('counter');
+
+    const image = await counter.screenshot();
+    expect(image).toMatchImageSnapshot();
+});
 
 test.each`
     min  | max   | defaultValue | removable | disabled
@@ -44,19 +52,13 @@ test.each`
     ${0} | ${20} | ${10}        | ${false}  | ${false}
     ${1} | ${1}  | ${1}         | ${true}   | ${false}
     ${0} | ${4}  | ${0}         | ${true}   | ${false}
-    ${0} | ${4}  | ${4}         | ${true}   | ${false}
-    ${3} | ${4}  | ${3}         | ${true}   | ${false}
     ${0} | ${20} | ${10}        | ${true}   | ${false}
     ${1} | ${1}  | ${1}         | ${false}  | ${true}
     ${0} | ${4}  | ${0}         | ${false}  | ${true}
     ${0} | ${4}  | ${4}         | ${false}  | ${true}
     ${3} | ${4}  | ${3}         | ${false}  | ${true}
     ${0} | ${20} | ${10}        | ${false}  | ${true}
-    ${1} | ${1}  | ${1}         | ${true}   | ${true}
-    ${0} | ${4}  | ${0}         | ${true}   | ${true}
-    ${0} | ${4}  | ${4}         | ${true}   | ${true}
     ${3} | ${4}  | ${3}         | ${true}   | ${true}
-    ${0} | ${20} | ${10}        | ${true}   | ${true}
 `(
     'Counter. min=$min max=$max defaultValue=$defaultValue removable=$removable disabled=$disabled',
     async ({min, max, defaultValue, removable, disabled}) => {
