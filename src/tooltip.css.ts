60a402132df2d6f6a7dd757611ca510493c8a189
marcoskolodny <marcoskolodny@gmail.com>
2023-11-07 17:32:55 +0100

    use sprinkles when possible

--- src/tooltip.css.ts
+++ src/tooltip.css.ts
@@ -37,17 +37,20 @@ export const tooltipTransitionClasses = {
     unmounted: {},
 };
 
-export const container = style({
-    top,
-    left,
-    zIndex: 1,
-    position: 'absolute',
-    filter: 'drop-shadow(0px 2px 2px rgba(0,0,0,0.2))',
-    opacity: 1,
-    padding,
-    transform: enterTransform,
-    cursor: 'default',
-});
+export const container = style([
+    sprinkles({
+        position: 'absolute',
+    }),
+    {
+        top,
+        left,
+        padding,
+        zIndex: 1,
+        opacity: 1,
+        transform: enterTransform,
+        filter: 'drop-shadow(0px 2px 2px rgba(0,0,0,0.2))',
+    },
+]);
 
 export const enterTransition = style({
     transition: `opacity .1s linear ${delay},transform .3s cubic-bezier(0.215,0.61,0.335,1) ${delay}`,
@@ -57,20 +60,28 @@ export const exitTransition = style({
     transition: `opacity .1s linear`,
 });
 
-export const tooltip = style({
-    position: 'relative',
-    boxSizing: 'border-box',
-    borderRadius: vars.borderRadii.popup,
-    background: vars.colors.backgroundContainer,
-});
+export const tooltip = style([
+    sprinkles({
+        position: 'relative',
+        borderRadius: vars.borderRadii.popup,
+        background: vars.colors.backgroundContainer,
+    }),
+    {
+        boxSizing: 'border-box',
+    },
+]);
 
-export const content = style({
-    padding: 7,
-    position: 'relative',
-    minWidth: 40,
-    maxWidth,
-    overflow: 'hidden',
-});
+export const content = style([
+    sprinkles({
+        position: 'relative',
+        minWidth: 40,
+        overflow: 'hidden',
+    }),
+    {
+        padding: 7,
+        maxWidth,
+    },
+]);
 
 export const tooltipCenter = sprinkles({
     display: 'flex',
@@ -78,21 +89,29 @@ export const tooltipCenter = sprinkles({
     alignItems: 'center',
 });
 
-export const arrowContainer = style({
-    position: 'absolute',
-    width: ARROW_CONTAINER_SIZE,
-    height: ARROW_CONTAINER_SIZE / 2,
-    overflow: 'hidden',
-});
+export const arrowContainer = style([
+    sprinkles({
+        position: 'absolute',
+        width: ARROW_CONTAINER_SIZE,
+        overflow: 'hidden',
+    }),
+    {
+        height: ARROW_CONTAINER_SIZE / 2,
+    },
+]);
 
-export const arrow = style({
-    width: ARROW_SIZE,
-    height: ARROW_SIZE,
-    position: 'absolute',
-    top: 0,
-    background: vars.colors.backgroundContainer,
-    left: '50%',
-    transform: 'translate(-50%, -50%) rotate(45deg)',
-    boxSizing: 'border-box',
-    borderRadius: '0 0 2px 0',
-});
+export const arrow = style([
+    sprinkles({
+        width: ARROW_SIZE,
+        height: ARROW_SIZE,
+        position: 'absolute',
+        top: 0,
+        background: vars.colors.backgroundContainer,
+    }),
+    {
+        left: '50%',
+        transform: 'translate(-50%, -50%) rotate(45deg)',
+        boxSizing: 'border-box',
+        borderRadius: '0 0 2px 0',
+    },
+]);
