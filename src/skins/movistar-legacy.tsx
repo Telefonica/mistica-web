d27b895829e6b55be1d078b8fba7f8e134e930ba
yceballost <yceballost@users.noreply.github.com>
2023-11-08 13:20:29 +0000

    feat(skin): update design tokens

--- src/skins/movistar-legacy.tsx
+++ src/skins/movistar-legacy.tsx
@@ -46,10 +46,7 @@ export const palette = {
     darkModeGrey: '#242424',
 };
 
-/**
- * @deprecated Use `getMovistarSkin` instead.
- */
-export const getMovistarLegacySkin: GetKnownSkin = (variant) => {
+export const getMovistarLegacySkin: GetKnownSkin = () => {
     const skin: KnownSkin = {
         name: MOVISTAR_LEGACY_SKIN,
         colors: {
@@ -109,6 +106,8 @@ export const getMovistarLegacySkin: GetKnownSkin = (variant) => {
             textLinkSnackbar: palette.movistarBlue30,
             control: palette.grey3,
             controlActivated: palette.movistarBlue,
+            controlInverse: palette.white,
+            controlActivatedInverse: palette.white,
             controlError: palette.pepper,
             loadingBar: palette.movistarBlue30,
             loadingBarBackground: palette.movistarBlue55,
@@ -216,6 +215,8 @@ export const getMovistarLegacySkin: GetKnownSkin = (variant) => {
             textLinkSnackbar: palette.movistarBlue30,
             control: palette.grey6,
             controlActivated: palette.movistarBlue,
+            controlInverse: palette.grey6,
+            controlActivatedInverse: palette.movistarBlue,
             controlError: palette.pepper,
             loadingBar: palette.movistarBlue,
             loadingBarBackground: palette.darkModeGrey,
@@ -299,15 +300,5 @@ export const getMovistarLegacySkin: GetKnownSkin = (variant) => {
             text10: {weight: 'bold'},
         },
     };
-
-    if (variant === 'prominent') {
-        skin.colors.brand = palette.movistarProminentBlue;
-        skin.colors.brandHigh = palette.movistarProminentBlueDark;
-        skin.colors.backgroundBrand = palette.movistarProminentBlue;
-        skin.colors.navigationBarBackground = palette.movistarProminentBlue;
-        skin.colors.backgroundFeedbackBottom = palette.movistarProminentBlue;
-        skin.colors.controlActivated = palette.movistarProminentBlue;
-        skin.colors.dividerInverse = palette.movistarProminentBlueDark;
-    }
     return skin;
 };
