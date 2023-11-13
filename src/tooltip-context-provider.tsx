8dd450aadbcf8b47b508df94ec5d8e654aa996c6
marcoskolodny <marcoskolodny@gmail.com>
2023-11-13 13:13:53 +0100

    add comment

--- src/tooltip-context-provider.tsx
+++ src/tooltip-context-provider.tsx
@@ -13,6 +13,16 @@ const TooltipStateContext = React.createContext<TooltipState>({openTooltipId: nu
 const TooltipStateSetterContext = React.createContext<TooltipSetter>({open: () => {}, close: () => {}});
 
 const TooltipContextProvider = ({children}: {children: React.ReactNode}): JSX.Element => {
+    /**
+     * We have to use a state and a ref to save the open tooltip id. If two targets (let's call them A and B) are next
+     * to each other (without space between them), the mouseOver/mouseLeave events will be triggered almost at the same time.
+     * Both tooltips will update their states, and it can happen that React decides to re-render B before A (i.e. it may first
+     * set the openTooltipId to B, and then to null because of mouse leaving A, causing both tooltips to be hidden).
+     *
+     * Setting the value inside the ref allows us to update the active tooltip id without triggering any re-renders, so this
+     * race condition won't happen. We also need the state with the active id because we want changes in this value to be
+     * reactive (i.e. trigger a re-render in each tooltip instance so that they can be opened/hidden accordingly).
+     */
     const [openTooltipId, setOpenTooltipId] = React.useState<string | null>(null);
     const openRef = React.useRef<string | null>(null);
 
