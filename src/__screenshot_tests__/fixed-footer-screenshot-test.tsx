5f17129b9204e5882486b7039ee1caa3b8cebbdf
Matteo Bertamini <matteo.bertamini@telefonica.com>
2023-11-14 18:58:00 +0100

    WEB-1619 consider safe-area-inset-bottom for FixedFooterLayout components

--- src/__screenshot_tests__/fixed-footer-screenshot-test.tsx
+++ src/__screenshot_tests__/fixed-footer-screenshot-test.tsx
@@ -37,3 +37,39 @@ test('Fixed footer with static position when height is smaller than the footer i
     const imageOverflowScrolled = await page.screenshot({fullPage: false});
     expect(imageOverflowScrolled).toMatchImageSnapshot();
 });
+jest.setTimeout(999999);
+
+test('Fixed footer with safe inset at the bottom', async () => {
+    const page = await openStoryPage({
+        id: 'layout-fixed-footer-layout--more-complex-footer',
+        device: 'MOBILE_IOS_INSET',
+    });
+
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
+
+    await (await screen.findByRole('button', {name: 'Load more text'})).click();
+    await (await screen.findByRole('button', {name: 'Load more text'})).click();
+    await (await screen.findByRole('button', {name: 'Load more text'})).click();
+
+    await (await screen.findByTestId('line-15')).evaluate((el) => el.scrollIntoView());
+
+    const imageOverflowLinesInset = await page.screenshot({fullPage: false});
+    expect(imageOverflowLinesInset).toMatchImageSnapshot();
+
+    await page.setViewport({
+        width: 564,
+        height: 150,
+        deviceScaleFactor: 1,
+        isMobile: true,
+        isLandscape: false,
+        hasTouch: true,
+    });
+
+    await (await screen.findByRole('button', {name: 'Load more text'})).evaluate((el) => el.scrollIntoView());
+    const imageOverflowLinesInsetHorizontal = await page.screenshot({fullPage: false});
+    expect(imageOverflowLinesInsetHorizontal).toMatchImageSnapshot();
+    //
+    // const imageOverflowScrolled = await page.screenshot({fullPage: false});
+    // expect(imageOverflowScrolled).toMatchImageSnapshot();
+});
--- src/utils/css.tsx
+++ src/utils/css.tsx
@@ -2,3 +2,8 @@ const BASE_FONT_SIZE = 16; // browser's default font-size in pixels
 
 export const pxToRem = (px: number | string): string =>
     `${(parseFloat(px as string) / BASE_FONT_SIZE).toFixed(3)}rem`;
+
+export const safeAreaInsetLeft = `max(env(safe-area-inset-left), var(--acceptance-test-override-safe-area-inset-left, 0px))`;
+export const safeAreaInsetTop = `max(env(safe-area-inset-top), var(--acceptance-test-override-safe-area-inset--top, 0px))`;
+export const safeAreaInsetBottom = `max(env(safe-area-inset-bottom), var(--acceptance-test-override-safe-area-inset--bottom, 0px))`;
+export const safeAreaInsetRight = `max(env(safe-area-inset-right), var(--acceptance-test-override-safe-area-inset--right, 0px))`;
