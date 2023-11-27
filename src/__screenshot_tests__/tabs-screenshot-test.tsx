2ade9f7ca97d47c86dd43209efa77872fa49f5ed
Marcos Kolodny <marcoskolodny@gmail.com>
2023-11-27 08:52:49 +0100

    feat(Tabs): add inverse/alternative variant, update story and add tests (#950)

--- src/__screenshot_tests__/tabs-screenshot-test.tsx
+++ src/__screenshot_tests__/tabs-screenshot-test.tsx
@@ -1,5 +1,7 @@
 import {openStoryPage, screen, setRootFontSize} from '../test-utils';
 
+const THEME_VARIANTS = ['default', 'inverse', 'alternative'] as const;
+
 test.each`
     device          | withIcon
     ${'MOBILE_IOS'} | ${true}
@@ -10,8 +12,9 @@ test.each`
     ${'DESKTOP'}    | ${false}
 `('Tabs in $device withIcon ($withIcon)', async ({device, withIcon}) => {
     await openStoryPage({
-        id: withIcon ? 'components-tabs--with-icon' : 'components-tabs--default',
+        id: 'components-tabs--default',
         device,
+        args: {icon: withIcon},
     });
     const image = await (await screen.findByRole('tablist')).screenshot();
     expect(image).toMatchImageSnapshot();
@@ -25,6 +28,69 @@ test('Tabs with large fontSize', async () => {
 
     await setRootFontSize(32);
 
+    const image = await (await screen.findByRole('tablist')).screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
+test('Tabs with long text', async () => {
+    await openStoryPage({
+        id: 'components-tabs--default',
+        device: 'MOBILE_IOS',
+        args: {text: 'A long text in the tab'},
+    });
+
+    const image = await (await screen.findByRole('tablist')).screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
+test('Tabs with long text and icon', async () => {
+    await openStoryPage({
+        id: 'components-tabs--default',
+        device: 'MOBILE_IOS',
+        args: {text: 'A long text in the tab', icon: true},
+    });
+
+    const image = await (await screen.findByRole('tablist')).screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
+test('Tabs selected line appears properly', async () => {
+    await openStoryPage({
+        id: 'components-tabs--default',
+        device: 'MOBILE_IOS',
+    });
+
+    await page.evaluate(() => {
+        document.querySelector<HTMLElement>('[data-tabindex="1"]')?.click();
+    });
+
+    const secondTabActive = await (await screen.findByRole('tablist')).screenshot();
+    expect(secondTabActive).toMatchImageSnapshot();
+
+    await page.evaluate(() => {
+        document.querySelector<HTMLElement>('[data-tabindex="2"]')?.click();
+    });
+
+    const thirdTabActive = await (await screen.findByRole('tablist')).screenshot();
+    expect(thirdTabActive).toMatchImageSnapshot();
+});
+
+test.each(THEME_VARIANTS)('Tabs in %s theme', async (theme) => {
+    await openStoryPage({
+        id: 'components-tabs--default',
+        device: 'MOBILE_IOS',
+        args: {theme},
+    });
+    const image = await (await screen.findByRole('tablist')).screenshot();
+    expect(image).toMatchImageSnapshot();
+});
+
+test('Tabs in dark mode', async () => {
+    await openStoryPage({
+        id: 'components-tabs--default',
+        device: 'MOBILE_IOS',
+        isDarkMode: true,
+    });
     const image = await (await screen.findByRole('tablist')).screenshot();
     expect(image).toMatchImageSnapshot();
 });
