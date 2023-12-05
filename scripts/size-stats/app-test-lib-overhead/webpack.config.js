d8f6569a6f89db1e92356a96308feb87a1116de9
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2023-12-05 11:18:20 +0100

    WEB-1645 update size stats

--- scripts/size-stats/app-test-lib-overhead/webpack.config.js
+++ scripts/size-stats/app-test-lib-overhead/webpack.config.js
@@ -1,12 +1,20 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const config = {
     entry: './src/index.js',
     // entry: './src/index-baseline.js', // uncomment to generate the baseline
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
     },
+
+    optimization: {
+        splitChunks: {chunks: 'async'},
+        runtimeChunk: {name: 'runtime'},
+    },
+
+    plugins: [process.env.BUNDLE_ANALYZER ? new BundleAnalyzerPlugin() : null].filter(Boolean),
+
     resolve: {
         alias: {'@telefonica/mistica': path.resolve(__dirname, '../../..')},
     },
