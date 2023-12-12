fde6386112a98e917c69ca32fb44cc7da23150aa
marcoskolodny <marcoskolodny@gmail.com>
2023-12-12 14:18:58 +0100

    fix custom render story

--- src/__stories__/radio-button-story.tsx
+++ src/__stories__/radio-button-story.tsx
@@ -1,114 +1,9 @@
 import * as React from 'react';
-import {Stack, Title1, RadioGroup, RadioButton, Inline, Text3, IconHandLeftRegular} from '..';
+import {Stack, RadioGroup, RadioButton, Inline, Text3} from '..';
 
 export default {
     title: 'Components/Radio Button',
 };
-
-export const Default: StoryComponent = () => {
-    const [value, setValue] = React.useState('banana');
-    const [uncontrolledValue, setUncontrolledValue] = React.useState('apple');
-    return (
-        <Stack space={32}>
-            <>
-                <Title1 id="uncontrolled">Uncontrolled: {uncontrolledValue}</Title1>
-                <RadioGroup
-                    name="uncontrolled"
-                    aria-labelledby="uncontrolled"
-                    onChange={setUncontrolledValue}
-                    defaultValue="apple"
-                >
-                    <Stack space={16}>
-                        <RadioButton value="banana">
-                            <Text3 regular>Banana</Text3>
-                        </RadioButton>
-                        <RadioButton value="apple">
-                            <Text3 regular>Apple</Text3>
-                        </RadioButton>
-                    </Stack>
-                </RadioGroup>
-            </>
-            <>
-                <Title1 id="controlled">Selected fruit: {value}</Title1>
-                <RadioGroup name="group1" aria-labelledby="controlled" value={value} onChange={setValue}>
-                    <Stack space={16}>
-                        <RadioButton value="banana">
-                            <Text3 regular>Banana</Text3>
-                        </RadioButton>
-                        <RadioButton value="apple">
-                            <Text3 regular>Apple</Text3>
-                        </RadioButton>
-                    </Stack>
-                </RadioGroup>
-            </>
-            <>
-                <Title1 id="custom-render">Custom render - Selected fruit: {value}</Title1>
-                <RadioGroup name="group2" aria-labelledby="custom-render" value={value} onChange={setValue}>
-                    <Stack space={16}>
-                        <RadioButton
-                            value="banana"
-                            render={() => (
-                                <Inline space={16}>
-                                    <Text3 regular>Banana</Text3>
-                                    {value === 'banana' && <IconHandLeftRegular size={20} />}
-                                </Inline>
-                            )}
-                        />
-                        <RadioButton
-                            value="apple"
-                            render={() => (
-                                <Inline space={16}>
-                                    <Text3 regular>Apple</Text3>
-                                    {value === 'apple' && <IconHandLeftRegular size={20} />}
-                                </Inline>
-                            )}
-                        />
-                    </Stack>
-                </RadioGroup>
-            </>
-            <>
-                <Title1 id="disabled">Disabled</Title1>
-                <RadioGroup
-                    disabled
-                    name="group3"
-                    aria-labelledby="disabled"
-                    value={value}
-                    onChange={setValue}
-                >
-                    <Stack space={16}>
-                        <RadioButton value="banana">
-                            <Text3 regular>Banana</Text3>
-                        </RadioButton>
-                        <RadioButton value="apple">
-                            <Text3 regular>Apple</Text3>
-                        </RadioButton>
-                    </Stack>
-                </RadioGroup>
-            </>
-            <>
-                <Title1 id="multiline">Multiline</Title1>
-                <RadioGroup name="group4" aria-labelledby="multiline" value={value} onChange={setValue}>
-                    <Stack space={16}>
-                        <RadioButton value="banana">
-                            <Text3 regular>
-                                Checkbox with a very large very large very large very large very large very
-                                large very large very large very large text that falls
-                            </Text3>
-                        </RadioButton>
-                        <RadioButton value="apple">
-                            <Text3 regular>
-                                Checkbox with a very large very large very large very large very large very
-                                large very large very large very large text that falls
-                            </Text3>
-                        </RadioButton>
-                    </Stack>
-                </RadioGroup>
-            </>
-        </Stack>
-    );
-};
-
-Default.storyName = 'Radio Button';
 
 type Args = {
     disabled: boolean;
@@ -163,19 +58,31 @@ export const CustomRender: StoryComponent<Args> = ({disabled}) => {
             <Stack space={16}>
                 <RadioButton
                     value="first"
-                    render={() => (
+                    render={({controlElement, labelId}) => (
                         <Inline space={16}>
-                            <Text3 regular>First option</Text3>
-                            {value === 'first' && <IconHandLeftRegular size={20} />}
+                            {/* Text3 wrapper added to have the same line-height and center radio button with text and -2px to perfect pixel center icon */}
+                            <Text3 regular as="div">
+                                <div style={{position: 'relative', top: -2}}>{controlElement}</div>
+                            </Text3>
+                            {/* set the text id to match the radio button name, so this text can be linked as label for accessibility */}
+                            <Text3 regular id={labelId}>
+                                First option
+                            </Text3>
                         </Inline>
                     )}
                 />
                 <RadioButton
                     value="second"
-                    render={() => (
+                    render={({controlElement, labelId}) => (
                         <Inline space={16}>
-                            <Text3 regular>Second option</Text3>
-                            {value === 'second' && <IconHandLeftRegular size={20} />}
+                            {/* Text3 wrapper added to have the same line-height and center radio button with text and -2px to perfect pixel center icon */}
+                            <Text3 regular as="div">
+                                <div style={{position: 'relative', top: -2}}>{controlElement}</div>
+                            </Text3>
+                            {/* set the text id to match the radio button name, so this text can be linked as label for accessibility */}
+                            <Text3 regular id={labelId}>
+                                Second option
+                            </Text3>
                         </Inline>
                     )}
                 />
