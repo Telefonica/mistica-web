eaf0f841915629a6d77de9bdb454cc10f9fbb465
Abel Toledano <atoledano@tuenti.com>
2024-01-03 16:03:37 +0100

    WEB-1672 fix(Carousel): SSR Layout shift issues

--- src/__acceptance_tests__/mosaic-ssr-acceptance-test.tsx
+++ src/__acceptance_tests__/mosaic-ssr-acceptance-test.tsx
@@ -1,6 +1,5 @@
 import {openSSRPage} from '../test-utils';
 
-// TODO WEB-1672: fix hydration visual mismatches and enable the check. These mismatches are caused by carousel when the itemsPerPage === items.length
 test('ssr VerticalMosaic and HorizontalMosaic', async () => {
-    await openSSRPage({name: 'mosaic', checkHidrationVisualMismatch: false});
+    await openSSRPage({name: 'mosaic'});
 });
