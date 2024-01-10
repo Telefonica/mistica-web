f7254c7739080b978c7bb1ed1db33149bbdd2db5
Pedro Ladaria <pladaria@tuenti.com>
2024-01-10 12:52:10 +0100

    feat(Inline): use flex/grid gap if available (#987)
    Co-authored-by: Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>

--- src/inline.css.ts
+++ src/inline.css.ts
@@ -34,23 +34,44 @@ export const flexInline = style({
     justifyContent: space,
 });
 
+/**
+ * https://caniuse.com/flexbox-gap
+ * chrome 84; safari 14.1
+ */
+const supportsFlexGap = '(display: flex) and (gap: 0px)';
+
 export const inline = style({
     pointerEvents: 'none', // to prevent negative margins from affecting clickable areas
-    flexDirection: 'row',
     gridAutoFlow: 'column',
+    marginTop: `calc(${space} * -1)`,
+    marginLeft: `calc(${space} * -1)`,
+    '@supports': {
+        [supportsFlexGap]: {
+            margin: 0, // restore
+            pointerEvents: 'auto', // restore
+            display: 'inline-flex',
+            flexDirection: 'row',
+            gap: space,
+        },
+    },
 });
 
 export const fullWidth = style([
     inline,
     {
         display: ['flex', 'grid'],
+        // style repeated to override the inline @supports style
+        '@supports': {
+            [supportsFlexGap]: {
+                display: ['flex', 'grid'],
+            },
+        },
     },
 ]);
 
 export const wrap = style([
     inline,
     {
-        display: 'inline-flex',
         flexWrap: 'wrap',
     },
 ]);
@@ -59,6 +80,11 @@ export const noFullWidth = style([
     inline,
     {
         display: ['inline-flex', 'inline-grid'],
+        '@supports': {
+            [supportsFlexGap]: {
+                display: 'inline-flex',
+            },
+        },
     },
 ]);
 
@@ -69,6 +95,25 @@ globalStyle(`${marginInline} > div`, {
 
 globalStyle(`${inline} > div`, {
     pointerEvents: 'auto', // restore pointer events for children
+    // Hack to fix https://jira.tid.es/browse/WEB-1683
+    // In iOS the inline component sometimes cuts the last line of the content
+    paddingBottom: 1,
+    '@supports': {
+        [supportsFlexGap]: {
+            padding: 0, // restore
+            // negative gap is not supported in flexbox, so we use negative margins instead
+            margin: `0 0 0 calc(min(${space}, 0px))`,
+        },
+    },
+});
+
+globalStyle(`${inline} > div:first-child`, {
+    marginLeft: space,
+    '@supports': {
+        [supportsFlexGap]: {
+            marginLeft: 0,
+        },
+    },
 });
 
 globalStyle(`${inline} > div:empty`, {
