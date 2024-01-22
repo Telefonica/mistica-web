b8bc057e5f6c7ebaa66a7a95df991aabeb50c69d
marcoskolodny <marcoskolodny@gmail.com>
2024-01-22 20:40:34 +0100

    simplify positioning of calendar icon in date fields

--- src/date-field.css.ts
+++ src/date-field.css.ts
@@ -1,28 +1,13 @@
 import {style} from '@vanilla-extract/css';
-import * as mq from './media-queries.css';
-import {
-    fieldRightPadding,
-    fieldVerticalPadding,
-    iconButtonSize,
-    iconSize,
-    inputLineHeight,
-    shrinkedLabelLineHeight,
-} from './text-field-base.css';
+import {fieldRightPadding, iconButtonSize, iconSize} from './text-field-base.css';
 
 export const iconContainer = style({
     position: 'absolute',
-    top: `calc((${shrinkedLabelLineHeight.desktop} + 2 * ${fieldVerticalPadding}px + ${inputLineHeight} - 2px) / 2)`, // Field height / 2
-    right: `calc(${fieldRightPadding}px + (${iconButtonSize} - ${iconSize}) / 2)`,
     pointerEvents: 'none',
+    height: '100%',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    right: `calc(${fieldRightPadding}px + (${iconButtonSize} - ${iconSize}) / 2)`,
     marginRight: -8,
-    marginTop: `calc(-1 * ${iconSize} / 2)`, // center icon vertically
-
-    '@media': {
-        [mq.tabletOrSmaller]: {
-            top: `calc((${shrinkedLabelLineHeight.mobile} + 2 * ${fieldVerticalPadding}px + ${inputLineHeight} - 2px) / 2)`, // Field height / 2
-        },
-    },
 });
