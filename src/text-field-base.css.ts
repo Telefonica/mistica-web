ff5a8061f4b786608a24a3aee2fc69941609735f
marcoskolodny <marcoskolodny@gmail.com>
2024-01-23 16:25:32 +0100

    update date field shadow element to remove harcoded values

--- src/text-field-base.css.ts
+++ src/text-field-base.css.ts
@@ -143,11 +143,9 @@ export const input = style([
             '&::-webkit-calendar-picker-indicator': {
                 position: 'absolute',
                 top: 0,
-                left: -24, // to fully cover input area
-                right: 0,
-                bottom: 0,
-                width: 'auto',
-                height: 'auto',
+                left: 0,
+                width: '100%',
+                height: '100%',
                 opacity: 0,
                 color: 'transparent',
                 background: 'transparent',
