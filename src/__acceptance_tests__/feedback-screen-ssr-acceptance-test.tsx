6e88aeece85f1d134d71e54467e666d163f7481c
Abel Toledano <atoledano@tuenti.com>
2024-01-23 17:27:40 +0100

    enable ssr tests

--- src/__acceptance_tests__/feedback-screen-ssr-acceptance-test.tsx
+++ src/__acceptance_tests__/feedback-screen-ssr-acceptance-test.tsx
@@ -1,16 +1,13 @@
 import {openSSRPage} from '../test-utils';
 
 test('ssr success feedback screen', async () => {
-    // TODO: fix hydration mismatches and enable the check
     await openSSRPage({name: 'feedback-screen-success', checkHidrationVisualMismatch: true});
 });
 
 test('ssr error feedback screen', async () => {
-    // TODO WEB-1673: fix hydration mismatches and enable the check
-    await openSSRPage({name: 'feedback-screen-error', checkHidrationVisualMismatch: false});
+    await openSSRPage({name: 'feedback-screen-error', checkHidrationVisualMismatch: true});
 });
 
 test('ssr info feedback screen', async () => {
-    // TODO WEB-1673: fix hydration mismatches and enable the check
-    await openSSRPage({name: 'feedback-screen-info', checkHidrationVisualMismatch: false});
+    await openSSRPage({name: 'feedback-screen-info', checkHidrationVisualMismatch: true});
 });
