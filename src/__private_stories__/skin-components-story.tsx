02d48775ecfd0de557b625276146763436cd7f24
marcoskolodny <marcoskolodny@gmail.com>
2024-02-01 14:04:17 +0100

    add components and tests

--- src/__private_stories__/skin-components-story.tsx
+++ src/__private_stories__/skin-components-story.tsx
@@ -4,8 +4,6 @@ import {
     Stack,
     Box,
     Text2,
-    Accordion,
-    AccordionItem,
     skinVars,
     BoxedAccordion,
     BoxedAccordionItem,
@@ -19,8 +17,27 @@ import {
     ButtonLink,
     Callout,
     IconBoxLight,
+    DataCard,
+    IconLightningRegular,
+    DisplayMediaCard,
+    ButtonLinkDanger,
+    PageBullets,
+    Checkbox,
+    Chip,
+    Counter,
+    TextField,
+    ProgressBar,
+    RadioButton,
+    RadioGroup,
+    Slider,
+    Stepper,
+    Switch,
+    Tag,
+    IconCloseRegular,
+    TextLink,
 } from '..';
 import avatarImg from '../__stories__/images/avatar.jpg';
+import usingVrImg from '../__stories__/images/using-vr.jpg';
 
 export default {
     title: 'Private/Components in different skins',
@@ -37,70 +54,75 @@ export const Default: StoryComponent<Args> = ({inverse}) => {
     return (
         <ResponsiveLayout fullWidth isInverse={inverse}>
             <Box padding={16}>
-                <Stack space={16}>
+                <Stack space={32}>
                     <Text2 regular>
                         This story is created to verify how some of the components look like when using
                         different skins
                     </Text2>
-                    <Stack space={16}>
-                        {/** Accordion */}
-                        <Accordion>
-                            <AccordionItem
-                                title="Title 1"
-                                content={
-                                    <Text2 regular color={skinVars.colors.textSecondary}>
-                                        Content 2
-                                    </Text2>
-                                }
-                            />
-                            <AccordionItem
-                                title="Title 2"
-                                content={
-                                    <Text2 regular color={skinVars.colors.textSecondary}>
-                                        Content 2
-                                    </Text2>
-                                }
-                            />
-                        </Accordion>
 
-                        {/** BoxedAccordion */}
-                        <BoxedAccordion>
-                            <BoxedAccordionItem
-                                title="Title 1"
-                                content={
-                                    <Text2 regular color={skinVars.colors.textSecondary}>
-                                        Content 2
-                                    </Text2>
-                                }
-                            />
-                            <BoxedAccordionItem
-                                title="Title 2"
-                                content={
-                                    <Text2 regular color={skinVars.colors.textSecondary}>
-                                        Content 2
-                                    </Text2>
-                                }
-                            />
-                        </BoxedAccordion>
-
-                        {/** Avatar */}
-                        <Inline space={16} alignItems="center">
-                            <Avatar src={avatarImg} size={64} badge={5} />
-                            <Avatar initials="MK" size={64} />
-                            <Avatar initials="MK" size={64} border />
-                        </Inline>
-
-                        {/** Breadcrumbs */}
-                        <NavigationBreadcrumbs
-                            title="Subsection"
-                            breadcrumbs={[
-                                {title: 'Home', url: 'https://example.org?path=home'},
-                                {title: 'Section', url: 'https://example.org?path=section'},
-                            ]}
+                    {/** BoxedAccordion */}
+                    <BoxedAccordion>
+                        <BoxedAccordionItem
+                            title="Title 1"
+                            content={
+                                <Text2 regular color={skinVars.colors.textSecondary}>
+                                    Content 2
+                                </Text2>
+                            }
                         />
+                        <BoxedAccordionItem
+                            title="Title 2"
+                            content={
+                                <Text2 regular color={skinVars.colors.textSecondary}>
+                                    Content 2
+                                </Text2>
+                            }
+                        />
+                    </BoxedAccordion>
 
-                        {/** ButtonPrimary */}
-                        <Inline space={16} alignItems="center">
+                    {/** Avatar */}
+                    <Inline space={16} alignItems="center">
+                        <Avatar src={avatarImg} size={64} badge={5} />
+                        <Avatar size={64} />
+                        <Avatar initials="MK" size={64} border />
+                    </Inline>
+
+                    {/** Breadcrumbs */}
+                    <NavigationBreadcrumbs
+                        title="Subsection"
+                        breadcrumbs={[
+                            {title: 'Home', url: 'https://example.org?path=home'},
+                            {title: 'Section', url: 'https://example.org?path=section'},
+                        ]}
+                    />
+
+                    {/** Callout */}
+                    <Callout
+                        icon={<IconBoxLight />}
+                        onClose={() => {}}
+                        title="Title"
+                        description="Description"
+                        button={
+                            <ButtonSecondary
+                                StartIcon={IconPhotoCameraRegular}
+                                EndIcon={IconPhotoCameraRegular}
+                                onPress={() => {}}
+                            >
+                                ButtonSecondary
+                            </ButtonSecondary>
+                        }
+                    />
+
+                    {/** DataCard */}
+                    <DataCard
+                        title="Title"
+                        subtitle="Subtitle"
+                        pretitle="Pretitle"
+                        headline="Headline"
+                        description="Description"
+                        onClose={() => {}}
+                        actions={[{label: 'action', Icon: IconLightningRegular, onPress: () => {}}]}
+                        button={
                             <ButtonPrimary
                                 StartIcon={IconPhotoCameraRegular}
                                 EndIcon={IconPhotoCameraRegular}
@@ -108,86 +130,126 @@ export const Default: StoryComponent<Args> = ({inverse}) => {
                             >
                                 Button
                             </ButtonPrimary>
-                            <ButtonPrimary showSpinner loadingText="Loading" onPress={() => {}}>
-                                Button
-                            </ButtonPrimary>
-                            <ButtonPrimary
-                                StartIcon={IconPhotoCameraRegular}
-                                EndIcon={IconPhotoCameraRegular}
-                                onPress={() => {}}
-                                small
-                            >
-                                Button
-                            </ButtonPrimary>
-                        </Inline>
-
-                        {/** ButtonSecondary */}
-                        <Inline space={16} alignItems="center">
-                            <ButtonSecondary
-                                StartIcon={IconPhotoCameraRegular}
-                                EndIcon={IconPhotoCameraRegular}
-                                onPress={() => {}}
-                            >
-                                Button
-                            </ButtonSecondary>
-                            <ButtonSecondary showSpinner loadingText="Loading" onPress={() => {}}>
-                                Button
-                            </ButtonSecondary>
-                            <ButtonSecondary
-                                StartIcon={IconPhotoCameraRegular}
-                                EndIcon={IconPhotoCameraRegular}
-                                onPress={() => {}}
-                                small
-                            >
-                                Button
-                            </ButtonSecondary>
-                        </Inline>
-
-                        {/** ButtonDanger */}
-                        <Inline space={16} alignItems="center">
-                            <ButtonDanger
-                                StartIcon={IconPhotoCameraRegular}
-                                EndIcon={IconPhotoCameraRegular}
-                                onPress={() => {}}
-                            >
-                                Button
-                            </ButtonDanger>
-                            <ButtonDanger showSpinner loadingText="Loading" onPress={() => {}}>
-                                Button
-                            </ButtonDanger>
-                            <ButtonDanger
-                                StartIcon={IconPhotoCameraRegular}
-                                EndIcon={IconPhotoCameraRegular}
-                                onPress={() => {}}
-                                small
-                            >
-                                Button
-                            </ButtonDanger>
-                        </Inline>
-
-                        {/** ButtonLink */}
-                        <Inline space={16} alignItems="center">
+                        }
+                        buttonLink={
                             <ButtonLink
                                 StartIcon={IconPhotoCameraRegular}
                                 EndIcon={IconPhotoCameraRegular}
                                 onPress={() => {}}
                             >
-                                Button
+                                Link
                             </ButtonLink>
-                            <ButtonLink showSpinner loadingText="Loading" onPress={() => {}}>
-                                Button
-                            </ButtonLink>
-                            <ButtonLink href="https://www.google.com/">Button</ButtonLink>
-                        </Inline>
+                        }
+                    />
 
-                        {/** Callout */}
-                        <Callout
-                            icon={<IconBoxLight />}
-                            onClose={() => {}}
-                            title="Title"
-                            description="Description"
-                        />
-                    </Stack>
+                    {/** DisplayMediaCard */}
+                    <DisplayMediaCard
+                        backgroundImage={usingVrImg}
+                        headline={
+                            <Tag type="error" Icon={IconCloseRegular}>
+                                Headline
+                            </Tag>
+                        }
+                        title="Title"
+                        pretitle="Pretitle"
+                        description="Description"
+                        onClose={() => {}}
+                        actions={[{label: 'action', Icon: IconLightningRegular, onPress: () => {}}]}
+                        button={
+                            <ButtonDanger
+                                StartIcon={IconPhotoCameraRegular}
+                                EndIcon={IconPhotoCameraRegular}
+                                onPress={() => {}}
+                            >
+                                ButtonDanger
+                            </ButtonDanger>
+                        }
+                        buttonLink={
+                            <ButtonLinkDanger
+                                StartIcon={IconPhotoCameraRegular}
+                                EndIcon={IconPhotoCameraRegular}
+                                onPress={() => {}}
+                            >
+                                LinkDanger
+                            </ButtonLinkDanger>
+                        }
+                    />
+
+                    {/** Carousel page bullets */}
+                    <PageBullets currentIndex={2} numPages={10} />
+
+                    {/** Checkbox */}
+                    <Inline space={16}>
+                        <Checkbox name="checkbox 1">Unchecked</Checkbox>
+
+                        <Checkbox name="checkbox 2" defaultChecked>
+                            Checked
+                        </Checkbox>
+
+                        <Checkbox name="checkbox 3" defaultChecked disabled>
+                            Disabled
+                        </Checkbox>
+                    </Inline>
+
+                    {/** RadioButton */}
+                    <RadioGroup name="radio-group" defaultValue="1">
+                        <Inline space={16}>
+                            <RadioButton value="1">First</RadioButton>
+                            <RadioButton value="2">Second</RadioButton>
+                        </Inline>
+                    </RadioGroup>
+
+                    <Inline space={16}>
+                        <Switch name="switch 1">Unchecked</Switch>
+
+                        <Switch name="switch 2" checked>
+                            Checked
+                        </Switch>
+
+                        <Switch name="switch 3" disabled>
+                            Disabled
+                        </Switch>
+                    </Inline>
+
+                    {/** Chip */}
+                    <Inline space={16}>
+                        <Chip Icon={IconLightningRegular} active>
+                            Active chip
+                        </Chip>
+                        <Chip badge={10} Icon={IconLightningRegular} active>
+                            Active chip with badge
+                        </Chip>
+                    </Inline>
+
+                    {/** Counter */}
+                    <Inline space={16}>
+                        <Counter min={0} max={10} defaultValue={10} />
+                        <Counter min={0} max={10} defaultValue={0} onRemove={() => {}} />
+                    </Inline>
+
+                    {/** TextField */}
+                    <TextField
+                        name="text"
+                        label="Label"
+                        defaultValue="Value"
+                        prefix="Prefix"
+                        error
+                        optional
+                        endIcon={<IconLightningRegular />}
+                        helperText="Helper text"
+                    />
+
+                    {/** ProgressBar */}
+                    <ProgressBar progressPercent={50} />
+
+                    {/** Slider */}
+                    <Slider name="slider" min={0} max={100} defaultValue={50} />
+
+                    {/** Stepper */}
+                    <Stepper currentIndex={2} steps={['First', 'Second', 'Third', 'Fourth', 'Fifth']} />
+
+                    {/** TextLink */}
+                    <TextLink onPress={() => {}}>This is a text link</TextLink>
                 </Stack>
             </Box>
         </ResponsiveLayout>
---
+++ src/__screenshot_tests__/private-skin-components-screenshot-test.tsx
@@ -0,0 +1,43 @@
+import {openStoryPage} from '../test-utils';
+
+const SKINS = ['Movistar', 'O2', 'Vivo', 'Vivo-new', 'Telefonica', 'Blau'] as const;
+
+test.each(SKINS)('Components in %s', async (skin) => {
+    const page = await openStoryPage({
+        id: 'private-components-in-different-skins--default',
+        skin,
+    });
+
+    expect(await page.screenshot({fullPage: true})).toMatchImageSnapshot();
+});
+
+test.each(SKINS)('Components in %s (inverse)', async (skin) => {
+    const page = await openStoryPage({
+        id: 'private-components-in-different-skins--default',
+        args: {inverse: true},
+        skin,
+    });
+
+    expect(await page.screenshot({fullPage: true})).toMatchImageSnapshot();
+});
+
+test.each(SKINS)('Components in %s (dark mode)', async (skin) => {
+    const page = await openStoryPage({
+        id: 'private-components-in-different-skins--default',
+        isDarkMode: true,
+        skin,
+    });
+
+    expect(await page.screenshot({fullPage: true})).toMatchImageSnapshot();
+});
+
+test.each(SKINS)('Components in %s (inverse and dark mode)', async (skin) => {
+    const page = await openStoryPage({
+        id: 'private-components-in-different-skins--default',
+        args: {inverse: true},
+        isDarkMode: true,
+        skin,
+    });
+
+    expect(await page.screenshot({fullPage: true})).toMatchImageSnapshot();
+});
