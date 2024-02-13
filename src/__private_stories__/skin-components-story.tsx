d215b09b0a077834931a1ae78ffa2f9761b6fae4
marcoskolodny <marcoskolodny@gmail.com>
2024-02-13 13:22:59 +0100

    add iconButton to skins story

--- src/__private_stories__/skin-components-story.tsx
+++ src/__private_stories__/skin-components-story.tsx
@@ -41,6 +41,7 @@ import {
     Title1,
     Title2,
     Title3,
+    IconButton,
 } from '..';
 import avatarImg from '../__stories__/images/avatar.jpg';
 import usingVrImg from '../__stories__/images/using-vr.jpg';
@@ -108,7 +109,7 @@ export const Default: StoryComponent<Args> = ({variant}) => {
                     </BoxedAccordion>
 
                     {/** Avatar */}
-                    <Inline space={16} alignItems="center">
+                    <Inline space={16} alignItems="center" wrap>
                         <Avatar src={avatarImg} size={64} badge={5} />
                         <Avatar size={64} />
                         <Avatar initials="MK" size={64} border />
@@ -200,7 +201,7 @@ export const Default: StoryComponent<Args> = ({variant}) => {
                     <PageBullets currentIndex={2} numPages={10} />
 
                     {/** Checkbox */}
-                    <Inline space={16}>
+                    <Inline space={16} wrap>
                         <Checkbox name="checkbox 1">Unchecked</Checkbox>
 
                         <Checkbox name="checkbox 2" defaultChecked>
@@ -214,13 +215,13 @@ export const Default: StoryComponent<Args> = ({variant}) => {
 
                     {/** RadioButton */}
                     <RadioGroup name="radio-group" defaultValue="1">
-                        <Inline space={16}>
+                        <Inline space={16} wrap>
                             <RadioButton value="1">First</RadioButton>
                             <RadioButton value="2">Second</RadioButton>
                         </Inline>
                     </RadioGroup>
 
-                    <Inline space={16}>
+                    <Inline space={16} wrap>
                         <Switch name="switch 1">Unchecked</Switch>
 
                         <Switch name="switch 2" checked>
@@ -233,7 +234,7 @@ export const Default: StoryComponent<Args> = ({variant}) => {
                     </Inline>
 
                     {/** Chip */}
-                    <Inline space={16}>
+                    <Inline space={16} wrap>
                         <Chip Icon={IconLightningRegular} active>
                             Active chip
                         </Chip>
@@ -243,7 +244,7 @@ export const Default: StoryComponent<Args> = ({variant}) => {
                     </Inline>
 
                     {/** Counter */}
-                    <Inline space={16}>
+                    <Inline space={16} wrap>
                         <Counter min={0} max={10} defaultValue={10} />
                         <Counter min={0} max={10} defaultValue={0} onRemove={() => {}} />
                     </Inline>
@@ -280,20 +281,35 @@ export const Default: StoryComponent<Args> = ({variant}) => {
                     <SkeletonText />
 
                     {/** Titles */}
-                    <Inline space={16} alignItems="baseline">
+                    <Inline space={16} alignItems="baseline" wrap>
                         <Title1>Title 1</Title1>
                         <Title2>Title 2</Title2>
                         <Title3>Title 3</Title3>
                     </Inline>
 
                     {/** Tag */}
-                    <Inline space={16}>
+                    <Inline space={16} wrap>
                         <Tag type="promo">Promo</Tag>
                         <Tag type="active">Active</Tag>
                         <Tag type="inactive">Inactive</Tag>
                         <Tag type="success">Success</Tag>
                         <Tag type="warning">Warning</Tag>
                         <Tag type="error">Error</Tag>
+                    </Inline>
+
+                    {/** IconButton */}
+                    <Inline space={16} wrap>
+                        <IconButton Icon={IconLightningRegular} type="neutral" variant="default" />
+                        <IconButton Icon={IconLightningRegular} type="neutral" variant="soft" />
+                        <IconButton Icon={IconLightningRegular} type="neutral" variant="solid" />
+
+                        <IconButton Icon={IconLightningRegular} type="brand" variant="default" />
+                        <IconButton Icon={IconLightningRegular} type="brand" variant="soft" />
+                        <IconButton Icon={IconLightningRegular} type="brand" variant="solid" />
+
+                        <IconButton Icon={IconLightningRegular} type="danger" variant="default" />
+                        <IconButton Icon={IconLightningRegular} type="danger" variant="soft" />
+                        <IconButton Icon={IconLightningRegular} type="danger" variant="solid" />
                     </Inline>
                 </Stack>
             </Box>
