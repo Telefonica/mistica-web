4f28b34f5170199e2e308611764942a522e1a048
Yayo <yceballost@gmail.com>
2024-02-27 13:33:50 +0100

    adding padding in naked

--- src/card.css.ts
+++ src/card.css.ts
@@ -230,6 +230,7 @@ export const nakedCardContent = style([
         flexDirection: 'column',
         justifyContent: 'space-between',
         paddingTop: 16,
+        paddingRight: 16,
     }),
     {
         transition: 'opacity 0.1s ease-in-out',
