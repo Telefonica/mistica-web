9edc7527aa576739eb6263df2f978ddbbc6f2f44
Abel Toledano <atoledano@tuenti.com>
2024-03-13 17:29:16 +0100

    fixes

--- src/overscroll-color-context.tsx
+++ src/overscroll-color-context.tsx
@@ -15,7 +15,7 @@ type OverscrollColorConfig = {topColor?: string; bottomColor?: string};
 const {Provider, useSetValue, useValue} = createNestableContext<OverscrollColorConfig>({});
 
 const shouldRender = (platformOverrides: Theme['platformOverrides']) =>
-    getPlatform(platformOverrides) === 'ios' || true;
+    getPlatform(platformOverrides) === 'ios';
 
 const TopOverscrollColor = () => {
     const {topColor} = useValue();
@@ -24,47 +24,58 @@ const TopOverscrollColor = () => {
         if (!topColor) {
             return;
         }
-        const metaTags = document.head.querySelectorAll('meta[name=theme-color]');
-        metaTags.forEach((metaTag) => {
+
+        const prevMetaTags = document.head.querySelectorAll('meta[name=theme-color]');
+        prevMetaTags.forEach((metaTag) => {
             metaTag.remove();
         });
+
         const meta = document.createElement('meta');
         meta.name = 'theme-color';
         meta.media = isDarkMode ? '(prefers-color-scheme: dark)' : '(prefers-color-scheme: light)';
         // css vars are not supported in theme-color meta tag
         meta.content = isCssVar(topColor) ? getCssVarValue(topColor) : topColor;
+
         document.head.appendChild(meta);
+
         return () => {
+            // remove the overscroll color meta tag and restore previous meta tags
             meta.remove();
+            prevMetaTags.forEach((metaTag) => {
+                document.head.appendChild(metaTag);
+            });
         };
     }, [isDarkMode, topColor]);
     return null;
 };
 
 const BottomOverscrollColor = () => {
-    const [defaultBottomColor, setDefaultBottomColor] = React.useState<string>(vars.colors.background);
-
-    React.useEffect(() => {
-        setDefaultBottomColor(getComputedStyle(document.body).backgroundColor);
-    }, []);
-
     const {topColor, bottomColor} = useValue();
     // if not specified a bottom color, dont render it, except if there is a top color defined,
     // in that case we need to render the bottom color to avoid theme-color affecting the bottom overscroll
     if (!bottomColor && !topColor) {
         return null;
     }
+
+    const defaultBottomColor = getComputedStyle(document.body).backgroundColor;
+    const bottomColorToApply = bottomColor ?? defaultBottomColor;
+
     // if top and bottom color are the same, theme-color set for top color will work for bottom too, so this is not needed
-    if (bottomColor === topColor) {
+    if (bottomColorToApply === topColor) {
         return null;
     }
 
+    // This sets a fixed div at the bottom of the screen with the bottom color. Fixed elements aren't affected by iOS scroll bounce,
+    // so this div is always visible as background.
+    // There is a known issue with this approach, if the bottom overscroll color is different from the body background color and there is a
+    // page section with a transparent background which is expected to have the background color of the body, the div with the bottom overscroll
+    // color will be visible behind the transparent section.
     return (
         <div
             style={{
                 position: 'fixed',
                 zIndex: -1,
-                background: bottomColor ?? defaultBottomColor,
+                background: bottomColorToApply,
                 width: '100%',
                 height: 300,
                 left: 0,
