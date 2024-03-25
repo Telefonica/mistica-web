762e1eaa973f2e813d3ddafafa91b0b3b1db277a
Raul Cesar <50491912+raulcesar54@users.noreply.github.com>
2024-03-25 09:19:08 -0400

    Update examples/vite-example/src/atomic/page/index.tsx
    Co-authored-by: Ignacio Ceballos (Yayo) <ignacio.ceballostorres@telefonica.com>

--- examples/vite-example/src/atomic/page/index.tsx
+++ examples/vite-example/src/atomic/page/index.tsx
@@ -27,7 +27,7 @@ export const Main = () => {
           button={<ButtonPrimary fake>Action</ButtonPrimary>}
           buttonLink={<ButtonLink href="#">Action Link Example</ButtonLink>}
           pretitle={"Pretitle example"}
-          title={"HERO TITLE"}
+          title={"Hero title"}
           description={
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
           }
