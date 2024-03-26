3ce0fac8848874b52fc2cd169304ebee8a449a81
Abel Toledano <atoledano@tuenti.com>
2024-03-26 09:25:29 +0100

    feat(ButtonLayout): change alignment logic (#1055)
    WEB-1763
    spec:
    https://www.figma.com/file/koROdh3HpEPG2O8jG52Emh/%F0%9F%94%B8-Buttons-Component-Specs?type=design&node-id=32-6371&mode=design&t=RUO9SmBqwNvVpHKG-0

--- src/__stories__/dialog-story.tsx
+++ src/__stories__/dialog-story.tsx
@@ -18,83 +18,89 @@ export default {
 export const Alert: StoryComponent = () => {
     const {alert} = useDialog();
     return (
-        <ButtonLayout>
-            <ButtonPrimary
-                aria-haspopup="dialog"
-                onPress={() =>
-                    alert({
-                        title: 'Profile updated',
-                        message: 'Your changes have been successfully saved',
-                        acceptText: 'Ok',
-                        onAccept: () => console.log('Accepted'),
-                    })
-                }
-            >
-                Open
-            </ButtonPrimary>
-        </ButtonLayout>
+        <ButtonLayout
+            primaryButton={
+                <ButtonPrimary
+                    aria-haspopup="dialog"
+                    onPress={() =>
+                        alert({
+                            title: 'Profile updated',
+                            message: 'Your changes have been successfully saved',
+                            acceptText: 'Ok',
+                            onAccept: () => console.log('Accepted'),
+                        })
+                    }
+                >
+                    Open
+                </ButtonPrimary>
+            }
+        />
     );
 };
 
 export const Confirm: StoryComponent<{destructive: boolean}> = ({destructive}) => {
     const {confirm} = useDialog();
     return (
-        <ButtonLayout>
-            <ButtonPrimary
-                aria-haspopup="dialog"
-                onPress={() =>
-                    confirm({
-                        message:
-                            'Are you sure you want to delete "rainy_day.jpg"? You cant undo this action.',
-                        title: 'Delete media?',
-                        destructive,
-                        onAccept: () => console.log('Accepted'),
-                        onCancel: () => console.log('Canceled'),
-                    })
-                }
-            >
-                Open
-            </ButtonPrimary>
-        </ButtonLayout>
+        <ButtonLayout
+            primaryButton={
+                <ButtonPrimary
+                    aria-haspopup="dialog"
+                    onPress={() =>
+                        confirm({
+                            message:
+                                'Are you sure you want to delete "rainy_day.jpg"? You cant undo this action.',
+                            title: 'Delete media?',
+                            destructive,
+                            onAccept: () => console.log('Accepted'),
+                            onCancel: () => console.log('Canceled'),
+                        })
+                    }
+                >
+                    Open
+                </ButtonPrimary>
+            }
+        />
     );
 };
 
 export const Dialog: StoryComponent = () => {
     const {dialog} = useDialog();
     return (
-        <ButtonLayout>
-            <ButtonPrimary
-                aria-haspopup="dialog"
-                onPress={() =>
-                    dialog({
-                        title: 'Title',
-                        subtitle: 'Subtitle',
-                        message: 'Message',
-                        acceptText: 'Accept terms and conditions',
-                        extra: (
-                            <Stack space={16}>
-                                <Text1 regular>Extra content</Text1>
-                                <Select
-                                    name="fruits"
-                                    value="orange"
-                                    label="Select"
-                                    options={[
-                                        {value: 'orange', text: 'Orange'},
-                                        {value: 'banana', text: 'Banana'},
-                                    ]}
-                                />
-                            </Stack>
-                        ),
-                        link: <ButtonLink href="https://google.com">Link</ButtonLink>,
-                        icon: <IconInformationUserLight color={skinVars.colors.brand} />,
-                        onAccept: () => console.log('Accepted'),
-                        onCancel: () => console.log('Canceled'),
-                    })
-                }
-            >
-                Open
-            </ButtonPrimary>
-        </ButtonLayout>
+        <ButtonLayout
+            primaryButton={
+                <ButtonPrimary
+                    aria-haspopup="dialog"
+                    onPress={() =>
+                        dialog({
+                            title: 'Title',
+                            subtitle: 'Subtitle',
+                            message: 'Message',
+                            acceptText: 'Accept terms and conditions',
+                            extra: (
+                                <Stack space={16}>
+                                    <Text1 regular>Extra content</Text1>
+                                    <Select
+                                        name="fruits"
+                                        value="orange"
+                                        label="Select"
+                                        options={[
+                                            {value: 'orange', text: 'Orange'},
+                                            {value: 'banana', text: 'Banana'},
+                                        ]}
+                                    />
+                                </Stack>
+                            ),
+                            link: <ButtonLink href="https://google.com">Link</ButtonLink>,
+                            icon: <IconInformationUserLight color={skinVars.colors.brand} />,
+                            onAccept: () => console.log('Accepted'),
+                            onCancel: () => console.log('Canceled'),
+                        })
+                    }
+                >
+                    Open
+                </ButtonPrimary>
+            }
+        />
     );
 };
 
