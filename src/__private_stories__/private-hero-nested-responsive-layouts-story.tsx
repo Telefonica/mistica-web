c95c9b7811b17bf9e24ea52ce5c6a507d4a896c6
marcoskolodny <marcoskolodny@gmail.com>
2024-04-29 11:43:17 +0200

    cleanup nested responsiveLayout hero private story

--- src/__private_stories__/private-hero-nested-responsive-layouts-story.tsx
+++ src/__private_stories__/private-hero-nested-responsive-layouts-story.tsx
@@ -1,39 +1,38 @@
 import * as React from 'react';
-import {ResponsiveLayout, Box, Slideshow, Hero, Image, ButtonPrimary, ButtonLink} from '..';
+import {ResponsiveLayout, Slideshow, Hero, Image, ButtonPrimary, ButtonLink} from '..';
 import usingVrImg from '../__stories__/images/using-vr.jpg';
 
 export default {
     title: 'Private/Hero with nested responsiveLayouts',
+    parameters: {fullScreen: true},
 };
 
 export const Default: StoryComponent = () => {
     return (
-        <Box paddingY={24}>
-            <ResponsiveLayout>
-                <Slideshow
-                    dataAttributes={{testid: 'slideshow'}}
-                    withBullets
-                    inverseBullets={false}
-                    items={Array.from({length: 3}, (_, idx) => (
-                        <Hero
-                            background="default"
-                            key={idx}
-                            media={<Image src={usingVrImg} aspectRatio="16:9" />}
-                            pretitle="Pretitle"
-                            title="Title"
-                            description="This is a long description with a long text to see how this works"
-                            desktopMediaPosition="right"
-                            button={
-                                <ButtonPrimary small onPress={() => {}}>
-                                    Action
-                                </ButtonPrimary>
-                            }
-                            buttonLink={<ButtonLink onPress={() => {}}>Link</ButtonLink>}
-                        />
-                    ))}
-                />
-            </ResponsiveLayout>
-        </Box>
+        <ResponsiveLayout>
+            <Slideshow
+                dataAttributes={{testid: 'slideshow'}}
+                withBullets
+                inverseBullets={false}
+                items={Array.from({length: 3}, (_, idx) => (
+                    <Hero
+                        background="default"
+                        key={idx}
+                        media={<Image src={usingVrImg} aspectRatio="16:9" />}
+                        pretitle="Pretitle"
+                        title="Title"
+                        description="This is a long description with a long text to see how this works"
+                        desktopMediaPosition="right"
+                        button={
+                            <ButtonPrimary small onPress={() => {}}>
+                                Action
+                            </ButtonPrimary>
+                        }
+                        buttonLink={<ButtonLink onPress={() => {}}>Link</ButtonLink>}
+                    />
+                ))}
+            />
+        </ResponsiveLayout>
     );
 };
 
