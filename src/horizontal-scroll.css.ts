7fad661f06350767a095e5580f0a229e7cc318e5
marcoskolodny <marcoskolodny@gmail.com>
2024-04-29 14:29:42 +0200

    restore horizontalScroll logic

--- src/horizontal-scroll.css.ts
+++ src/horizontal-scroll.css.ts
@@ -1,6 +1,16 @@
-import {globalStyle, style} from '@vanilla-extract/css';
+import {fallbackVar, globalStyle, style} from '@vanilla-extract/css';
 import * as mq from './media-queries.css';
+import {vars as responsiveLayoutVars} from './responsive-layout.css';
 import {sprinkles} from './sprinkles.css';
+
+const responsiveLayoutSideMargin = fallbackVar(responsiveLayoutVars.sideMargin, '0px');
+
+const sideMarginSpacer = {
+    content: '""',
+    display: 'block',
+    flexShrink: 0,
+    width: responsiveLayoutSideMargin,
+};
 
 const hideScrollbar = {
     scrollbarWidth: 'none', // Firefox
@@ -16,6 +26,9 @@ export const scroll = style([
     {
         overflowX: 'auto',
         overflowY: 'hidden',
+        margin: `0 calc(${responsiveLayoutSideMargin} * -1)`,
+        ':after': sideMarginSpacer,
+        ':before': sideMarginSpacer,
 
         '@media': {
             [mq.touchableOnly]: hideScrollbar,
