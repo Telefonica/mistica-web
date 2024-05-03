ff4509d6e320bbdc5e8c70e2fd6c5ea5c829ddbb
marcoskolodny <marcoskolodny@gmail.com>
2024-05-03 13:57:11 +0200

    fix test name

--- src/__screenshot_tests__/private-image-srcset-screenshot-test.tsx
+++ src/__screenshot_tests__/private-image-srcset-screenshot-test.tsx
@@ -2,7 +2,7 @@ import {openStoryPage, screen} from '../test-utils';
 
 const DEVICES = ['MOBILE_IOS', 'DESKTOP', 'LARGE_DESKTOP'] as const;
 
-test.each(DEVICES)('Hero with nested responsive layouts (%s)', async (device) => {
+test.each(DEVICES)('Image with srcSet is responsive (%s)', async (device) => {
     await openStoryPage({
         id: 'private-image-with-srcset-attribute-is-responsive--default',
         device,
