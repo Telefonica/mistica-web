fa63884a4aeb2b89e0f4504bae7607cfe1edeb98
Marcos Kolodny <marcoskolodny@gmail.com>
2024-05-06 16:50:20 +0200

    feat(Image): add srcSet (#1109)
    Issue: [Link](https://jira.tid.es/browse/WEB-1842)
    Closes: https://github.com/Telefonica/mistica-web/issues/1085

---
+++ src/__private_stories__/image-srcset-story.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {Image, Stack, Text3} from '..';
+import usingVrImg from '../__stories__/images/using-vr.jpg';
+import beachImg from '../__stories__/images/beach.jpg';
+import laptopImg from '../__stories__/images/laptop.jpg';
+
+export default {
+    title: 'Private/Image with srcSet attribute is responsive',
+};
+
+export const Default: StoryComponent = () => {
+    return (
+        <Stack space={16}>
+            <Text3 medium>When using srcSet prop, Image src should be responsive</Text3>
+
+            <Image
+                src={usingVrImg}
+                aspectRatio="16:9"
+                width={400}
+                dataAttributes={{testid: 'image'}}
+                srcSet={`${usingVrImg} 600w, ${beachImg} 1300w, ${laptopImg} 3000w`}
+            />
+        </Stack>
+    );
+};
+
+Default.storyName = 'Image with srcSet attribute is responsive';
