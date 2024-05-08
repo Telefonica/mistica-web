Telefonica/mistica-web

commit 78e0e805de8c6fb97c17fc82b5f4af0a0a5d5697

fix(AdvancedDataCard): footer extra space (#1102)
Fix advanced data card extra space at bottom

Original:
<img width="439" alt="original"
src="https://github.com/Telefonica/mistica-web/assets/90707957/b19e4e76-a7da-43f9-8b7d-0f54d007b824">

Fixed:
<img width="439" alt="fixed"
src="https://github.com/Telefonica/mistica-web/assets/90707957/db24d53f-205f-4e65-8287-058d7995d2e0">

---------

Co-authored-by: Bruno Cezar <bruno.guimaraes.ext@telefonica.com>

src/community/advanced-data-card.css.ts
@@ -115,6 +115,8 @@ export const zindex = style({zIndex: -1});
 
 export const minHeight = style({minHeight: 216});
 
+export const minHeightBox = style({minHeight: 110}); // This minHeight is taking into account that 216px - 106px which is the combined height of the padding, border and footer
+
 export const dataCard = style([
     {
         isolation: 'isolate', // This avoids problems with some screenshot tests in webapp

src/community/advanced-data-card.tsx
@@ -315,7 +315,10 @@ export const AdvancedDataCard = React.forwardRef<HTMLDivElement, AdvancedDataCar
                                 !hasFooter && !hasExtras ? styles.minHeight : ''
                             )}
                         >
-                            <Box paddingTop={8}>
+                            <Box
+                                paddingTop={8}
+                                className={!hasFooter && !hasExtras ? '' : styles.minHeightBox}
+                            >
                                 <Inline space={0}>
                                     <Stack space={8}>
                                         {stackingGroup}

commit 3e561cdba20a0f12649dd7b86e693e38be1f39b8

fix(AdvancedDataCard): minHeight issues caused by #1102

src/community/advanced-data-card.css.ts
@@ -25,7 +25,6 @@ export const actions = style([
     paddingX,
     sprinkles({
         display: 'flex',
-        flex: 1,
         justifyContent: 'space-between',
         paddingBottom: 24,
     }),
@@ -49,6 +48,7 @@ export const touchableArea = style({
 
 export const touchableContainer = style({
     display: 'flex',
+    flex: 1,
     position: 'relative',
     flexDirection: 'column',
     width: '100%',
@@ -114,8 +114,6 @@ export const cardContentStyle = style([
 export const zindex = style({zIndex: -1});
 
 export const minHeight = style({minHeight: 216});
-
-export const minHeightBox = style({minHeight: 110}); // This minHeight is taking into account that 216px - 106px which is the combined height of the padding, border and footer
 
 export const dataCard = style([
     {

src/community/advanced-data-card.tsx
@@ -315,10 +315,7 @@ export const AdvancedDataCard = React.forwardRef<HTMLDivElement, AdvancedDataCar
                                 !hasFooter && !hasExtras ? styles.minHeight : ''
                             )}
                         >
-                            <Box
-                                paddingTop={8}
-                                className={!hasFooter && !hasExtras ? '' : styles.minHeightBox}
-                            >
+                            <Box paddingTop={8}>
                                 <Inline space={0}>
                                     <Stack space={8}>
                                         {stackingGroup}