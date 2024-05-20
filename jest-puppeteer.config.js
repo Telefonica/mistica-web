86cff87b8fc88dae2bd60413de04a33913188df0
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-05-20 18:32:37 +0200

    WEB-1614 update config override

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -4,14 +4,25 @@ const getConfig = async () => {
     const config = await getInitialConfig();
 
     if (process.env.CI) {
-        config.launch.executablePath = undefined;
-        config.launch.args = [];
-        config.connect = {
-            browserUrl: 'http://localhost:9222',
-            ignoreHTTPSErrors: true,
-            headless: true,
-            slowMo: 0,
-        };
+        config.launch.args = [
+            // https://peter.sh/experiments/chromium-command-line-switches/
+            '--no-sandbox', // probably not needed in ubuntu
+            '--font-render-hinting=none', // this flag is important because we use it when launching dockerized chromium too
+            '--disable-background-networking',
+            '--disable-background-timer-throttling',
+            '--disable-breakpad',
+            '--disable-client-side-phishing-detection',
+            '--disable-default-apps',
+            '--disable-dev-shm-usage',
+            '--disable-extensions',
+            '--disable-features=site-per-process',
+            '--disable-hang-monitor',
+            '--disable-popup-blocking',
+            '--disable-prompt-on-repost',
+            '--disable-sync',
+            '--disable-translate',
+            '--disable-smooth-scrolling',
+        ];
     }
     console.log(JSON.stringify(config, null, 2));
     return config;
