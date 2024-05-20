5f094c68ea687ade01f847f69da855798952d226
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-05-20 17:10:35 +0200

    WEB-1614 update config override

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,16 +1,20 @@
-const config = require('@telefonica/acceptance-testing/jest-puppeteer-config');
+const getInitialConfig = require('@telefonica/acceptance-testing/jest-puppeteer-config');
 
-if (process.env.CI) {
-    config.launch.executablePath = undefined;
-    config.launch.args = [];
-    config.connect = {
-        browserUrl: 'http://localhost:9222',
-        ignoreHTTPSErrors: true,
-        headless: true,
-        slowMo: 0,
-    };
-}
+const getConfig = async () => {
+    const config = await getInitialConfig();
 
-console.log(JSON.stringify(config, null, 2));
+    if (process.env.CI) {
+        config.launch.executablePath = undefined;
+        config.launch.args = [];
+        config.connect = {
+            browserUrl: 'http://localhost:9222',
+            ignoreHTTPSErrors: true,
+            headless: true,
+            slowMo: 0,
+        };
+    }
+    console.log(JSON.stringify(config, null, 2));
+    return config;
+};
 
-module.exports = config;
+module.exports = getConfig;
