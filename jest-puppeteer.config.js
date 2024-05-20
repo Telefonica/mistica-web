16a53abda2c51e94630bcc9bfee8019465be7816
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-05-20 16:58:00 +0200

    WEB-1614 update config override

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,12 @@ const config = require('@telefonica/acceptance-testing/jest-puppeteer-config');
 if (process.env.CI) {
     config.launch.executablePath = undefined;
     config.launch.args = [];
-    config.connect = 'http://localhost:9222';
+    config.connect = {
+        browserUrl: 'http://localhost:9222',
+        ignoreHTTPSErrors: true,
+        headless: true,
+        slowMo: 0,
+    };
 }
 
 console.log(JSON.stringify(config, null, 2));
