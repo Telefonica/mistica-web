1ebbef561828104db9e22da490661ccbfa57f717
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-05-20 16:51:09 +0200

    WEB-1614 update jest pptr config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,2 +1,11 @@
 const config = require('@telefonica/acceptance-testing/jest-puppeteer-config');
+
+if (process.env.CI) {
+    config.launch.executablePath = undefined;
+    config.launch.args = [];
+    config.connect = 'http://localhost:9222';
+}
+
+console.log(JSON.stringify(config, null, 2));
+
 module.exports = config;
