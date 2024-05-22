813a615ccd24bac57ec54ecf803451cd034cd75b
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-05-22 17:54:15 +0200

    WEB-1614 update dependencies

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,18 +1,1 @@
-const configPromise = require('@telefonica/acceptance-testing/jest-puppeteer-config');
-
-module.exports = (async () => {
-    const config = await configPromise;
-
-    config.launch.env = {
-        // ...process.env,
-        // TZ: 'UTC',
-        // LANG: 'es_ES',
-        // LANGUAGE: 'es_ES',
-        // DBUS_SESSION_BUS_ADDRESS: 'autolaunch:',
-        // DISPLAY: ':0', // https://github.com/puppeteer/puppeteer/issues/8148
-    };
-    // config.launch.dumpio = true;
-    // config.launch.headless = 'new';
-    console.log(config);
-    return config;
-})();
+module.exports = require('@telefonica/acceptance-testing/jest-puppeteer-config');
