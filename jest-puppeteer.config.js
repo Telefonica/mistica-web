f6c79d0e3fd3e717a2901e0b693e1504d8c2cee7
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-05-22 11:38:59 +0200

    WEB-1614 update acceptance-testing

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -5,46 +5,14 @@ module.exports = (async () => {
 
     config.launch.env = {
         // ...process.env,
-        TZ: 'UTC',
-        LANG: 'es_ES',
-        LANGUAGE: 'es_ES',
+        // TZ: 'UTC',
+        // LANG: 'es_ES',
+        // LANGUAGE: 'es_ES',
         // DBUS_SESSION_BUS_ADDRESS: 'autolaunch:',
         // DISPLAY: ':0', // https://github.com/puppeteer/puppeteer/issues/8148
     };
-    config.launch.dumpio = true;
-    config.launch.args = [
-        '--lang=es_ES',
-        '--disable-background-networking',
-        '--disable-background-timer-throttling',
-        '--disable-breakpad',
-        '--disable-client-side-phishing-detection',
-        '--disable-default-apps',
-        '--disable-dev-shm-usage',
-        '--disable-extensions',
-        '--disable-features=site-per-process',
-        '--disable-hang-monitor',
-        '--disable-popup-blocking',
-        '--disable-prompt-on-repost',
-        '--disable-sync',
-        '--disable-translate',
-        '--disable-gpu',
-        '--metrics-recording-only',
-        '--no-first-run',
-        '--safebrowsing-disable-auto-update',
-        '--enable-automation',
-        '--password-store=basic',
-        '--use-mock-keychain',
-        '--headless=new',
-        '--hide-scrollbars',
-        '--no-sandbox',
-        '--disabled-setupid-sandbox',
-        '--font-render-hinting=none',
-        '--disable-font-subpixel-positioning',
-        // '--remote-debugging-port=9222', // https://github.com/puppeteer/puppeteer/issues/8546
-        // '--remote-debugging-address=0.0.0.0',
-        '--ignore-certificate-errors',
-    ];
-    config.launch.headless = 'new';
+    // config.launch.dumpio = true;
+    // config.launch.headless = 'new';
     console.log(config);
     return config;
 })();
