90d5f7159c28938dc566c8035c06d778da0d33da
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-06-03 12:13:51 +0200

    WEB-1893 update screenshots

--- src/__screenshot_tests__/circle-screenshot-test.tsx
+++ src/__screenshot_tests__/circle-screenshot-test.tsx
@@ -2,10 +2,11 @@ import {openStoryPage, screen} from '../test-utils';
 
 const CASES = [
     {size: 100, background: 'color'},
-    {size: 100, background: 'color', icon: true},
-    {size: 100, background: 'color', icon: true, border: true},
+    {size: 40, background: 'color', icon: true},
+    {size: 40, background: 'color', icon: true, border: true},
     {size: 100, background: 'image'},
-    {size: 100, background: 'custom', icon: true},
+    {size: 100, background: 'custom'},
+    {size: 40, background: 'custom', icon: true},
 ] as const;
 
 test.each(CASES)('Circle - %s', async (args) => {
