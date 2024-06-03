dbaa378f767db901967fd87dad43c7fc06de8a6b
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-06-03 13:17:19 +0200

    WEB-1893 add boxSizing style

--- src/circle.css.ts
+++ src/circle.css.ts
@@ -1,8 +1,14 @@
+import {style} from '@vanilla-extract/css';
 import {sprinkles} from './sprinkles.css';
 
-export const circle = sprinkles({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: '50%',
-});
+export const circle = style([
+    {
+        boxSizing: 'border-box',
+    },
+    sprinkles({
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '50%',
+    }),
+]);
