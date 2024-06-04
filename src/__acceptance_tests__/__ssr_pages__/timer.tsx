92cb93119e27220b25a9f9f0e79a05fadbcf1e9e
marcoskolodny <marcoskolodny@gmail.com>
2024-06-04 17:03:20 +0200

    fix ssr test

--- src/__acceptance_tests__/__ssr_pages__/timer.tsx
+++ src/__acceptance_tests__/__ssr_pages__/timer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {Timer} from '../../..';
 
-const TimerTest = (): JSX.Element => <Timer endTimestamp={0} />;
+const TimerTest = (): JSX.Element => <Timer minTimeUnit="seconds" maxTimeUnit="hours" endTimestamp={0} />;
 
 export default TimerTest;
