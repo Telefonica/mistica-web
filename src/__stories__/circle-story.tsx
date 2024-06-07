bedeaa4dd567aa8dc844ffb364629698dce3c229
Pedro Ladaria <pladaria@tuenti.com>
2024-06-07 09:56:12 +0200

    feat(Circle): custom background support (#1136)
    Co-authored-by: Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>

--- src/__stories__/circle-story.tsx
+++ src/__stories__/circle-story.tsx
@@ -8,35 +8,43 @@ export default {
         size: {
             control: {type: 'range', min: 40, max: 128, step: 4},
         },
-        content: {
-            options: ['color', 'image', 'icon', 'none'],
+        background: {
+            options: ['color', 'image', 'custom'],
             control: {type: 'select'},
         },
     },
 };
 
 type Args = {
-    content: string;
+    background: string;
     size: number;
     border: boolean;
+    children: boolean;
 };
 
-export const Default: StoryComponent<Args> = ({content, size, border}) => {
+export const Default: StoryComponent<Args> = ({size, border, background, children}) => {
+    if (background === 'custom') {
+        return (
+            <Circle
+                size={size}
+                dataAttributes={{testid: 'circle'}}
+                background="linear-gradient(yellow, red)"
+                border={border}
+            >
+                {!!children && <IconShopRegular color={skinVars.colors.inverse} />}
+            </Circle>
+        );
+    }
+
     return (
         <Circle
             size={size}
             dataAttributes={{testid: 'circle'}}
-            backgroundColor={
-                content === 'color'
-                    ? skinVars.colors.brand
-                    : content === 'icon'
-                    ? skinVars.colors.brandLow
-                    : skinVars.colors.background
-            }
-            backgroundImage={content === 'image' ? avatarImg : undefined}
+            backgroundColor={background === 'color' ? skinVars.colors.brand : skinVars.colors.background}
+            backgroundImage={background === 'image' ? avatarImg : undefined}
             border={border}
         >
-            {content === 'icon' ? <IconShopRegular color={skinVars.colors.brand} /> : undefined}
+            {!!children && <IconShopRegular color={skinVars.colors.inverse} />}
         </Circle>
     );
 };
@@ -44,6 +52,7 @@ export const Default: StoryComponent<Args> = ({content, size, border}) => {
 Default.storyName = 'Circle';
 Default.args = {
     size: 40,
-    content: 'color',
-    border: true,
+    background: 'color',
+    border: false,
+    children: false,
 };
--- src/circle.tsx
+++ src/circle.tsx
@@ -4,26 +4,32 @@ import {vars} from './skins/skin-contract.css';
 import {getPrefixedDataAttributes} from './utils/dom';
 
 import type {DataAttributes} from './utils/types';
+import type {ExclusifyUnion} from './utils/utility-types';
 
-type Props = {
-    backgroundColor?: string;
-    backgroundImage?: string;
+type CommonProps = {
     size?: number | string;
-    children?: React.ReactElement;
+    children?: React.ReactNode;
     border?: boolean | string;
     dataAttributes?: DataAttributes;
 };
 
+type BackgroundProps = {background: string};
+type BackgroundShorthandProps = {backgroundColor?: string; backgroundImage?: string};
+
+type Props = CommonProps & ExclusifyUnion<BackgroundProps | BackgroundShorthandProps>;
+
 const Circle: React.FC<Props> = ({
     children,
+    background,
     backgroundColor,
     backgroundImage,
     size,
-    border = false,
+    border,
     dataAttributes,
 }) => {
     const borderColor = border === true ? vars.colors.border : border;
     const borderProp = border ? `1px solid ${borderColor}` : undefined;
+    const imageStyle = backgroundImage ? `url(${backgroundImage})` : '';
 
     return (
         <div
@@ -31,8 +37,9 @@ const Circle: React.FC<Props> = ({
             style={{
                 width: size,
                 height: size,
-                backgroundColor,
-                backgroundImage: backgroundImage ? `url(${backgroundImage})` : 'none',
+                background: background
+                    ? background
+                    : `center / cover no-repeat ${imageStyle} ${backgroundColor || ''}`,
                 border: borderProp,
             }}
             {...getPrefixedDataAttributes(dataAttributes, 'Circle')}
