d73c21955c2c1ec98ec6af0956d27fdddba127f3
Marcos Kolodny <marcoskolodny@gmail.com>
2024-06-07 08:10:11 -0300

    feat(EmptyState): allow using only ButtonLink as action (#1140)

--- src/__screenshot_tests__/empty-state-screenshot-test.tsx
+++ src/__screenshot_tests__/empty-state-screenshot-test.tsx
@@ -4,11 +4,14 @@ import type {Device} from '../test-utils';
 
 const devices: Array<Device> = ['MOBILE_IOS', 'TABLET', 'DESKTOP'];
 const assets = ['Icon', 'Image', 'img'];
+const actions = ['button and link', 'button', 'link', 'none'];
 const createCases = () => {
     const cases = [];
     for (const device of devices) {
         for (const asset of assets) {
-            cases.push([device, asset]);
+            for (const action of actions) {
+                cases.push([device, asset, action]);
+            }
         }
     }
     return cases;
@@ -26,18 +29,21 @@ test.each(devices)('EmptyState in %s with image', async (device) => {
     expect(image).toMatchImageSnapshot();
 });
 
-test.each(createCases())('EmptyState in %s with %s asset', async (device, asset) => {
-    await openStoryPage({
-        id: 'patterns-empty-states-emptystate--with-icon',
-        device: device as Device,
-        args: {asset},
-    });
+test.each(createCases())(
+    'EmptyState in %s with %s asset and actions = %s',
+    async (device, asset, actions) => {
+        await openStoryPage({
+            id: 'patterns-empty-states-emptystate--with-icon',
+            device: device as Device,
+            args: {asset, actions},
+        });
 
-    const emptyState = await screen.findByTestId('empty-state-with-icon');
-    const image = await emptyState.screenshot();
+        const emptyState = await screen.findByTestId('empty-state-with-icon');
+        const image = await emptyState.screenshot();
 
-    expect(image).toMatchImageSnapshot();
-});
+        expect(image).toMatchImageSnapshot();
+    }
+);
 
 test.each(devices)('EmptyState in %s with small image', async (device) => {
     await openStoryPage({
