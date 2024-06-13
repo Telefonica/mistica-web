b106bd1bfcf96840c351b9629cdff72692c2d7f1
Bruno Cezar <bruno.guimaraes.ext@telefonica.com>
2024-06-13 10:06:04 -0300

    fix: remove divide and add 32px spacing in slots

--- src/community/advanced-data-card.tsx
+++ src/community/advanced-data-card.tsx
@@ -250,7 +250,7 @@ type AdvancedDataCardProps = MaybeTouchableCard<{
     description?: string;
     descriptionLinesMax?: number;
     extra?: ReadonlyArray<RendersNullableElement<AllowedExtra>>;
-    extraDividerPadding?: 8 | 24;
+    extraDividerPadding?: 8 | 16 | 24;
     button?: RendersNullableElement<typeof ButtonPrimary>;
     footerImage?: RendersNullableElement<typeof Image>;
     footerText?: string;
@@ -279,7 +279,7 @@ export const AdvancedDataCard = React.forwardRef<HTMLDivElement, AdvancedDataCar
             descriptionLinesMax,
 
             extra,
-            extraDividerPadding = 24,
+            extraDividerPadding = 16,
 
             button,
             footerImage,
@@ -365,9 +365,7 @@ export const AdvancedDataCard = React.forwardRef<HTMLDivElement, AdvancedDataCar
                                             <div className={styles.paddingX}>{item}</div>
 
                                             {index + 1 !== extra.length && (
-                                                <Box paddingY={extraDividerPadding}>
-                                                    <Divider />
-                                                </Box>
+                                                <Box paddingY={extraDividerPadding} />
                                             )}
                                         </div>
                                     );
