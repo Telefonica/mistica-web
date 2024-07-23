938b2759f5dcb7ba470253a5183a0a9f884899ba
Marcos Kolodny <marcoskolodny@gmail.com>
2024-07-23 16:59:08 +0200

    feat(Table): allow setting any column as row header (#1175)

--- src/__screenshot_tests__/table-screenshot-test.tsx
+++ src/__screenshot_tests__/table-screenshot-test.tsx
@@ -122,6 +122,14 @@ const cases = [
         },
     ],
     [
+        'desktop collapse-rows with rowHeaderIndex = 2',
+        {
+            device: 'DESKTOP',
+            args: {numItems: 6, responsive: 'collapse-rows', rowHeaderIndex: 2},
+            fullPageScreenshot: true,
+        },
+    ],
+    [
         'desktop boxed with actions',
         {
             device: 'DESKTOP',
@@ -226,6 +234,22 @@ const cases = [
         },
     ],
     [
+        'tablet collapse-rows with rowHeaderIndex = 0',
+        {
+            device: 'TABLET',
+            args: {numItems: 6, responsive: 'collapse-rows', withRowHeader: true, rowHeaderIndex: 0},
+            fullPageScreenshot: true,
+        },
+    ],
+    [
+        'tablet collapse-rows with rowHeaderIndex = 2',
+        {
+            device: 'TABLET',
+            args: {numItems: 6, responsive: 'collapse-rows', withRowHeader: true, rowHeaderIndex: 2},
+            fullPageScreenshot: true,
+        },
+    ],
+    [
         'tablet boxed with actions',
         {
             device: 'TABLET',
@@ -246,6 +270,20 @@ const cases = [
         {
             device: 'TABLET',
             args: {numItems: 6, boxed: true, responsive: 'collapse-rows', hideHeaders: true},
+            fullPageScreenshot: true,
+        },
+    ],
+    [
+        'tablet collapse-rows boxed with rowHeaderIndex = 0',
+        {
+            device: 'TABLET',
+            args: {
+                numItems: 6,
+                boxed: true,
+                responsive: 'collapse-rows',
+                withRowHeader: true,
+                rowHeaderIndex: 0,
+            },
             fullPageScreenshot: true,
         },
     ],
