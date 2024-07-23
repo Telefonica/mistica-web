bdd48832198289680f41517005945363242c6429
marcoskolodny <marcoskolodny@gmail.com>
2024-07-23 17:33:13 +0200

    add screenshots in mobile

--- src/__screenshot_tests__/private-text-preset-vars-screenshot-test.tsx
+++ src/__screenshot_tests__/private-text-preset-vars-screenshot-test.tsx
@@ -1,11 +1,23 @@
 import {openStoryPage, screen} from '../test-utils';
 
 const SKINS = ['Movistar', 'Vivo-new', 'O2-new'] as const;
+const DEVICES = ['MOBILE_IOS', 'DESKTOP'] as const;
 
-test.each(SKINS)('TextPresets as CSS vars in %s', async (skin) => {
+const getCases = () => {
+    const cases = [];
+    for (const skin of SKINS) {
+        for (const device of DEVICES) {
+            cases.push([skin, device]);
+        }
+    }
+    return cases;
+};
+
+test.each(getCases())('TextPresets as CSS vars in %s (%s)', async (skin, device) => {
     await openStoryPage({
         id: 'private-textpresets-css-vars--default',
-        skin,
+        skin: skin as (typeof SKINS)[number],
+        device: device as (typeof DEVICES)[number],
     });
 
     const story = await screen.findByTestId('story');
