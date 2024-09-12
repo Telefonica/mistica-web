2ef5804b11908d2f3a8b5f98ceba3b391d7e3418
Marcos Kolodny <marcoskolodny@gmail.com>
2024-09-12 18:52:41 +0200

    feat(Snackbar): update snackbar duration logic (#1208)
    BREAKING CHANGE: snackbar duration prop type has changed

--- src/snackbar.tsx
+++ src/snackbar.tsx
@@ -27,7 +27,7 @@ export type Props = {
     buttonText?: string;
     buttonAccessibilityLabel?: string;
     closeButtonLabel?: string;
-    duration?: number;
+    duration?: 'PERSISTENT';
     message: string;
     onClose?: SnackbarCloseHandler;
     type?: SnackbarType;
@@ -62,7 +62,8 @@ const SnackbarComponent = React.forwardRef<ImperativeHandle, Props>(
         const longButtonWidth = isDesktopOrBigger ? 160 : 128;
         const hasLongButton = buttonWidth > longButtonWidth;
         const elementRef = React.useRef<HTMLDivElement>(null);
-        const shouldShowDismissButton = (duration === Infinity && !buttonText) || withDismiss;
+        const shouldShowDismissButton = (duration === 'PERSISTENT' && !buttonText) || withDismiss;
+        const defaultDuration = buttonText ? DEFAULT_DURATION_WITH_BUTTON : DEFAULT_DURATION_WITHOUT_BUTTON;
 
         const onCloseRef = React.useRef(onClose);
         React.useEffect(() => {
@@ -89,13 +90,15 @@ const SnackbarComponent = React.forwardRef<ImperativeHandle, Props>(
             }, 50);
 
             const closeTimeout =
-                duration !== Infinity ? setTimeout(() => close({action: 'TIMEOUT'}), duration) : undefined;
+                duration !== 'PERSISTENT'
+                    ? setTimeout(() => close({action: 'TIMEOUT'}), defaultDuration)
+                    : undefined;
 
             return () => {
                 clearTimeout(openTimeout);
                 clearTimeout(closeTimeout);
             };
-        }, [close, duration]);
+        }, [close, duration, defaultDuration]);
 
         return (
             <Portal className={styles.snackbarContainer}>
@@ -203,8 +206,6 @@ const Snackbar = React.forwardRef<ImperativeHandle & HTMLDivElement, Props>(
         },
         ref
     ) => {
-        const defaultDuration = buttonText ? DEFAULT_DURATION_WITH_BUTTON : DEFAULT_DURATION_WITHOUT_BUTTON;
-        duration = Math.max(duration ?? defaultDuration, defaultDuration);
         const renderNative = isWebViewBridgeAvailable();
         const onCloseRef = React.useRef(onCloseProp);
         const isOpenRef = React.useRef(false);
@@ -219,7 +220,7 @@ const Snackbar = React.forwardRef<ImperativeHandle & HTMLDivElement, Props>(
                 isOpenRef.current = true;
                 nativeMessage({
                     message,
-                    duration: duration === Infinity ? 'PERSISTENT' : undefined,
+                    duration,
                     buttonText,
                     buttonAccessibilityLabel,
                     type,
