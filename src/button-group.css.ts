bbb799c4d6ae768a58a4c1b9a45b22b8677d780b
Marcos Kolodny <marcoskolodny@gmail.com>
2024-09-12 14:45:20 +0200

    Breaking link 3 (#1239)

--- src/button-group.css.ts
+++ src/button-group.css.ts
@@ -1,11 +1,13 @@
-import {createVar, style} from '@vanilla-extract/css';
+import {style} from '@vanilla-extract/css';
 import {sprinkles} from './sprinkles.css';
 import * as mq from './media-queries.css';
+import * as buttonStyles from './button.css';
 
 const buttonLayoutSpacing = '16px';
-const buttonLinkPadding = createVar();
-
-export const vars = {buttonLinkPadding};
+const buttonLinkPadding = {
+    default: `calc(${buttonStyles.buttonPaddingX.default} + ${buttonStyles.borderSize})`,
+    small: `calc(${buttonStyles.buttonPaddingX.small} + ${buttonStyles.borderSize})`,
+};
 
 export const inline = style([
     sprinkles({display: 'inline-flex', alignItems: 'center', flexDirection: 'row'}),
@@ -40,7 +42,31 @@ export const centerInDesktop = style({
 
 export const container = style({
     marginTop: `-${buttonLayoutSpacing}`,
-    marginLeft: `calc(-1 * ${buttonLayoutSpacing})`,
+    marginLeft: `calc(-1 * (${buttonLayoutSpacing} + ${buttonLinkPadding.default}))`,
+
+    selectors: {
+        [`&:has(${buttonStyles.smallLink})`]: {
+            marginLeft: `calc(-1 * (${buttonLayoutSpacing} + ${buttonLinkPadding.small}))`,
+        },
+    },
+});
+
+export const containerWithSmallLink = style({
+    marginTop: `-${buttonLayoutSpacing}`,
+    marginLeft: `calc(-1 * (${buttonLayoutSpacing} + ${buttonLinkPadding.small}))`,
+});
+
+export const buttons = style({
+    marginLeft: buttonLinkPadding.default,
+    selectors: {
+        [`${container}:has(${buttonStyles.smallLink}) &`]: {
+            marginLeft: buttonLinkPadding.small,
+        },
+    },
+});
+
+export const buttonsWithSmallLink = style({
+    marginLeft: buttonLinkPadding.small,
 });
 
 export const buttonChild = style({
