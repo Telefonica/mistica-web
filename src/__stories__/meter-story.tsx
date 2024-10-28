6e938c192f79f231acd290efe21d6dd496415a77
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2024-10-28 11:31:54 +0100

    WEB-2079 update story

--- src/__stories__/meter-story.tsx
+++ src/__stories__/meter-story.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Meter} from '..';
+import {Meter, Stack} from '..';
 
 export default {
     title: 'Components/Meters',
@@ -15,13 +15,31 @@ type ProgressBarStoryArgs = {foo: 'bar'};
 
 export const ProgressBarStory: StoryComponent<ProgressBarStoryArgs> = () => {
     const [key, setKey] = React.useState(0);
+    const [values, setValues] = React.useState(Array.from({length: 8}, () => 0.1));
     return (
-        <>
-            <Meter value={0.25} key={key} />
-            <div>
-                <button onClick={() => setKey(Date.now())}>Reset</button>
+        <div
+            style={{
+                padding: 16,
+            }}
+        >
+            <div key={key}>
+                <Meter values={values} />
             </div>
-        </>
+            <Stack space={16}>
+                <input
+                    type="range"
+                    style={{width: 400}}
+                    value={values[0] * 1000 * 8}
+                    max={1000}
+                    onChange={(e) => {
+                        const v = +e.currentTarget.value / 1000 / 8;
+                        setValues(Array.from({length: 8}, () => v));
+                    }}
+                />
+                <div>{values[0]}</div>
+                <button onClick={() => setKey(Date.now())}>Reset</button>
+            </Stack>
+        </div>
     );
 };
 
--- src/meter.tsx
+++ src/meter.tsx
@@ -1,6 +1,7 @@
 // https://www.figma.com/design/jWWCJ9kYl6I5uHLz3GcgRp/%F0%9F%94%B6-%5BREADY%5D-Data-Visualizations-Specs
 'use client';
 import * as React from 'react';
+import {vars} from './skins/skin-contract.css';
 // @ts-expect-error - no types
 import bezier from 'cubic-bezier';
 
@@ -8,12 +9,20 @@ const VIEW_BOX_WIDTH = 100;
 const CENTER_X = VIEW_BOX_WIDTH / 2;
 const CENTER_Y = VIEW_BOX_WIDTH / 2;
 
-const STROKE_WIDTH_PX = 26;
-const SEPARATION_PX = 6;
+const STROKE_WIDTH_PX = 6;
+const SEPARATION_PX = 2;
 
 const ANIMATION_DELAY_MS = 200;
 const ANIMATION_DURATION_MS = 1000;
 const ANIMATION_EPSILON = 1000 / 60 / ANIMATION_DURATION_MS / 4;
+
+const DEFAULT_COLORS = [
+    vars.colors.success,
+    vars.colors.error,
+    vars.colors.warning,
+    vars.colors.promo,
+    vars.colors.highlight,
+];
 
 type Segment = {
     a1: number; // start angle
@@ -38,38 +47,20 @@ const clamp = (n: number, min: number, max: number) => Math.min(Math.max(n, min)
  */
 const calculateArcSegments = (startValues: Array<number>, endValues: Array<number>, time: number) => {
     const segments: Array<Segment> = [];
-
     for (let i = 0; i < startValues.length; i++) {
-        // accumulate the values
-        const desiredValue = endValues.slice(0, i + 1).reduce((acc, v) => acc + v, 0);
+        const startValue = startValues.slice(0, i + 1).reduce((acc, v) => acc + v, 0);
+        const endValue = endValues.slice(0, i + 1).reduce((acc, v) => acc + v, 0);
 
         // each segment has an accumulated delay time. The last segment has no delay
         const delay = ANIMATION_DELAY_MS * (startValues.length - i - 1);
         const animationTime = clamp((time - delay) / ANIMATION_DURATION_MS, 0, 1);
 
-        const isFirst = i === 0;
-
-        const position = clamp(timingFunction(animationTime), 0, 1);
-
-        const a1 = isFirst ? 0 : segments.at(-1)?.a2 || 0;
-        const a2 = (desiredValue - startValues[i]) * position * Math.PI;
-
+        const t = clamp(timingFunction(animationTime), 0, 1);
+        const a1 = segments.at(-1)?.a2 || 0;
+        const a2 = (startValue + (endValue - startValue) * t) * Math.PI;
         segments.push({a1, a2});
-
-        if (isFirst) {
-            // console.log({v0: startValues[i], v1: endValues[i], value, position, animationTime, delay});
-        }
     }
-
     return segments;
-};
-
-const Marker = ({id, color}: {id: string; color: string}): JSX.Element => {
-    return (
-        <marker id={id} viewBox="0 0 10 10" markerWidth={1} markerHeight={1} orient="auto" refX={5} refY={5}>
-            <circle cx={5} cy={5} r={5} fill={color} />
-        </marker>
-    );
 };
 
 /**
@@ -83,14 +74,15 @@ const createArcPath = ({
     x2,
     y2,
     radius,
+    clockwise = 1,
 }: {
     x1: number;
     y1: number;
     x2: number;
     y2: number;
     radius: number;
+    clockwise?: 0 | 1;
 }): string => {
-    const clockwise = 1;
     const xAxisRotation = 0;
     const largeArchFlag = 0;
     return `M ${x1} ${y1} A ${radius} ${radius} ${xAxisRotation} ${largeArchFlag} ${clockwise} ${x2} ${y2}`;
@@ -102,48 +94,42 @@ type MeterProps = {
      */
     values: Array<number>;
     width?: number;
+    colors?: Array<string>;
 };
 
-const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink', 'brown'];
-
-const Meter = ({width = 400}: MeterProps): JSX.Element => {
+const Meter = ({width = 400, colors = DEFAULT_COLORS, values}: MeterProps): JSX.Element => {
     const scaleFactor = VIEW_BOX_WIDTH / width;
     const strokeWidth = STROKE_WIDTH_PX * scaleFactor;
     const radius = CENTER_X - strokeWidth / 2;
     const separation = SEPARATION_PX * scaleFactor;
-
-    const [values, setValue] = React.useState<Array<number>>(Array.from({length: 8}, () => 0.1));
-
-    const [segments, setSegments] = React.useState<Array<Segment>>(
-        values.map(() => ({
-            a1: 0,
-            a2: 0,
-            x1: CENTER_X - radius,
-            y1: CENTER_Y,
-            x2: CENTER_X - radius,
-            y2: CENTER_Y,
-        }))
-    );
-
-    const initialValuesRef = React.useRef(Array.from({length: 8}, () => 0));
-
-    const arcLenght = Math.PI * radius;
     const separationAngle = separation / radius;
+
+    const initialValuesRef = React.useRef(Array.from({length: values.length}, () => 0));
+
+    const [segments, setSegments] = React.useState<Array<Segment>>(() => {
+        return values.map(() => ({a1: 0, a2: 0}));
+    });
+
+    const firstNonZeroIndex = segments.findIndex((s) => s.a1 !== s.a2);
+
+    const getColor = (index: number) => {
+        return colors[index % colors.length];
+    };
 
     React.useEffect(() => {
         let raf: number;
         const start = performance.now();
         const end = start + ANIMATION_DURATION_MS + ANIMATION_DELAY_MS * (values.length - 1);
         console.log('EFFECT-------------------', {start, end, duration: end - start});
+        console.log({initialValuesRef: initialValuesRef.current, values});
         const animate = () => {
             const now = performance.now();
             setSegments(calculateArcSegments(initialValuesRef.current, values, now - start));
             if (now < end) {
-                // request next frame
-                raf = requestAnimationFrame(animate);
+                raf = requestAnimationFrame(animate); // request next frame
             } else {
-                // animation is done, update the initial values
                 console.log('DONE');
+                setSegments(calculateArcSegments(initialValuesRef.current, values, end - start)); // set the final values
                 initialValuesRef.current = values;
             }
         };
@@ -154,56 +140,72 @@ const Meter = ({width = 400}: MeterProps): JSX.Element => {
     }, [radius, values]);
 
     return (
-        <div
-            style={{
-                padding: 8,
-                background: 'url(https://img.freepik.com/free-photo/fantasy-style-clouds_23-2151057636.jpg)',
-                backgroundSize: 'cover',
-            }}
+        <svg
+            viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_WIDTH}`}
+            width={width}
+            height={width}
+            style={{border: '1px dotted red'}}
         >
-            <svg
-                viewBox={`0 0 ${VIEW_BOX_WIDTH} ${VIEW_BOX_WIDTH}`}
-                width={width}
-                height={width}
-                style={{border: '1px dotted red'}}
-            >
-                <defs>
-                    <Marker id="marker-start" color={colors[0]} />
-                    <Marker id="marker-current" color={colors[values.length - 1]} />
-                    <mask id="mask-circle">
-                        <rect x={0} y={0} width={VIEW_BOX_WIDTH} height={VIEW_BOX_WIDTH} fill="white" />
-                        <circle
-                            cx={getX(segments.at(-1)?.a2 || 0, radius)}
-                            cy={getY(segments.at(-1)?.a2 || 0, radius)}
-                            r={strokeWidth / 2 + separation}
-                            fill="black"
+            <defs>
+                <marker
+                    id="marker-current"
+                    viewBox="0 0 10 10"
+                    markerWidth={1}
+                    markerHeight={1}
+                    orient="auto"
+                    refX={5}
+                    refY={5}
+                >
+                    <circle cx={5} cy={5} r={5} fill={getColor(values.length - 1)} />
+                </marker>
+
+                <mask id="mask-line">
+                    <rect x={0} y={0} width={VIEW_BOX_WIDTH} height={VIEW_BOX_WIDTH} fill="white" />
+                    {firstNonZeroIndex >= 0 && (
+                        <path
+                            stroke="black"
+                            fill="none"
+                            strokeWidth={strokeWidth + separation * 2}
+                            strokeLinecap="round"
+                            d={createArcPath({
+                                x1: strokeWidth / 2,
+                                y1: CENTER_Y,
+                                x2: getX(segments.at(-1)?.a2 ?? VIEW_BOX_WIDTH - strokeWidth / 2, radius),
+                                y2: getY(segments.at(-1)?.a2 ?? CENTER_Y, radius),
+                                radius,
+                            })}
                         />
-                    </mask>
-                </defs>
+                    )}
+                </mask>
+            </defs>
 
-                <path
-                    stroke="lightgray"
-                    fill="none"
-                    strokeWidth={strokeWidth}
-                    strokeLinecap="round"
-                    // strokeDasharray={`${arcLenght - sum * arcLenght} ${arcLenght}`}
-                    // strokeDashoffset={-sum * arcLenght}
-                    d={createArcPath({
-                        x1: strokeWidth / 2,
-                        y1: VIEW_BOX_WIDTH / 2,
-                        x2: VIEW_BOX_WIDTH - strokeWidth / 2,
-                        y2: VIEW_BOX_WIDTH / 2,
-                        radius,
-                    })}
-                    mask="url(#mask-circle)"
-                />
+            <path
+                stroke={vars.colors.barTrack}
+                fill="none"
+                strokeWidth={strokeWidth}
+                strokeLinecap="round"
+                d={createArcPath({
+                    x1: strokeWidth / 2,
+                    y1: VIEW_BOX_WIDTH / 2,
+                    x2: VIEW_BOX_WIDTH - strokeWidth / 2,
+                    y2: VIEW_BOX_WIDTH / 2,
+                    radius,
+                })}
+                mask="url(#mask-line)"
+            />
 
-                {[...segments].reverse().map((segment, reversedIndex) => {
+            {firstNonZeroIndex >= 0 &&
+                [...segments].reverse().map((segment, reversedIndex) => {
                     // note that the list is reversed, so the first segment is drawn last
                     const index = segments.length - 1 - reversedIndex;
-                    const color = colors[index % colors.length];
-                    const isFirst = index === 0;
+                    const color = getColor(index);
+                    const isFirst = index === firstNonZeroIndex;
                     const isLast = index === segments.length - 1;
+                    const a1 = isFirst ? segment.a1 : segment.a1 + separationAngle / 2;
+                    const a2 = isLast ? segment.a2 : segment.a2 - separationAngle / 2;
+                    if (a2 <= a1) {
+                        return null;
+                    }
                     return (
                         <path
                             key={reversedIndex}
@@ -211,33 +213,33 @@ const Meter = ({width = 400}: MeterProps): JSX.Element => {
                             fill="none"
                             strokeWidth={strokeWidth}
                             strokeLinecap="butt"
-                            markerStart={isFirst ? 'url(#marker-start)' : undefined}
                             markerEnd={isLast ? 'url(#marker-current)' : undefined}
                             d={createArcPath({
-                                x1: getX(segment.a1, radius),
-                                y1: getY(segment.a1, radius),
-                                x2: getX(segment.a2, radius),
-                                y2: getY(segment.a2, radius),
+                                x1: getX(a1, radius),
+                                y1: getY(a1, radius),
+                                x2: getX(a2, radius),
+                                y2: getY(a2, radius),
                                 radius,
                             })}
                         />
                     );
                 })}
-            </svg>
-            <div>
-                <input
-                    type="range"
-                    style={{width: 400}}
-                    value={values[0] * 1000 * 8}
-                    max={1000}
-                    onChange={(e) => {
-                        const v = +e.currentTarget.value / 1000 / 8;
-                        setValue(Array.from({length: 8}, () => v));
-                    }}
-                ></input>
-                {values[0]}
-            </div>
-        </div>
+
+            {firstNonZeroIndex >= 0 && (
+                <path
+                    strokeWidth={0}
+                    fill={getColor(firstNonZeroIndex)}
+                    d={createArcPath({
+                        x1: 0,
+                        y1: CENTER_Y,
+                        x2: strokeWidth,
+                        y2: CENTER_Y,
+                        radius: strokeWidth / 2,
+                        clockwise: 0,
+                    })}
+                />
+            )}
+        </svg>
     );
 };
 
