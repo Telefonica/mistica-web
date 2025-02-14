de8a67ed358ed011f4e330cc7d6aa933a7bf0a44
Wiljan Arias <wyunreal@gmail.com>
2025-02-14 12:34:40 +0100

    WEB-2147 adding tests

--- src/__screenshot_tests__/meter-screenshot-test.tsx
+++ src/__screenshot_tests__/meter-screenshot-test.tsx
@@ -1,26 +1,32 @@
 import {openStoryPage, screen} from '../test-utils';
 
 test.each`
-    values                 | type          | themeVariant
-    ${[0]}                 | ${'linear'}   | ${'default'}
-    ${[0]}                 | ${'circular'} | ${'default'}
-    ${[0]}                 | ${'angular'}  | ${'default'}
-    ${[100]}               | ${'linear'}   | ${'default'}
-    ${[100]}               | ${'circular'} | ${'default'}
-    ${[100]}               | ${'angular'}  | ${'default'}
-    ${[33, 33]}            | ${'linear'}   | ${'default'}
-    ${[33, 33]}            | ${'circular'} | ${'default'}
-    ${[33, 33]}            | ${'angular'}  | ${'default'}
-    ${[33, 33]}            | ${'linear'}   | ${'inverse'}
-    ${[33, 33]}            | ${'circular'} | ${'inverse'}
-    ${[33, 33]}            | ${'angular'}  | ${'inverse'}
-    ${[33, 33]}            | ${'linear'}   | ${'media'}
-    ${[33, 33]}            | ${'circular'} | ${'media'}
-    ${[33, 33]}            | ${'angular'}  | ${'media'}
-    ${[20, 20, 20, 20, 0]} | ${'linear'}   | ${'default'}
-    ${[20, 20, 20, 20, 0]} | ${'circular'} | ${'default'}
-    ${[20, 20, 20, 20, 0]} | ${'angular'}  | ${'default'}
-`('Meter $themeVariant $type $values', async ({themeVariant, values, type}) => {
+    values                 | type          | themeVariant | extraContent
+    ${[0]}                 | ${'linear'}   | ${'default'} | ${''}
+    ${[0]}                 | ${'circular'} | ${'default'} | ${''}
+    ${[0]}                 | ${'angular'}  | ${'default'} | ${''}
+    ${[100]}               | ${'linear'}   | ${'default'} | ${''}
+    ${[100]}               | ${'circular'} | ${'default'} | ${''}
+    ${[100]}               | ${'angular'}  | ${'default'} | ${''}
+    ${[33, 33]}            | ${'linear'}   | ${'default'} | ${''}
+    ${[33, 33]}            | ${'circular'} | ${'default'} | ${''}
+    ${[33, 33]}            | ${'angular'}  | ${'default'} | ${''}
+    ${[33, 33]}            | ${'linear'}   | ${'inverse'} | ${''}
+    ${[33, 33]}            | ${'circular'} | ${'inverse'} | ${''}
+    ${[33, 33]}            | ${'angular'}  | ${'inverse'} | ${''}
+    ${[33, 33]}            | ${'linear'}   | ${'media'}   | ${''}
+    ${[33, 33]}            | ${'circular'} | ${'media'}   | ${''}
+    ${[33, 33]}            | ${'angular'}  | ${'media'}   | ${''}
+    ${[20, 20, 20, 20, 0]} | ${'linear'}   | ${'default'} | ${''}
+    ${[20, 20, 20, 20, 0]} | ${'circular'} | ${'default'} | ${''}
+    ${[20, 20, 20, 20, 0]} | ${'angular'}  | ${'default'} | ${''}
+    ${[0]}                 | ${'linear'}   | ${'default'} | ${'This is a small extra'}
+    ${[0]}                 | ${'linear'}   | ${'default'} | ${'This is a supper long extra This is a supper long extra This is a supper long extra This is a supper long extra'}
+    ${[0]}                 | ${'angular'}  | ${'default'} | ${'This is a small extra'}
+    ${[0]}                 | ${'angular'}  | ${'default'} | ${'This is a supper long extra This is a supper long extra This is a supper long extra This is a supper long extra'}
+    ${[0]}                 | ${'circular'} | ${'default'} | ${'This is a small extra'}
+    ${[0]}                 | ${'circular'} | ${'default'} | ${'This is a supper long extra This is a supper long extra This is a supper long extra This is a supper long extra'}
+`('Meter $themeVariant $type $values', async ({themeVariant, values, type, extraContent}) => {
     await openStoryPage({
         id: 'components-data-visualizations-meter--meter-story',
         args: {
@@ -35,6 +41,7 @@ test.each`
                 }),
                 {}
             ),
+            extraContent,
         },
     });
 
