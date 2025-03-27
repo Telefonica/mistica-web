b393e0e9988c10eb83c1809c170e872520cb3b37
SilasYaw <e.novum-b2cde-13.cdo@telefonica.com>
2025-03-27 14:59:18 +0100

    style: :rotating_light: Run prettier for Lintting

--- src/fixed-footer-layout.tsx
+++ src/fixed-footer-layout.tsx
@@ -163,11 +163,11 @@ const FixedFooterLayout = ({
                 {renderBackground()}
                 {children}
             </div>
-            /**
-             * This style is added to the html element to avoid the
-             * footer covering the focus of the elements when there is overflow 
-             */
-            {isFooterVisible &&  isFooterFixed && <style>{`html {scroll-padding-bottom: ${domFooterHeight}px};`}</style>}
+            /** * This style is added to the html element to avoid the * footer covering the focus of the
+            elements when there is overflow */
+            {isFooterVisible && isFooterFixed && (
+                <style>{`html {scroll-padding-bottom: ${domFooterHeight}px};`}</style>
+            )}
             <div
                 className={classnames(styles.footer, {
                     [styles.withoutFooter]: !isFooterVisible,
