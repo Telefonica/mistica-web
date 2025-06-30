43cfdfaaf81c63aff224fc72d7342177568c6e13
Ignacio Ceballos (Yayo) <yceballost@gmail.com>
2025-06-30 17:24:09 +0200

    fix(Text): use tokens in Text (#1401)
    WEB-2240

--- src/__screenshot_tests__/text-screenshot-test.tsx
+++ src/__screenshot_tests__/text-screenshot-test.tsx
@@ -3,18 +3,23 @@ import {openStoryPage, screen} from '../test-utils';
 import type {Device} from '../test-utils';
 
 const TESTABLE_DEVICES: Array<Device> = ['MOBILE_IOS', 'DESKTOP'];
+const SKINS = ['Movistar', 'O2', 'Vivo-new', 'Telefonica', 'Blau', 'O2-new', 'Tu', 'Esimflag'] as const;
 
-test.each(TESTABLE_DEVICES)('Text in %s', async (device) => {
-    await openStoryPage({
-        id: 'components-text--text-components',
-        device,
-    });
+test.each(TESTABLE_DEVICES.flatMap((device) => SKINS.map((skin) => ({device, skin}))))(
+    'Text in $device with $skin skin',
+    async ({device, skin}) => {
+        await openStoryPage({
+            id: 'components-text--text-components',
+            device,
+            skin,
+        });
 
-    const element = await screen.findByTestId('text');
+        const element = await screen.findByTestId('text');
 
-    const image = await element.screenshot();
-    expect(image).toMatchImageSnapshot();
-});
+        const image = await element.screenshot();
+        expect(image).toMatchImageSnapshot();
+    }
+);
 
 test('Text wrapping', async () => {
     await openStoryPage({
