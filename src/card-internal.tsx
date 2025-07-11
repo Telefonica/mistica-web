f3235c0f893a93d05732044b34903cd83da6020b
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2025-07-11 10:59:27 +0200

    WEB-2264 update screenshots

--- src/card-internal.tsx
+++ src/card-internal.tsx
@@ -445,7 +445,7 @@ export const useVideoWithControls = (
 
     return {
         video,
-        videoAction: videoStatus === 'playing' || videoStatus === 'paused' ? videoAction : undefined,
+        videoAction,
         videoStatus,
     };
 };
