658226a68fe8969bd406bbf76dbbe2c07cd670ce
Pedro Ladaria <pedro.jose.ladaria.linden@telefonica.com>
2025-07-18 13:22:15 +0200

    WEB-2264 fix cover-hero

--- src/cover-hero-media.tsx
+++ src/cover-hero-media.tsx
@@ -29,7 +29,7 @@ export const CoverHeroMedia = ({
             {backgroundImage && <Image width="100%" height="100%" src={backgroundImage} />}
             {backgroundVideo && video}
             <div className={styles.mediaOverlay} />
-            {videoAction && <TopActions videoAction={videoAction} variant="media" />}
+            {videoAction && <TopActions topActions={[videoAction]} variant="media" />}
         </div>
     );
 };
