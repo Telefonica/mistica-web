a6cb21f524a3b95e1c29fbd2e0342da20eb6df2d
Ignacio Ceballos (Yayo) <yceballost@gmail.com>
2025-09-22 18:45:25 +0200

    update chip radius

--- src/skins/movistar-new.tsx
+++ src/skins/movistar-new.tsx
@@ -409,7 +409,7 @@ export const getMovistarNewSkin: GetKnownSkin = () => {
             checkbox: '4px',
             container: '16px',
             indicator: '0px',
-            chip: '4px',
+            chip: '24px',
             input: '12px',
             legacyDisplay: '16px',
             popup: '8px',
