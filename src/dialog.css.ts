f7a0a59ceef11298ca6aabf63faca8c753343658
Jaime Fernández Moreno <jfmoreno@paradigmadigital.com>
2025-10-09 12:07:00 +0200

    Adding maxHeight to dialog-container in order to avoid screen overflow in dialog, alert and confirm

--- src/dialog.css.ts
+++ src/dialog.css.ts
@@ -90,6 +90,7 @@ const dialogContainer = style([
     {
         width: 'calc(100vw - 48px)',
         margin: 'auto',
+        maxHeight: '90vh',
 
         '@media': {
             [mq.desktopOrBigger]: {
