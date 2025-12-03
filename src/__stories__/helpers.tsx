c9677b48c1e3427505b0cc4981e66cc0dc654f0b
Abel Toledano <atoledano@tuenti.com>
2025-12-03 15:18:04 +0100

    removed unused import

--- src/__stories__/helpers.tsx
+++ src/__stories__/helpers.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Text, Select, TextField, Checkbox, skinVars, Box} from '..';
+import {Text, Select, TextField, Checkbox, skinVars} from '..';
 import {isRunningAcceptanceTest} from '../utils/platform';
 
 type Props = {
