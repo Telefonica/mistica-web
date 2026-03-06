4e2e270dcd9863eaae5bb8bded6757ac6d27f63b
Marcos Luis Delgado <mluis@tuenti.com>
2026-03-06 09:48:40 +0000

    WEB-2340: restore image ssr

--- src/__acceptance_tests__/__ssr_pages__/image.tsx
+++ src/__acceptance_tests__/__ssr_pages__/image.tsx
@@ -5,11 +5,7 @@ const ImageTest = (): JSX.Element => (
     <ResponsiveLayout>
         <Stack space={48}>
             <Title1>Image aspectRatio: 16:9</Title1>
-            <Image
-                src="not-found-image-so-it-does-not-load-sometimes-and-makes-test-flaky.jpg"
-                aspectRatio="16:9"
-                width={200}
-            />
+            <Image src="using-vr.jpg" aspectRatio="16:9" width={200} />
         </Stack>
     </ResponsiveLayout>
 );
