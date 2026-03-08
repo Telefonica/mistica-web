3d59c23c1cb99850d4c1a4f08abbec8e0daf7f28
Ignacio Ceballos (Yayo) <yceballost@gmail.com>
2026-03-08 11:50:57 +0100

    dialog and carousel

--- src/__figma__/carousel.figma.tsx
+++ src/__figma__/carousel.figma.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import {Carousel, CenteredCarousel, Slideshow} from '../carousel';
+import {
+    Carousel,
+    CenteredCarousel,
+    Slideshow,
+    MediaCard,
+    Tag,
+    ButtonPrimary,
+    ButtonLink,
+    Image,
+    Circle,
+    Text10,
+    skinVars,
+} from '../../dist';
 import figma from '@figma/code-connect';
 
 const carouselProps = {
     autoplay: figma.boolean('Autoplay'),
-    // withBullets: figma.boolean('Show bullets'),
+    withBullets: figma.boolean('Show bullets'),
     withControls: figma.boolean('Controls'),
 };
 
@@ -13,10 +25,36 @@ figma.connect(
     Carousel,
     'https://www.figma.com/design/DSWhPLyJzbliP1fBrLxDUR/M%C3%ADstica-Desktop?node-id=2308%3A4703',
     {
-        props: carouselProps,
+        props: {
+            ...carouselProps,
+            initialActiveItem: figma.enum('Current page', {
+                First: 0,
+                Middle: 1,
+                Last: 2,
+            }),
+        },
         example: (props) => (
             <Carousel
-                items={[<div key="1">Slide 1</div>, <div key="2">Slide 2</div>, <div key="3">Slide 3</div>]}
+                items={Array.from({length: 6}, (_, idx) => (
+                    <MediaCard
+                        headline={<Tag type="promo">Headline</Tag>}
+                        title={'Card ' + idx}
+                        description="Description"
+                        imageSrc="https://picsum.photos/1200/1200"
+                        buttonPrimary={
+                            <ButtonPrimary small onPress={() => {}}>
+                                Action
+                            </ButtonPrimary>
+                        }
+                        buttonLink={
+                            <ButtonLink small onPress={() => {}}>
+                                Link
+                            </ButtonLink>
+                        }
+                    />
+                ))}
+                itemsPerPage={{mobile: 2, desktop: 10}}
+                initialActiveItem={props.initialActiveItem}
                 autoplay={props.autoplay}
                 withBullets={props.withBullets}
                 withControls={props.withControls}
@@ -30,12 +68,32 @@ figma.connect(
     'https://www.figma.com/design/DSWhPLyJzbliP1fBrLxDUR/M%C3%ADstica-Desktop?node-id=29254%3A860',
     {
         props: {
-            withBullets: figma.boolean('Show bullets'),
-            withControls: figma.boolean('Controls'),
+            ...carouselProps,
+            initialActiveItem: figma.enum('Current page', {
+                First: 0,
+                Middle: 1,
+                Last: 2,
+            }),
         },
         example: (props) => (
             <CenteredCarousel
-                items={[<div key="1">Slide 1</div>, <div key="2">Slide 2</div>, <div key="3">Slide 3</div>]}
+                items={Array.from({length: 6}, (_, idx) => (
+                    <div
+                        style={{
+                            border: `1px solid ${skinVars.colors.border}`,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <div style={{flexShrink: 0}}>
+                            <Circle backgroundColor={skinVars.colors.brand} size={160}>
+                                <Text10>{idx}</Text10>
+                            </Circle>
+                        </div>
+                    </div>
+                ))}
+                initialActiveItem={props.initialActiveItem}
                 withBullets={props.withBullets}
                 withControls={props.withControls}
             />
@@ -48,13 +106,20 @@ figma.connect(
     'https://www.figma.com/design/DSWhPLyJzbliP1fBrLxDUR/M%C3%ADstica-Desktop?node-id=2249%3A5240',
     {
         props: {
-            autoplay: figma.boolean('Autoplay'),
-            withBullets: figma.boolean('Show bullets'),
-            withControls: figma.boolean('Controls'),
+            ...carouselProps,
+            initialActiveIndex: figma.enum('Current Slide', {
+                First: 0,
+                Middle: 1,
+                Last: 2,
+            }),
         },
         example: (props) => (
             <Slideshow
-                items={[<div key="1">Slide 1</div>, <div key="2">Slide 2</div>, <div key="3">Slide 3</div>]}
+                items={[
+                    <Image src="https://picsum.photos/1200/1200" aspectRatio="16:9" />,
+                    <Image src="https://picsum.photos/1200/1200" aspectRatio="16:9" />,
+                ]}
+                initialPageIndex={props.initialActiveIndex}
                 autoplay={props.autoplay}
                 withBullets={props.withBullets}
                 withControls={props.withControls}
@@ -68,10 +133,43 @@ figma.connect(
     Carousel,
     'https://www.figma.com/design/WCkDDzlXE16R6yXaljxddj/M%C3%ADstica-Mobile?node-id=4229%3A8914',
     {
-        props: carouselProps,
+        props: {
+            ...carouselProps,
+
+            itemsPerPage: figma.enum('Items per page', {
+                '1': 1,
+                '2': 2,
+            }),
+            initialActiveItem: figma.enum('Current slide', {
+                '#1': 0,
+                '#2': 1,
+                '#3': 2,
+                '#4': 3,
+                '#5': 4,
+            }),
+        },
         example: (props) => (
             <Carousel
-                items={[<div key="1">Slide 1</div>, <div key="2">Slide 2</div>, <div key="3">Slide 3</div>]}
+                items={Array.from({length: 6}, (_, idx) => (
+                    <MediaCard
+                        headline={<Tag type="promo">Headline</Tag>}
+                        title={'Card ' + idx}
+                        description="Description"
+                        imageSrc="https://picsum.photos/1200/1200"
+                        buttonPrimary={
+                            <ButtonPrimary small onPress={() => {}}>
+                                Action
+                            </ButtonPrimary>
+                        }
+                        buttonLink={
+                            <ButtonLink small onPress={() => {}}>
+                                Link
+                            </ButtonLink>
+                        }
+                    />
+                ))}
+                itemsPerPage={props.itemsPerPage}
+                initialActiveItem={props.initialActiveItem}
                 autoplay={props.autoplay}
                 withBullets={props.withBullets}
                 withControls={props.withControls}
--- src/__figma__/dialog.figma.tsx
+++ src/__figma__/dialog.figma.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import Dialog from '../dialog';
+import {ButtonPrimary, ButtonLink, IconInformationUserLight, Text1, skinVars} from '../../dist';
 import figma from '@figma/code-connect';
-
-const dialogProps = {
-    title: figma.boolean('Title', {
-        true: 'Title',
-        false: undefined,
-    }),
-};
 
 // Desktop - Confirm dialog
 figma.connect(
     Dialog,
     'https://www.figma.com/design/DSWhPLyJzbliP1fBrLxDUR/M%C3%ADstica-Desktop?node-id=5689%3A4566',
     {
-        props: dialogProps,
+        props: {
+            title: figma.boolean('Title', {
+                true: figma.textContent('Title'),
+                false: undefined,
+            }),
+            message: figma.textContent('Description text'),
+        },
         example: (props) => (
-            <Dialog
-                type="confirm"
-                title={props.title}
-                message="Message"
-                acceptText="Accept"
-                cancelText="Cancel"
-                onAccept={() => {}}
-                onCancel={() => {}}
-            />
+            <ButtonPrimary
+                onPress={() =>
+                    confirm({
+                        title: props.title,
+                        message: props.message,
+                        acceptText: 'Accept',
+                        cancelText: 'Cancel',
+                    })
+                }
+            >
+                Open dialog
+            </ButtonPrimary>
         ),
     }
 );
@@ -34,15 +37,25 @@ figma.connect(
     Dialog,
     'https://www.figma.com/design/DSWhPLyJzbliP1fBrLxDUR/M%C3%ADstica-Desktop?node-id=1217%3A4059',
     {
-        props: dialogProps,
+        props: {
+            title: figma.boolean('Title', {
+                true: figma.textContent('Title'),
+                false: undefined,
+            }),
+            message: figma.textContent('Description text'),
+        },
         example: (props) => (
-            <Dialog
-                type="alert"
-                title={props.title}
-                message="Message"
-                acceptText="Accept"
-                onAccept={() => {}}
-            />
+            <ButtonPrimary
+                onPress={() =>
+                    alert({
+                        title: props.title,
+                        message: 'Message',
+                        acceptText: 'Accept',
+                    })
+                }
+            >
+                Open dialog
+            </ButtonPrimary>
         ),
     }
 );
@@ -53,24 +66,38 @@ figma.connect(
     'https://www.figma.com/design/DSWhPLyJzbliP1fBrLxDUR/M%C3%ADstica-Desktop?node-id=5680%3A5004',
     {
         props: {
-            subtitle: figma.boolean('Subtitle', {
-                true: 'Subtitle',
+            asset: figma.boolean('Icon', {
+                true: <IconInformationUserLight color={skinVars.colors.brand} />,
                 false: undefined,
             }),
-            description: figma.boolean('Description', {
-                true: 'Description',
+            title: figma.textContent('Title'),
+            subtitle: figma.boolean('Subtitle', {
+                true: figma.textContent('Subtitle'),
+                false: undefined,
+            }),
+            message: figma.boolean('Description', {
+                true: figma.textContent('Description text'),
                 false: undefined,
             }),
         },
         example: (props) => (
-            <Dialog
-                type="dialog"
-                title="Title"
-                subtitle={props.subtitle}
-                message="Message"
-                acceptText="Accept"
-                onAccept={() => {}}
-            />
+            <ButtonPrimary
+                onPress={() =>
+                    dialog({
+                        asset: props.asset,
+                        title: props.title,
+                        subtitle: props.subtitle,
+                        message: props.message,
+                        acceptText: 'Accept',
+                        cancelText: 'Cancel',
+                        extra: <Text1 regular>This is the slot zone</Text1>,
+                        showCancel: true,
+                        link: <ButtonLink href="https://google.com">Link</ButtonLink>,
+                    })
+                }
+            >
+                Open dialog
+            </ButtonPrimary>
         ),
     }
 );
@@ -78,19 +105,105 @@ figma.connect(
 // Mobile
 figma.connect(
     Dialog,
-    'https://www.figma.com/design/WCkDDzlXE16R6yXaljxddj/M%C3%ADstica-Mobile?node-id=10722%3A6907',
+    'https://www.figma.com/design/WCkDDzlXE16R6yXaljxddj/M%C3%ADstica-Mobile?node-id=10720-7368&t=5PCXgGCWm5Z9d7Us-4',
     {
-        props: dialogProps,
+        props: {
+            asset: figma.boolean('Asset', {
+                true: <IconInformationUserLight color={skinVars.colors.brand} />,
+                false: undefined,
+            }),
+            title: figma.boolean('Title', {
+                true: figma.textContent('Title'),
+                false: undefined,
+            }),
+            subtitle: figma.boolean('Subtitle', {
+                true: figma.textContent('Subtitle'),
+                false: undefined,
+            }),
+            message: figma.boolean('Description', {
+                true: figma.textContent('Description'),
+                false: undefined,
+            }),
+            slot: figma.boolean('Slot', {
+                true: figma.children('*'),
+                false: undefined,
+            }),
+        },
         example: (props) => (
-            <Dialog
-                type="confirm"
-                title={props.title}
-                message="Message"
-                acceptText="Accept"
-                cancelText="Cancel"
-                onAccept={() => {}}
-                onCancel={() => {}}
-            />
+            <ButtonPrimary
+                onPress={() =>
+                    dialog({
+                        asset: props.asset,
+                        title: props.title,
+                        message: props.message,
+                        acceptText: 'Accept',
+                        cancelText: 'Cancel',
+                        forceWeb: true,
+                        extra: props.slot,
+                        showCancel: true,
+                        link: <ButtonLink href="https://google.com">Link</ButtonLink>,
+                    })
+                }
+            >
+                Open dialog
+            </ButtonPrimary>
+        ),
+    }
+);
+
+// Mobile - Alert dialog
+figma.connect(
+    Dialog,
+    'https://www.figma.com/design/WCkDDzlXE16R6yXaljxddj/M%C3%ADstica-Mobile?node-id=1953-10250&t=5PCXgGCWm5Z9d7Us-4',
+    {
+        props: {
+            title: figma.boolean('Title', {
+                true: figma.textContent('Title'),
+                false: undefined,
+            }),
+            message: figma.textContent('Description text'),
+        },
+        example: (props) => (
+            <ButtonPrimary
+                onPress={() =>
+                    alert({
+                        title: props.title,
+                        message: 'Message',
+                        acceptText: 'Accept',
+                    })
+                }
+            >
+                Open dialog
+            </ButtonPrimary>
+        ),
+    }
+);
+
+// Mobile - Confirm dialog
+figma.connect(
+    Dialog,
+    'https://www.figma.com/design/WCkDDzlXE16R6yXaljxddj/M%C3%ADstica-Mobile?node-id=10722-6907&t=5PCXgGCWm5Z9d7Us-4',
+    {
+        props: {
+            title: figma.boolean('Title', {
+                true: figma.textContent('Title'),
+                false: undefined,
+            }),
+            message: figma.textContent('Description text'),
+        },
+        example: (props) => (
+            <ButtonPrimary
+                onPress={() =>
+                    alert({
+                        title: props.title,
+                        message: 'Message',
+                        acceptText: 'Accept',
+                        cancelText: 'Cancel',
+                    })
+                }
+            >
+                Open dialog
+            </ButtonPrimary>
         ),
     }
 );
