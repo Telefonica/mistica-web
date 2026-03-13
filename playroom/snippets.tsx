818748f1bf2f8a1d012c5085312ca91fef6eede5
Abel Toledano <atoledano@tuenti.com>
2026-03-13 11:11:26 +0100

    feat(MainNavigationBar): topSlot support (#1518)
    WEB-2378

--- playroom/snippets.tsx
+++ playroom/snippets.tsx
@@ -2677,6 +2677,21 @@ const navigationBarSnippets = [
         name: 'MainNavigationBar',
         code: `
           <MainNavigationBar
+            topSlotBackgroundColor={colors.backgroundNegative}
+            topSlot={
+              <ThemeVariant variant="negative">
+                <Box paddingY={8}>
+                  <Align x="center">
+                    <Inline space={24} alignItems="center">
+                      <Text2 medium>BLACK FRIDAY - 60% en tecnología</Text2>
+                      <ButtonSecondary small onPress={() => {}}>
+                        See offers
+                      </ButtonSecondary>
+                    </Inline>
+                  </Align>
+                </Box>
+              </ThemeVariant>
+            }
             sections={[
               {
                 title: "Start",
