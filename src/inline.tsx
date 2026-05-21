8089eca7ff95a5a476667c5b2daccf93b6f626c1
AlexandraGallipoliRodrigues <alexandra.gallipolirodrigues@telefonica.com>
2026-05-21 17:40:56 +0200

    minor changes

--- src/inline.tsx
+++ src/inline.tsx
@@ -141,9 +141,7 @@ const Inline = ({
                 return (
                     <div
                         role={role === 'list' ? 'listitem' : undefined}
-                        className={classnames(
-                            shouldExpandItem(expand, index) && styles.expandItem
-                        )}
+                        className={classnames(shouldExpandItem(expand, index) && styles.expandItem)}
                         style={{
                             // Hack to fix https://jira.tid.es/browse/WEB-1683
                             // In iOS the inline component sometimes cuts the last line of the content
