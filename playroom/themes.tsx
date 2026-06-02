588bf02ffae0e97cf138d72e6ad8f089ae7566e4
AlexandraGallipoliRodrigues <alexandra.gallipolirodrigues@telefonica.com>
2026-06-02 16:46:15 +0200

    renamed theme at playroom

--- playroom/themes.tsx
+++ playroom/themes.tsx
@@ -16,7 +16,7 @@ export const O2_New: ThemeConfig = {...themes.O2_New, ...common};
 export const Telefonica: ThemeConfig = {...themes.Telefonica, ...common};
 export const Blau: ThemeConfig = {...themes.Blau, ...common};
 export const Esimflag: ThemeConfig = {...themes.Esimflag, ...common};
-export const Cyber: ThemeConfig = {...themes.Cyber, ...common};
+export const Community_Cyber: ThemeConfig = {...themes.Cyber, ...common};
 
 export const Movistar_New_iOS: ThemeConfig = {...Movistar_New, platformOverrides: {platform: 'ios'}};
 export const Vivo_New_iOS: ThemeConfig = {...Vivo_New, platformOverrides: {platform: 'ios'}};
@@ -24,4 +24,7 @@ export const O2_New_iOS: ThemeConfig = {...O2_New, platformOverrides: {platform:
 export const Telefonica_iOS: ThemeConfig = {...Telefonica, platformOverrides: {platform: 'ios'}};
 export const Blau_iOS: ThemeConfig = {...Blau, platformOverrides: {platform: 'ios'}};
 export const Esimflag_iOS: ThemeConfig = {...Esimflag, platformOverrides: {platform: 'ios'}};
-export const Cyber_iOS: ThemeConfig = {...Cyber, platformOverrides: {platform: 'ios'}};
+export const Community_Cyber_iOS: ThemeConfig = {
+    ...Community_Cyber,
+    platformOverrides: {platform: 'ios'},
+};
