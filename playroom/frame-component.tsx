1c75294f0baaa5d4f8990942e367246575436f52
AlexandraGallipoliRodrigues <alexandra.gallipolirodrigues@telefonica.com>
2026-06-25 16:43:51 +0200

    resolved conflicts

--- playroom/frame-component.tsx
+++ playroom/frame-component.tsx
@@ -24,7 +24,6 @@ import {
 } from '../src';
 import {CYBER_SKIN} from '../src/community';
 import {Movistar_New as defaultThemeConfig} from './themes';
-import {CYBER_SKIN} from '../src/community';
 
 import type {ThemeConfig} from '../src';
 
