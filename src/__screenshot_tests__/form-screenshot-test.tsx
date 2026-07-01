4551f73d3e19a97764960d9ffab4542bb32cfdda
Andy Méndez <andyh_007@hotmail.com>
2026-07-01 14:42:55 +0200

    VIVO-5902 apply some changes from CR

--- src/__screenshot_tests__/form-screenshot-test.tsx
+++ src/__screenshot_tests__/form-screenshot-test.tsx
@@ -1,29 +1,20 @@
 import {openStoryPage, screen} from '../test-utils';
 
-test('Form fields after submit validation', async () => {
-    await openStoryPage({
-        id: 'patterns-forms-form-with-automatic-state-management--default',
-        device: 'MOBILE_IOS',
-    });
+import type {Device} from '../test-utils';
 
-    await (await screen.findByText('Send')).click();
+test.each(['DESKTOP', 'MOBILE_IOS'] as Array<Device>)(
+    'form fields with required errors after submit validation on %s',
+    async (device) => {
+        await openStoryPage({
+            id: 'patterns-forms-form-with-automatic-state-management--default',
+            device,
+        });
 
-    const fieldWrapper = await screen.findByTestId('form-wrapper');
-    const image = await fieldWrapper.screenshot();
+        await (await screen.findByText('Send')).click();
 
-    expect(image).toMatchImageSnapshot();
-});
+        const fieldWrapper = await screen.findByTestId('form-wrapper');
+        const image = await fieldWrapper.screenshot();
 
-test('Form fields after submit validation on desktop', async () => {
-    await openStoryPage({
-        id: 'patterns-forms-form-with-automatic-state-management--default',
-        device: 'DESKTOP',
-    });
-
-    await (await screen.findByText('Send')).click();
-
-    const fieldWrapper = await screen.findByTestId('form-wrapper');
-    const image = await fieldWrapper.screenshot();
-
-    expect(image).toMatchImageSnapshot();
-});
+        expect(image).toMatchImageSnapshot();
+    }
+);
