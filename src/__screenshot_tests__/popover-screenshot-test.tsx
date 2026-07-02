6dbc6ce154d9e1d25b331f46ccfee080dab7ec2f
Matteo Bertamini <61736059+brtbrt@users.noreply.github.com>
2026-07-02 03:01:16 +0200

    update unstable tests

--- src/__screenshot_tests__/popover-screenshot-test.tsx
+++ src/__screenshot_tests__/popover-screenshot-test.tsx
@@ -19,7 +19,8 @@ test('Popover - appears properly on mobile', async () => {
     const page = await openStoryPage({
         id: 'components-popover--default',
         device: 'MOBILE_IOS',
-        skin: 'Movistar', // todo https://github.com/Telefonica/mistica-web/issues/1621 Popover unstable on Movistar skin
+        // todo https://github.com/Telefonica/mistica-web/issues/1621 Popover unstable on Movistar skin
+        skin: 'O2',
     });
 
     await page.click(await screen.findByTestId('target'));
@@ -32,7 +33,8 @@ test('Popover - inverse', async () => {
     const page = await openStoryPage({
         id: 'components-popover--default',
         device: 'MOBILE_IOS',
-        skin: 'Movistar', // todo https://github.com/Telefonica/mistica-web/issues/1621 Popover unstable on Movistar skin
+        // todo https://github.com/Telefonica/mistica-web/issues/1621 Popover unstable on Movistar skin
+        skin: 'O2',
         args: {variantOutside: 'brand'},
     });
 
