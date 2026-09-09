e84448cdd024e03ce3f4208c71f0d9a15bd2f186
Alexandra Gallipoli Rodrigues <alexandra.gallipolirodrigues@telefonica.com>
2026-09-09 21:08:33 +0200

    Update src/list.css.ts
    Co-authored-by: Ignacio Ceballos (Yayo) <yceballost@gmail.com>

--- src/list.css.ts
+++ src/list.css.ts
@@ -25,7 +25,7 @@ export const selectionOutline = style({
             position: 'absolute',
             inset: -(selectionOutlineWidth + selectionOutlineOffset),
             border: `${selectionOutlineWidth}px solid transparent`,
-            borderRadius: vars.borderRadii.container,
+            borderRadius: `calc(${vars.borderRadii.container} + 2px)`,
             pointerEvents: 'none',
             transition: 'border-color 0.08s ease-in-out',
         },
